37e3aa5baddfd24ac686f5bdf2d34d9f1aa99ebc
Song TaeHwan <axoghks@gmail.com>
2022-04-04 17:48:29 +0900

    refactor: iOS 프로젝트 디렉토리 구조 변경

--- ios/TodoApp/TodoApp/Controllers/TodoListContainerViewController.swift
+++ ios/TodoApp/TodoApp/Controllers/TodoListContainerViewController.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListContainerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
