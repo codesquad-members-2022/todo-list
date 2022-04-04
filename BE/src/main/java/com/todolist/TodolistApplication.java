00e40c5f75132e74bcf58397ee75109908469633
ikjo93 <auddlr100@naver.com>
2022-04-04 18:16:58 +0900

    init : 프로젝트 재초기화
    BE 및 FE 패키지 분리

--- BE/src/main/java/com/todolist/TodolistApplication.java
+++ BE/src/main/java/com/todolist/TodolistApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoListApplication {
+public class TodolistApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoListApplication.class, args);
+		SpringApplication.run(TodolistApplication.class, args);
 	}
 
 }
--- BE/src/test/java/com/todolist/TodolistApplicationTests.java
+++ BE/src/test/java/com/todolist/TodolistApplicationTests.java
@@ -1,10 +1,10 @@
-package com.todolist.todolist;
+package com.todolist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodoListApplicationTests {
+class TodolistApplicationTests {
 
 	@Test
 	void contextLoads() {
