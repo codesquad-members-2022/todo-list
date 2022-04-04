c9a3990ee110ca57f248c0f323469638c23832f5
Song TaeHwan <axoghks@gmail.com>
2022-04-04 19:39:13 +0900

    feat: TodoListContainerViewController 헤더 UI 추가

--- ios/TodoApp/TodoApp/Controllers/TodoListContainerViewController.swift
+++ ios/TodoApp/TodoApp/Controllers/TodoListContainerViewController.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 class TodoListContainerViewController: UIViewController {
-
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureMenuButton()
+    }
+    
+    private func configureMenuButton() {
+        self.menuButton.addAction(UIAction(handler: self.toggleMenuButton(_:)), for: .touchUpInside)
+    }
+    
+    private func toggleMenuButton(_ action: UIAction) {
+        // TODO: 활동기록을 보여주는 ViewController 보여주기
     }
 
 
