7ac9ebdb57672f3bf60176bef11603cdc642987d
sanghyeok-kim <slsnsi1212@gmail.com>
2022-04-04 11:53:40 +0900

    [add] #2 프로젝트 파일 생성 및 불필요한 코드 제거

---
+++ ios/ToDoListApp/ToDoListApp/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  ToDoListApp
+//
+//  Created by 김상혁 on 2022/04/04.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ ios/ToDoListApp/ToDoListApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by 김상혁 on 2022/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
