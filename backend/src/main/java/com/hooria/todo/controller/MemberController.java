e7467f0c7bb275a950fb4127ad68cbb7f1f0c129
naneun <rmtlrksdp@naver.com>
2022-04-05 15:17:44 +0900

    ✨ Swagger 설정 완료

---
+++ backend/src/main/java/com/hooria/todo/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.hooria.todo.controller;
+
+import com.hooria.todo.domain.Member;
+import com.hooria.todo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping
+    public List<Member> getAllMembers(){
+        return memberRepository.findAll();
+    }
+}
