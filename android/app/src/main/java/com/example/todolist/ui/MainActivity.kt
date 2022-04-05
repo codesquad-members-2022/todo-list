d73c31bbae0b79b4d60e05b008554ec94cd38932
bn-tw2020 <skaxodn97@gmail.com>
2022-04-05 15:55:42 +0900

    ui: navigation_drawer 리사이클러 뷰 레이아웃 추가#14

--- android/app/src/main/java/com/example/todolist/ui/MainActivity.kt
+++ android/app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -1,14 +1,21 @@
-package com.example.todolist
+package com.example.todolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import androidx.databinding.DataBindingUtil
+import com.example.todolist.R
 import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnDrawer.setOnClickListener {
+            binding.dlDrawer.openDrawer(Gravity.RIGHT)
+        }
     }
 }
