fa97649e93e893bb97b5642c24544e7b363c9017
Song TaeHwan <axoghks@gmail.com>
2022-04-05 19:39:01 +0900

    feat: 활동기록 Drawer view 추가

--- ios/TodoApp/TodoApp/Controllers/TodoListContainerViewController.swift
+++ ios/TodoApp/TodoApp/Controllers/TodoListContainerViewController.swift
@@ -8,21 +8,31 @@
 import UIKit
 
 class TodoListContainerViewController: UIViewController {
-    @IBOutlet weak var menuButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var drawerView: UIView!
+    @IBOutlet weak var closeButton: UIButton!
     @IBOutlet weak var columnStack: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureMenuButton()
+        self.configureUI()
     }
     
-    private func configureMenuButton() {
+    private func configureUI() {
+        self.drawerView.frame.origin.x = self.view.frame.width
         self.menuButton.addAction(UIAction(handler: self.toggleMenuButton(_:)), for: .touchUpInside)
+        self.closeButton.addAction(UIAction(handler: self.toggleMenuButton(_:)), for: .touchUpInside)
     }
     
     private func toggleMenuButton(_ action: UIAction) {
-        // TODO: 활동기록을 보여주는 ViewController 보여주기
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
+            if self.drawerView.frame.origin.x == self.view.frame.width {
+                self.drawerView.frame.origin = CGPoint(x: self.drawerView.frame.origin.x - self.drawerView.frame.width, y: 0)
+            } else {
+                self.drawerView.frame.origin = CGPoint(x: self.drawerView.frame.origin.x + self.drawerView.frame.width, y: 0)
+            }
+        }
     }
 
 
--- ios/TodoApp/TodoApp/Controllers/TodoListViewController.swift
+++ ios/TodoApp/TodoApp/Controllers/TodoListViewController.swift
@@ -11,7 +11,7 @@ class TodoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .green
+        self.view.backgroundColor = .orange
     }
 
 
