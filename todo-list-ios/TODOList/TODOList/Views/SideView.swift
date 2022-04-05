75a620cc881d08a71e1e5a6cb4c29196e23bbc8e
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-05 15:48:01 +0900

    [#9] Style: SideView 내부의 TableView 설정 추가, 오토레이아웃 일부 수정
    - UITableView 타입 속성 추가
    - closeButton 속성 오토레이아웃 수정

--- todo-list-ios/TODOList/TODOList/Views/SideView.swift
+++ todo-list-ios/TODOList/TODOList/Views/SideView.swift
@@ -6,7 +6,15 @@ class SideView: UIView {
     let closeButton: UIButton = {
         let button = UIButton(type: .close)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .white
         return button
+    }()
+    
+    let tableView: UITableView = {
+        let view = UITableView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(SideViewTableViewCell.self, forCellReuseIdentifier: SideViewTableViewCell.identifier)
+        return view
     }()
     
     required override init(frame: CGRect) {
@@ -21,11 +29,17 @@ class SideView: UIView {
     
     private func setLayout() {
         self.addSubview(closeButton)
+        self.addSubview(tableView)
         NSLayoutConstraint.activate([
-            closeButton.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0),
+            closeButton.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
             closeButton.widthAnchor.constraint(equalToConstant: 60),
             closeButton.heightAnchor.constraint(equalToConstant: 60),
-            closeButton.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: 0)
+            closeButton.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            
+            tableView.topAnchor.constraint(equalTo: self.closeButton.bottomAnchor, constant: 20),
+            tableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: 0)
         ])
     }
 }
