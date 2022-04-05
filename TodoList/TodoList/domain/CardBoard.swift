eb2119177bdc42c45fc1ba0564a4293f67179c94
kimdongjune <jun36817@naver.com>
2022-04-05 22:19:13 +0900

    feature - JSON Decode, Encode 사용 가능하게 변경. #5

--- TodoList/TodoList/domain/CardBoard.swift
+++ TodoList/TodoList/domain/CardBoard.swift
@@ -8,6 +8,16 @@
 import Foundation
 
 class CardBoard {
-    private(set) var selectedTodo: Card?
+    private(set) var selectedCard: Card?
     private(set) var selectedIndex: Int?
+    private(set) var todoCards: [Card] = []
+    private(set) var doingCards: [Card] = []
+    private(set) var doneCards: [Card] = []
+    private let cardFactory = CardFactory()
+    
+    func addNewCard(){
+        guard let card = cardFactory.createRandomCard() as? Card else { return }
+        let data = try? JSONEncoder().encode(card)
+        
+    }
 }
