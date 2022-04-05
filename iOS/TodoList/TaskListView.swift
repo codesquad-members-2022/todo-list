63a151454ac34866df961eade51c3d58fe6ad5d2
Lee SeEun <sally4405@naver.com>
2022-04-05 14:47:34 +0900

    [#1] feat: task list xib 생성

---
+++ iOS/TodoList/TaskListView.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class TaskListView: UIView {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var taskCountBadge: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    @IBAction func addTaskButtonTouched(_ sender: UIButton) {
+    }
+}
