c65a14589a79133b8183eab6fa043b6b42ed3d44
happyGyu <happyboyto.dev@gmail.com>
2022-04-05 23:58:07 +0900

    Feat: header 컴포넌트 분리

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import "./app.scss";
-import "./components/header.js";
+import Header from "./components/header.js";
 import "./components/card.js";
 import "./components/column.js";
 import "./components/menu.js";
 import "./components/fab.js";
 import "./components/alert.js";
+
+function runApp() {
+  renderApp();
+}
+
+function renderApp() {
+  const header = new Header();
+  header.render();
+}
+
+runApp();
+
--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,17 @@
 import "./header.scss";
+
+export default class Header {
+  render() {
+    const template = this.#getTemplate();
+    document.body.insertAdjacentHTML("afterbegin", template);
+  }
+
+  #getTemplate() {
+    return `
+      <header>
+        <h1 class="header__title">TO-DO LIST</h1>
+        <div class="header__menu-btn"></div>
+      </header>
+    `;
+  }
+}
