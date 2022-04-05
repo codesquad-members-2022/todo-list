9ec731ba44f90017efb732c36cbe992b18591ce5
wnsxor1993 <wnsxor1993@naver.com>
2022-04-05 14:09:27 +0900

    CHORE: 폴더명 및 main VC명 변경

--- TodoList/TodoList/ViewController/ContainerViewController.swift
+++ TodoList/TodoList/ViewController/ContainerViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContainerViewController: UIViewController {
     
     var headerVC: HeaderViewController!
     var contentVC: ContentViewController!
