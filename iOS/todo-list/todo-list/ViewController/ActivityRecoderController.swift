e2531fb1e459507008c692ef39197659cff0d996
Jason <ggj00125@gmail.com>
2022-04-05 14:19:52 +0900

    🔧:기존 ViewController 파일 SplitViewController로 파일 및 클래스 네이밍 변경 & ActivityRecordViewController파일 및 클래스 생성

---
+++ iOS/todo-list/todo-list/ViewController/ActivityRecoderController.swift
@@ -0,0 +1,13 @@
+//
+//  ActivityRecoderController.swift
+//  todo-list
+//
+//  Created by Jason on 2022/04/05.
+//
+
+import Foundation
+import UIKit
+
+class ActivityRecordController: UITableViewController {
+    
+}
