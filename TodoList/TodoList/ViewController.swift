6491693f55d8535b1cbc4df1fbc7be12425036a9
kimdongjune <jun36817@naver.com>
2022-04-05 11:40:38 +0900

    init - 프로젝트 초기 셋팅 #1

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by 김동준 on 2022/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
