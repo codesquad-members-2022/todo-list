aee13a1d93297157f6c8576f48536cf8786a9f57
ak2j38 <k2j38@naver.com>
2022-04-05 14:39:11 +0900

    init : todolist 프로젝트 백엔드 최초 커밋
    1. 설정파일 추가
    2. README 수정
    
    end

---
+++ BE/src/test/java/com/team26/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team26.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
