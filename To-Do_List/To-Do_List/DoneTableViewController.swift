2df901104601400b3fc408d0da6a0beaf1943073
P_SeoB <akdlsjtm21@naver.com>
2022-04-05 09:47:20 +0900

    Feat:Add Each ViewController to TableView

--- To-Do_List/To-Do_List/DoneTableViewController.swift
+++ To-Do_List/To-Do_List/DoneTableViewController.swift
@@ -1,32 +1,30 @@
 //
-//  LogTableViewController.swift
+//  DoneTableViewController.swift
 //  To-Do_List
 //
-//  Created by 박진섭 on 2022/04/04.
+//  Created by 박진섭 on 2022/04/05.
 //
 
 import UIKit
 
-class LogTableViewController: UITableViewController {
+class DoneTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .gray
-        
-        
+
+        self.view.backgroundColor = .brown
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        
         return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return 0
     }
-
 
 }
--- To-Do_List/To-Do_List/MainViewController.swift
+++ To-Do_List/To-Do_List/MainViewController.swift
@@ -9,22 +9,47 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var tableStackView: UIStackView!
+//    @IBOutlet weak var tableStackView: UIStackView!
     
-    @IBOutlet weak var doingTable: UITableView!
-    @IBOutlet weak var todoTable: UITableView!
-    @IBOutlet weak var doneTable: UITableView!
-    
+    @IBOutlet weak var statckView1: UIStackView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.title = "TO-DO-LIST"
-        self.doingTable.backgroundColor = .blue
-        self.tableStackView.backgroundColor = .green
-        self.doneTable.backgroundColor = .brown
-        
-        
         self.view.backgroundColor = .white
-    }
+        self.title = "TO-DO-LIST"
+        self.navigationController?.navigationBar.backgroundColor = .darkGray
+        
+        // 현재의 뷰 컨트롤러에 자식 뷰 컨트롤러를 추가하는 코드
+        let storyboard1 = UIStoryboard(name: "Main", bundle: .main)
+        if let viewController = storyboard1.instantiateViewController(identifier: "ToDoTableViewController")
+                                            as? ToDoTableViewController {
+            
+           // 핵심 메소드로서, 뷰 컨트롤러에 부모-자식 관계를 형성한다
+           addChild(viewController)
+           
+           // 자식 뷰 컨트롤러의 루트 뷰를 현재 뷰의 뷰 계층에 추가하는 코드
+            self.statckView1.addArrangedSubview(viewController.view)
+        }
+        
+        let storyboard2 = UIStoryboard(name: "Main", bundle: .main)
+        if let viewController = storyboard2.instantiateViewController(identifier: "DoneTableViewController")
+                                            as? DoneTableViewController {
 
+           // 핵심 메소드로서, 뷰 컨트롤러에 부모-자식 관계를 형성한다
+           addChild(viewController)
+
+            self.statckView1.addArrangedSubview(viewController.view)
+        }
+        
+        let storyboard3 = UIStoryboard(name: "Main", bundle: .main)
+        if let viewController = storyboard3.instantiateViewController(identifier: "DoingTableViewController")
+                                            as? DoingTableViewController {
+
+           // 핵심 메소드로서, 뷰 컨트롤러에 부모-자식 관계를 형성한다
+           addChild(viewController)
+
+            self.statckView1.addArrangedSubview(viewController.view)
+        }
+    }
+    
 }
