db490e46f7873b98d72f99d1eaf10ae5bda98cab
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-05 15:11:24 +0900

    [#9] Style: 사이드뷰에 닫기 버튼 추가

--- todo-list-ios/TODOList/TODOList/Controllers/ViewController.swift
+++ todo-list-ios/TODOList/TODOList/Controllers/ViewController.swift
@@ -1,10 +1,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let sideView: SideView = {
+        let view = SideView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setLayout()
     }
-
+    
+    private func setLayout(){
+        self.view.addSubview(sideView)
+        NSLayoutConstraint.activate([
+            sideView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            sideView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            sideView.widthAnchor.constraint(equalToConstant: 200),
+            sideView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0)
+        ])
+    }
+    
 }
 
--- todo-list-ios/TODOList/TODOList/Views/SideView.swift
+++ todo-list-ios/TODOList/TODOList/Views/SideView.swift
@@ -3,11 +3,29 @@ import UIKit
 
 class SideView: UIView {
     
+    let closeButton: UIButton = {
+        let button = UIButton(type: .close)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     required override init(frame: CGRect) {
         super.init(frame: frame)
+        setLayout()
     }
     
     override required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setLayout()
+    }
+    
+    private func setLayout() {
+        self.addSubview(closeButton)
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0),
+            closeButton.widthAnchor.constraint(equalToConstant: 60),
+            closeButton.heightAnchor.constraint(equalToConstant: 60),
+            closeButton.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: 0)
+        ])
     }
 }
