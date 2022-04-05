1ce0936f0bdf8a80b023e8ea35c974f465880267
junseokseo <junseoks9306@gmail.com>
2022-04-05 14:55:27 +0900

    [AOS-josh] feat: 앱바 메뉴 설정

--- android/app/src/main/java/com/codesquad/aos/todolist/MainActivity.kt
+++ android/app/src/main/java/com/codesquad/aos/todolist/MainActivity.kt
@@ -2,10 +2,25 @@ package com.codesquad.aos.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.codesquad.aos.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.toolbarMenu?.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.appbar_main_menu -> {
+                    Log.d("mainActivity", "appbar-menu")
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
