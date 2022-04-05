6b1bf3e7b4f377b74eb89da1e639c0bf686d73d2
inust33 <inust33@gmail.com>
2022-04-05 10:49:33 +0900

    FEAT:todo-column markup

---
+++ core/Store.ts
@@ -0,0 +1,17 @@
+
+export class Store {
+    private _state;
+    protected state;
+    constructor(state) {
+        this._state = state;
+    }
+    observer(state){
+        const handler:ProxyHandler<any> = {
+            get:(target, name)=>{
+                const prop = target[name];
+                if(!prop)return undefined;
+
+            }
+        }
+    }
+}
