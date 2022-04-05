d7d1555fa6d8458ae9bdc91c7ee1a05149f148b4
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-05 14:43:10 +0900

    [#9] Feat : HeaderView, MemoCanvasView, SideView 추가

---
+++ todo-list-ios/TODOList/TODOList/Views/SideView.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+class SideView: UIView {
+    
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
