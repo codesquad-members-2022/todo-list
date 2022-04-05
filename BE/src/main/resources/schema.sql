41e6436165e3ea5448d3469e534a32ce8eb5b007
Minseok-Choi <cms05041@gmail.com>
2022-04-05 12:31:13 +0900

    [#3] Feat: schema.sql 작성
    todo_user, card, log ddl 쿼리 작성

---
+++ BE/src/main/resources/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE card
+(
+     id BIGINT AUTO_INCREMENT NOT NULL,
+     user_name VARCHAR(255) NOT NULL,
+     column INT NOT NULL,
+     subject VARCHAR(255) NOT NULL,
+     contents VARCHAR(255) NOT NULL,
+     create_time TIMESTAMP NOT NULL,
+     update_time TIMESTAMP,
+     deleted boolean DEFAULT FALSE,
+     PRIMARY KEY (id)
+);
+
+CREATE TABLE todo_user
+(
+    id       VARCHAR(255) NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE log
+(
+    id BIGINT NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    `from` INT,
+    `to` INT,
+    active_time TIMESTAMP NOT NULL,
+    activity VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
