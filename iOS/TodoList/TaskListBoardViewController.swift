21318e956c8b5972ba25adcd441fc373557daa09
sungju-kim <wnwn0719@gmail.com>
2022-04-05 18:02:46 +0900

    [#1] feat: cardCell view 구현

--- iOS/TodoList/TaskListBoardViewController.swift
+++ iOS/TodoList/TaskListBoardViewController.swift
@@ -1,18 +1,27 @@
 import UIKit
 
 class TaskListBoardViewController: UIViewController {
-    
+    var foo: [UIColor] = [.red,.gray,.green,.brown]
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpStackView()
     }
-    
+
     func setUpStackView() {
         var taskViewList: [TaskListView] = []
         
         for _ in 0...2 {
-            let taskListView: TaskListView = TaskListView()
-            taskViewList.append(taskListView)
+            if let taskListView = Bundle.main.loadNibNamed("TaskListView", owner: nil, options: nil)?.first as? TaskListView {
+                taskListView.tableView.delegate = self
+                taskListView.tableView.dataSource = self
+                taskListView.tableView.rowHeight = UITableView.automaticDimension
+                taskListView.tableView.estimatedRowHeight = 400
+                let nibName = UINib(nibName: "TaskCardViewCell", bundle: nil)
+                taskListView.tableView.register(nibName, forCellReuseIdentifier: "cardCell")
+                taskListView.frame = self.view.bounds
+                taskViewList.append(taskListView)
+            }
+            
         }
         
         let stackView = UIStackView(arrangedSubviews: taskViewList)
@@ -31,10 +40,24 @@ class TaskListBoardViewController: UIViewController {
             stackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10),
             stackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
             stackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            stackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 10)
+            stackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
         ])
 
     }
 
 
+}
+
+
+extension TaskListBoardViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.foo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath) as! TaskCardViewCell
+        return cell
+    }
+    
 }
--- iOS/TodoList/TaskListView.swift
+++ iOS/TodoList/TaskListView.swift
@@ -1,20 +1,12 @@
 import UIKit
 
 class TaskListView: UIView {
-    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var taskCountBadge: UILabel!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let className = String(describing: type(of: self))
-        let nib = UINib(nibName: className, bundle: Bundle.main)
-        
-        guard let xibView = nib.instantiate(withOwner: self, options: nil).first as? UIView else { return }
-        xibView.frame = self.bounds
-        xibView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.addSubview(xibView)
     }
     
     required init?(coder: NSCoder) {
@@ -24,3 +16,4 @@ class TaskListView: UIView {
     @IBAction func addTaskButtonTouched(_ sender: UIButton) {
     }
 }
+
