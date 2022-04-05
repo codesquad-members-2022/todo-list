f368a6a1acc8f7ddecc27f7f7ee7ada374d2f612
Millie <jay.lee.2280@gmail.com>
2022-04-05 11:47:56 +0900

    Add: webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index_bundles.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  plugins: [new HtmlWebpackPlugin({ filename: 'index.html', template: './index.html' })],
+};
