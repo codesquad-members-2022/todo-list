43cd108415a91cc6e984678cc24cc6b885bb83f0
jiwoo <zbqmgldjfh@gmail.com>
2022-04-05 11:42:53 +0900

    feat(WorkRepository): WorkRepository Interface 정의

---
+++ BE/src/main/java/codesquad/todo/domain/work/WorkRepository.java
@@ -0,0 +1,13 @@
+package codesquad.todo.domain.work;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface WorkRepository {
+    Work save (Work work);
+    Optional<Work> findById(Long id);
+    void update(Long id, Work updateWork);
+    void delete(Long id);
+    List<Work> findAll();
+    List<Work> findByStatus(WorkStatus workStatus);
+}
