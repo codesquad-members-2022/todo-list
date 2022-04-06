641e5ddc1b44d450a3898ca68cd04cbc80c5f5e8
mogooee <92701121+mogooee@users.noreply.github.com>
2022-04-06 15:33:16 +0900

    메뉴 - 우측 히든 레이어 생성, 애니메이션 구현 (#13)
    * feat: sidebar 메뉴 버튼
    사이드바 메뉴 버튼 클릭시 사이드바 숨김,보임 기능 구현
    Related to: #10
    
    * design: sidebar 애니메이션
    왼쪽방향으로 나타나고 오른쪽 방향으로 숨겨지는 애니메이션 구현
    Related to: #10
    
    * feat: action분리
    action에 따라 sidebar 리스트의 comment를 다르게 출력함
    Related to: #10
    
    * feat: timestamp 기능 구현
    현재 시간을 기준으로 투두리스트 작성 및 변경 시간을 보여주는 timestamp 구현
    Related to: #10
    
    * feat: 을/를, 으로/로 구분
    단어의 마지막 글자 종성에 따라 모음을 다르게 렌더하는 기능 구현
    Related to: #10

--- client/src/js/sidebar/view.js
+++ client/src/js/sidebar/view.js
@@ -2,6 +2,65 @@ import { $ } from "../utils/utils.js";
 import { toggleActivation } from "../store/model.js";
 import { sidebarData } from "../../../../server/data/sidebar.js";
 
+const getFinalConsonant = (object) => {
+  const finalWord = object.length - 1;
+  const fianlCharCode = object.charCodeAt(finalWord);
+  const finalConsonantCode = (fianlCharCode - 44032) % 28;
+  return finalConsonantCode;
+};
+
+const hasFinalConsonant = (object) => {
+  const finalConsonantCode = getFinalConsonant(object);
+  return finalConsonantCode !== 0;
+};
+
+const appendEulReul = (object) => {
+  return hasFinalConsonant(object) ? "을" : "를";
+};
+
+const appenRoEro = (object) => {
+  const rieul = 8;
+  return hasFinalConsonant(object) //
+    ? getFinalConsonant(object) === rieul
+      ? "로"
+      : "으로"
+    : "로";
+};
+
+const calcTimeForToday = (writeTimeValue) => {
+  const today = new Date();
+  const writeTime = new Date(writeTimeValue);
+  const timeDifference = today.getTime() - writeTime.getTime();
+
+  const minuteDifference = Math.floor(timeDifference / 1000 / 60);
+  if (minuteDifference < 1) return "방금전";
+  else if (minuteDifference < 60) return `${minuteDifference}분전`;
+
+  const HourDifference = Math.floor(minuteDifference / 60);
+  if (HourDifference < 24) return `${HourDifference}시간전`;
+
+  const DayDifference = Math.floor(HourDifference / 60 / 24);
+  if (DayDifference < 365) return `${DayDifference}일전`;
+};
+
+const identifyCategory = (activity) => {
+  switch (activity.action) {
+    case "등록":
+      return `${activity.category[0]}에 ${activity.title}${appendEulReul(activity.title)} 
+      ${activity.action}하였습니다.`;
+    case "이동":
+      return `${activity.title}${appendEulReul(activity.title)} ${activity.category[0]}에서 
+      ${activity.category[1]}${appenRoEro(activity.category[1])} ${activity.action}하였습니다.`;
+    case "삭제":
+      return `${activity.category}에서 ${activity.title}${appendEulReul(activity.title)} 
+      ${activity.action}하였습니다.`;
+    case "변경":
+      return `${activity.category}에서 ${activity.title}이 ${action}되었습니다.`;
+    default:
+      return "동작이 잘못 되었습니다.";
+  }
+};
+
 const createHTML = () => {
   const sidebarList = sidebarData.reduce(
     (acc, cur) =>
