87fe94d44dcbdf1acd450ec6b48939dc7b9aa6a9
ldldz <qkrqudrjs4350@gmail.com>
2022-04-06 16:23:06 +0900

    [#8]Chore: Webpack 환경설정 실습 완료

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,38 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 __webpack_base_uri__ = 'http://localhost:5501';
 
-module.exports = {
-    mode: "development",
+const config = {
     entry: "./src/app.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
-    },
-    module: {
+    }, module: {
         rules: [
             {
                 test: /\.js$/,
                 use: 'babel-loader',
                 exclude: /node_modules/
-            }
+            },
+            {
+              test:/\.css$/,
+              use:['style-loader','css-loader'],
+            },
         ]
     },
     plugins: [new HtmlWebpackPlugin(
         {
-            template: 'src/app.html'
+            template: 'src/app.html',
         }
     )]
+}
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.mode = 'development';
+    config.devtool = 'inline-source-map';
+  }
+  else {
+    config.mode = 'production';
+  }
+  return config;
 }
