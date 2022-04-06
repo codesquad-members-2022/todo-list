eaa58350e78e3c084db52b09357571e39cc84559
soralee <sora2821@gmail.com>
2022-04-06 20:37:24 +0900

    Build: sourcemap 기능 추가

--- build/dist/bundle.js
+++ build/dist/bundle.js
@@ -1,7 +1,7 @@
 /*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * ATTENTION: An "eval-source-map" devtool has been used.
  * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
@@ -15,7 +15,7 @@
   \*******************************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://build/./src/javascript/app.js?");
+eval("function _readOnlyError(name) { throw new TypeError(\"\\\"\" + name + \"\\\" is read-only\"); }\n\n// webpack test entry 파일\nvar abc = 1;\n2, _readOnlyError(\"abc\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9idWlsZC8uL3NyYy9qYXZhc2NyaXB0L2FwcC5qcz84YTk0Il0sIm5hbWVzIjpbImFiYyJdLCJtYXBwaW5ncyI6Ijs7QUFBQTtBQUNBLElBQU1BLEdBQUcsR0FBRyxDQUFaO0FBQ00sQ0FBTiIsInNvdXJjZXNDb250ZW50IjpbIi8vIHdlYnBhY2sgdGVzdCBlbnRyeSDtjIzsnbxcclxuY29uc3QgYWJjID0gMTtcclxuYWJjID0gMjtcclxuIl0sImZpbGUiOiIuL3NyYy9qYXZhc2NyaXB0L2FwcC5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/javascript/app.js\n");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("\n\n//# sourceURL=webpack://build/./src/javascript/app.js?");
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/javascript/app.js"]();
 /******/ 	
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  devtool: "eval-source-map",
   module: {
     rules: [
       {
