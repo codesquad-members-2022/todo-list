de316951490e5a307e1477785504c64d8d361cab
OceanShape <oceanshape700@gmail.com>
2022-04-06 17:39:31 +0900

    [#6] Feat: CardListTableViewCell에 항목 추가

--- ToDoList/ToDoList/CardListTableViewCell.swift
+++ ToDoList/ToDoList/CardListTableViewCell.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class CardListTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var body: UILabel!
+    
+    @IBOutlet weak var caption: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
