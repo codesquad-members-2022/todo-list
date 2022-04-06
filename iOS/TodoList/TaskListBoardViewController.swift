c96e6a9c040cadf26d36eeec9bd1dfb958384d54
sungju-kim <wnwn0719@gmail.com>
2022-04-06 14:15:33 +0900

    refacor: taskListView 구조 개선

--- iOS/TodoList/TaskListBoardViewController.swift
+++ iOS/TodoList/TaskListBoardViewController.swift
@@ -6,46 +6,31 @@ class TaskListBoardViewController: UIViewController {
         super.viewDidLoad()
         setUpStackView()
     }
-
+    
     func setUpStackView() {
-        var taskViewList: [TaskListView] = []
-        
-        for _ in 0...2 {
-            if let taskListView = Bundle.main.loadNibNamed("TaskListView", owner: nil, options: nil)?.first as? TaskListView {
-                taskListView.tableView.delegate = self
-                taskListView.tableView.dataSource = self
-                taskListView.tableView.rowHeight = UITableView.automaticDimension
-                taskListView.tableView.estimatedRowHeight = 400
-                let nibName = UINib(nibName: "TaskCardViewCell", bundle: nil)
-                taskListView.tableView.register(nibName, forCellReuseIdentifier: "cardCell")
-                taskListView.frame = self.view.bounds
-                taskViewList.append(taskListView)
-            }
+        if let taskListView = Bundle.main.loadNibNamed("TaskListView", owner: nil, options: nil)?.first as? TaskListView {
+            taskListView.tableView.delegate = self
+            taskListView.tableView.dataSource = self
+            let nibName = UINib(nibName: "TaskCardViewCell", bundle: nil)
+            taskListView.tableView.register(nibName, forCellReuseIdentifier: "cardCell")
+            self.view.addSubview(taskListView)
+            taskListView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                taskListView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10),
+                taskListView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+                taskListView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+                taskListView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+            ])
         }
-        
-        let stackView = UIStackView(arrangedSubviews: taskViewList)
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        stackView.axis = .horizontal
-        stackView.alignment = .fill
-        stackView.distribution = .fillEqually
-        stackView.spacing = 22
-        
-        
-        view.addSubview(stackView)
-
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            stackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            stackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            stackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
-        ])
-
     }
-
-
+    
+    func add() {
+        self.foo.append(UIColor.brown)
+    }
 }
+           
+
+
 
 
 extension TaskListBoardViewController: UITableViewDelegate, UITableViewDataSource {
