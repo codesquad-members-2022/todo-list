8d888dedbc22d2391728ad74824b12532694340f
sanghee <anshee97@naver.com>
2022-04-06 16:39:54 +0900

    [#9] Design: SideView 디자인 수정

---
+++ todo-list-ios/TODOList/TODOList/Extension/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, alpha: Int = 0xFF) {
+        self.init(red: CGFloat(red) / 255.0,
+                  green: CGFloat(green) / 255.0,
+                  blue: CGFloat(blue) / 255.0,
+                  alpha: CGFloat(alpha) / 255.0)
+    }
+    
+    convenience init(hex: String) {
+        let value = Array(hex)
+        let hexRed = "\(value[1])\(value[2])"
+        let hexGreen = "\(value[3])\(value[4])"
+        let hexBlue = "\(value[5])\(value[6])"
+        self.init(red: Int(hexRed, radix: 16)!,
+                  green: Int(hexGreen, radix: 16)!,
+                  blue: Int(hexBlue, radix: 16)!)
+    }
+}
--- todo-list-ios/TODOList/TODOList/Views/HistoryStackView.swift
+++ todo-list-ios/TODOList/TODOList/Views/HistoryStackView.swift
@@ -5,29 +5,14 @@ class HistoryStackView: UIStackView {
     var nameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont(name: "Apple SD Gothic Neo Bold", size: 30)
-        label.text = "asfasdfa"
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 16)
         return label
     }()
     
     var contentLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont(name: "Apple SD Gothic Neo Bold", size: 15)
-        label.text = """
-어둠 속에 파고든
-내 안에 날 가두던
-시간은 이미 지나고
-It fades away
-하염없이 떠돌다
-낯선 벽에 부딪혀 버린
-Oh 이제 난 어디로
-Please hold my hand
-        하나둘씩 열어
-        감춰왔던 나를 깨워
-        잊으려 했던 결국엔 늘
-        잊을 수 없었던 꿈들
-"""
+        label.font = UIFont(name: "Apple SD Gothic Neo Bold", size: 16)
         label.numberOfLines = 0
         return label
     }()
@@ -35,8 +20,8 @@ Please hold my hand
     var timeLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont(name: "Apple SD Gothic Neo Bold", size: 50)
-        label.text = "time"
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 14)
+        label.textColor = UIColor(hex: "#828282")
         return label
     }()
     
