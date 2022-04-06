dd6c58801d4c11b56327c6831d70c72364615aff
shncder <91953080+shncder@users.noreply.github.com>
2022-04-06 16:55:43 +0900

    feat: appbar 및 navigation drawer 구현 (#24)

--- Android/app/src/main/java/com/example/todo_list/MainActivity.kt
+++ Android/app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -2,14 +2,34 @@ package com.example.todo_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageButton
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.todo_list.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var navigationView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.main_layout)
+
+        val btnmenu = findViewById<ImageButton>(R.id.btn_menu)
+        btnmenu.setOnClickListener { drawerLayout.openDrawer(GravityCompat.END) }
+
+        navigationView = findViewById(R.id.navi_view)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return false
     }
 }
