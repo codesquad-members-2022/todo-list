354c181d70131172a0864873336f22fb06dbdda7
OceanShape <oceanshape700@gmail.com>
2022-04-06 16:48:44 +0900

    [#6] Feat: CardListTableViewCell 추가

---
+++ ToDoList/ToDoList/CardListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardListTableViewCell.swift
+//  ToDoList
+//
+//  Created by 허태양 on 2022/04/06.
+//
+
+import UIKit
+
+class CardListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
