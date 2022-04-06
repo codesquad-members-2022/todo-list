a045d4ef698e383daf3fd2e14586d3fba8b82a11
ak2j38 <k2j38@naver.com>
2022-04-06 16:18:55 +0900

    fix: JSON 매핑 에러 해결
    1. 기본생성자 추가로 JSON 매핑 에러 해결
    
    end

--- BE/src/main/java/com/team26/todolist/dto/request/CardDeleteRequest.java
+++ BE/src/main/java/com/team26/todolist/dto/request/CardDeleteRequest.java
@@ -1,10 +1,8 @@
 package com.team26.todolist.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter @Setter
 public class CardDeleteRequest {
     private Long id;
--- BE/src/main/java/com/team26/todolist/dto/request/CardMoveRequest.java
+++ BE/src/main/java/com/team26/todolist/dto/request/CardMoveRequest.java
@@ -1,5 +1,10 @@
 package com.team26.todolist.dto.request;
 
-public class CardMoveRequest {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class CardMoveRequest {
+    private Long id;
 }
