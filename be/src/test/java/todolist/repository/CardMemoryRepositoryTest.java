f60724fe0340cf60fd119198e7851f2cab97d8ca
함석호 <kalejuice02@gmail.com>
2022-04-06 16:13:09 +0900

    test : CardMemoryRepository의 save 메서드에 대한 테스트코드 작성

---
+++ be/src/test/java/todolist/repository/CardMemoryRepositoryTest.java
@@ -0,0 +1,28 @@
+package todolist.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import todolist.domain.Card;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CardMemoryRepositoryTest {
+
+    private TodoRepository<Card> repository = new CardMemoryRepository();
+
+    @Test
+    @DisplayName("Card 객체를 저장한다.")
+    void save() {
+        Card card = new Card("해야할 일", "제목", "내용");
+
+        Card result = repository.save(card);
+
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getSection()).isEqualTo("해야할 일");
+        assertThat(result.getTitle()).isEqualTo("제목");
+        assertThat(result.getContent()).isEqualTo("내용");
+    }
+
+
+
+}
