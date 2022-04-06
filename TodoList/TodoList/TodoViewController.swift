179bb03b6822584b8a64e46f27ae4218382b9087
bibi <bibi6666667@kakao.com>
2022-04-06 16:58:19 +0900

    feat : CardBoardViewController와 TodoViewController 생성 및 CardBoardView, TodoView 생성 #9

---
+++ TodoList/TodoList/TodoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TodoViewController.swift
+//  TodoList
+//
+//  Created by Bibi on 2022/04/06.
+//
+
+import UIKit
+
+class TodoViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.gray
+        // Do any additional setup after loading the view.
+    }
+
+}
