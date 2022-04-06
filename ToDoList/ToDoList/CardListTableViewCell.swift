723c6844ffc8426cf96a4cb6e9b43ef5a5ecd640
OceanShape <oceanshape700@gmail.com>
2022-04-06 18:07:27 +0900

    [#6] Style: CardListTableViewCell 주석 제거 및 코드 정리

--- ToDoList/ToDoList/CardListTableViewCell.swift
+++ ToDoList/ToDoList/CardListTableViewCell.swift
@@ -1,29 +1,17 @@
-//
-//  CardListTableViewCell.swift
-//  ToDoList
-//
-//  Created by 허태양 on 2022/04/06.
-//
-
 import UIKit
 
 class CardListTableViewCell: UITableViewCell {
 
     @IBOutlet weak var title: UILabel!
-    
     @IBOutlet weak var body: UILabel!
-    
     @IBOutlet weak var caption: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
