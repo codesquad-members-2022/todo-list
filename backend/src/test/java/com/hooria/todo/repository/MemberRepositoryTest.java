b7329a35a03dcc8764b526d120c3983670e553e5
naneun <rmtlrksdp@naver.com>
2022-04-06 15:47:24 +0900

    ✅ 테스트 코드 TODO 처리 및 개선

--- backend/src/test/java/com/hooria/todo/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/hooria/todo/repository/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.hooria.todo.repository;
 
 import com.hooria.todo.domain.Member;
-import org.assertj.core.api.AssertionsForInterfaceTypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 @JdbcTest
 class MemberRepositoryTest {
@@ -35,11 +33,19 @@ class MemberRepositoryTest {
         Member result = repository.insert(member);
 
         // then
-        assertThat(result).usingRecursiveComparison().isEqualTo(member);
+        assertThat(result.getId()).isEqualTo(member.getId());
+        assertThat(result.getUserId()).isEqualTo(member.getUserId());
+        assertThat(result.getPassword()).isEqualTo(member.getPassword());
+        assertThat(result.getName()).isEqualTo(member.getName());
+
+        /*
+            assertThat(result).usingRecursiveComparison().isEqualTo(member);
+            객체 비교 시 usingRecursiveComparison() 메서드를 사용하는 검증도 괜찮은 지 궁금합니다! - @Riako
+        */
     }
 
     @Test
-    @DisplayName("저장소에서 전체 사용자 목록을 읽어 반환한다.")
+    @DisplayName("저장소에 저장되어 있는 전체 사용자 목록(List<Member)을 읽어 반환한다.")
     void findAllSuccess() {
 
         // given
@@ -53,12 +59,27 @@ class MemberRepositoryTest {
         List<Member> result = repository.findAll();
 
         // then
-        // TODO - usingRecursiveComparison() 사용하지 않고 요소 별로 비교하기
-        AssertionsForInterfaceTypes.assertThat(result).usingRecursiveComparison().isEqualTo(members);
+        assertThat(result.size()).isEqualTo(members.size());
+
+        int size = result.size();
+        for (int index = 0; index < size; ++index) {
+            Member member1 = result.get(index);
+            Member member2 = members.get(index);
+
+            assertThat(member1.getId()).isEqualTo(member2.getId());
+            assertThat(member1.getUserId()).isEqualTo(member2.getUserId());
+            assertThat(member1.getPassword()).isEqualTo(member2.getPassword());
+            assertThat(member1.getName()).isEqualTo(member2.getName());
+        }
+
+        /*
+            assertThat(result).usingRecursiveComparison().isEqualTo(members);
+            Collection 의 경우에도 usingRecursiveComparison() 메서드를 사용하는 검증도 괜찮은 지 궁금합니다! - @Riako
+        */
     }
 
     @Test
-    @DisplayName("인자로 주어진 'userId' 를 가진 사용자를 저장소에서 찾아 반환한다.")
+    @DisplayName("인자로 주어진 'userId'를 가진 사용자를 저장소에서 찾아 반환한다.")
     void findByIdSuccess() {
         // given
         String userId = "userId1";
@@ -70,10 +91,11 @@ class MemberRepositoryTest {
         // then
         assertThat(result).isNotEmpty();
 
-        // TODO
-        assertThat(result.get().getId()).isEqualTo(member.getId());
-        assertThat(result.get().getUserId()).isEqualTo(member.getUserId());
-        assertThat(result.get().getName()).isEqualTo(member.getName());
-        assertThat(result.get().getPassword()).isEqualTo(member.getPassword());
+        Member resultMember = result.get();
+
+        assertThat(resultMember.getId()).isEqualTo(member.getId());
+        assertThat(resultMember.getUserId()).isEqualTo(member.getUserId());
+        assertThat(resultMember.getName()).isEqualTo(member.getName());
+        assertThat(resultMember.getPassword()).isEqualTo(member.getPassword());
     }
 }
