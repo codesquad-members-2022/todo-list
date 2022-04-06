bb4d922503d5f51b0d081bfcc2f74d7df68804b3
P_SeoB <akdlsjtm21@naver.com>
2022-04-06 15:13:45 +0900

    [Feat] Add Newwork Class
    contents
    - Network작업을 위한 class 제작
    - Generic 이용 get post 메서드 생성
    - Error 처리 Enum 생성
    - 하드코딩 방지 HttpMethod Struct생성
    
    Todo
    - BE API와 연동 테스트

---
+++ To-Do_List/To-Do_List/Network/HttpMethod.swift
@@ -0,0 +1,12 @@
+//
+//  HttpMethod.swift
+//  To-Do_List
+//
+//  Created by 박진섭 on 2022/04/06.
+//
+
+
+struct HttpMethod {
+    static let get = "GET"
+    static let post = "POST"
+}
---
+++ To-Do_List/To-Do_List/Network/NetworkManager.swift
@@ -0,0 +1,91 @@
+
+import Foundation
+
+final class NetworkManager {
+    private var signUpURL = URL(string:"https://api.codesquad.kr/signup")
+    private var config = URLSessionConfiguration.default
+    private var session = URLSession(configuration:.default)
+    
+    func getRequest<T:Decodable>(completion:@escaping (Result<T,NetworkError>) -> Void) {
+        //is URL available?
+        guard let signUpURL = signUpURL else { return }
+        
+        var urlRequest = URLRequest(url: signUpURL)
+        urlRequest.httpMethod = HttpMethod.get
+        
+        let dataTask = session.dataTask(with: urlRequest) { [weak self] data, response, _ in
+            guard let self = self else { return }
+            guard let data = data, self.isResponseClear(response: response) == true
+            else {
+                completion(.failure(.responseError))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let data = try decoder.decode(T.self, from: data)
+                completion(.success(data))
+                }
+            catch {
+                completion(.failure(.decodingError))
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+    func postRequest<T:Decodable,U:Encodable>(postBody:U, completion: @escaping((Result<T,NetworkError>) -> Void)) {
+        //is URL available?
+        guard let signUpURL = signUpURL else { return }
+        var urlRequest = URLRequest(url: signUpURL)
+        urlRequest.httpMethod = HttpMethod.post
+        
+        do {
+            urlRequest.httpBody = try JSONEncoder().encode(postBody)
+            
+            //is response clear?
+            let dataTask = session.dataTask(with: urlRequest) { [weak self] data, response, _ in
+                    guard let self = self else { return }
+                    guard let data = data, self.isResponseClear(response: response) == true
+                    else {
+                    completion(.failure(.responseError))
+                    return
+                }
+            
+                do {
+                    //then decode
+                    let messageData = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(messageData))
+                }
+                    //else failure
+                catch {
+                    completion(.failure(.decodingError))
+                    
+                }
+            }
+            dataTask.resume()
+        }
+        
+        catch {
+            completion(.failure(.encodingError))
+        }
+        
+    }
+    
+    func session(_ urlSession: URLSession) {
+        self.session = urlSession
+    }
+    
+    func url(_ url:URL) {
+        self.signUpURL = url
+    }
+    
+    private func isResponseClear(response:URLResponse?) -> Bool {
+        guard let httpResponse = response as? HTTPURLResponse else { return false }
+        if (200..<300) ~= httpResponse.statusCode {
+            return true
+        } else {
+            return false
+        }
+    }
+}
