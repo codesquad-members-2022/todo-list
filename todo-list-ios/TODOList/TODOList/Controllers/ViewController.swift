4343e957cc9d1aa5939beb0c5c923a846eea4b04
sanghee <anshee97@naver.com>
2022-04-06 15:30:22 +0900

    [#9] Feat: 우측 메뉴 버튼 클릭 시, 사이드뷰 (히스토리 목록)가 나타나도록 구현

--- todo-list-ios/TODOList/TODOList/Controllers/ViewController.swift
+++ todo-list-ios/TODOList/TODOList/Controllers/ViewController.swift
@@ -2,6 +2,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    lazy var sideMenuButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setBackgroundImage(UIImage(systemName: "list.bullet.circle.fill"), for: .normal)
+        button.addTarget(self, action: #selector(sideMenuButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
     private let sideView: SideView = {
         let view = SideView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -17,15 +25,41 @@ class ViewController: UIViewController {
         setLayout()
     }
     
+    /// 초기 뷰 설정. 초기 뷰는 sideMenuButton이 보여지는 상태.
     private func addViews() {
-        self.view.addSubview(sideView)
+        self.view.addSubview(sideMenuButton)
     }
     
+    /// sideMenuButton Layout 설정
     private func setLayout(){
+        sideMenuButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        sideMenuButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        sideMenuButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        sideMenuButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
+    /// sideMenuButton 클릭 시 SideView를 보여준다. 
+    private func showSideView() {
+        self.sideMenuButton.removeFromSuperview()
+        self.view.addSubview(sideView)
+        
         sideView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
         sideView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
         sideView.widthAnchor.constraint(equalToConstant: 300).isActive = true
         sideView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+    }
+    
+    /// SideMenu에 있는 닫기 버튼 클릭 시, sideView를 제거하고 SideMenuButton을 보여줌.
+    private func hideSideView() {
+        UIView.transition(with: self.sideView, duration: 0.25) {
+            self.sideView.removeFromSuperview()
+        }
+        
+        self.view.addSubview(sideMenuButton)
+    }
+    
+    @objc func sideMenuButtonDidTap() {
+        showSideView()
     }
 }
 
