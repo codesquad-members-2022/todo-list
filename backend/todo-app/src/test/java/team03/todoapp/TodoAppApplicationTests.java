966c6275f0aaea8f6fb59ed3e6353e0659cd478f
nori <nohriter@gmail.com>
2022-04-06 15:31:15 +0900

    Build: 백엔드 프로젝트 초기 설정
    - Java 11 + Spring Boot(2.6.6) + Gradle + Spring Web + Lombok + Jdbc + mysqlDriver
    - 불필요한 파일 삭제(help.md)
    - gitignore 윈도우, 맥, 자바, 인텔리제이, 그레이들 추가

---
+++ backend/todo-app/src/test/java/team03/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package team03.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
