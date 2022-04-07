cedffe463b5fdd753a809c4be4e1d7563eeed5c3
Jason <ggj00125@gmail.com>
2022-04-08 07:35:12 +0900

    :sparkles: LogTableViewCell에 username,title,timestamp 추가

--- iOS/todo-list/todo-list/View/LogTableViewCell.swift
+++ iOS/todo-list/todo-list/View/LogTableViewCell.swift
@@ -9,5 +9,48 @@ import Foundation
 import UIKit
 
 class LogTableViewCell: UITableViewCell {
+    private var log = Log(userName: "@sam", title: "HTML/CSS공부하기")
+    
+    // userName, TimeStamp, 활동내역 모두 같은 Font 사용
+    enum common {
+        static let commonFont = UIFont(name: "NotoSansKR-Regular", size: 14)
+        static let commonAttribute: [NSMutableAttributedString.Key: Any] = [
+            .foregroundColor : UIColor.black,
+            .font : commonFont ?? UIFont()
+        ]
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    //MARK: Username Method
+    func makeUserName(userName: String) -> NSAttributedString {
+        return NSAttributedString(string: log.userName, attributes: common.commonAttribute)
+    }
+    
+    //MARK: Title Method
+    func makeTitle(title: String) -> NSAttributedString {
+        return NSAttributedString(string: log.title, attributes: common.commonAttribute)
+    }
+    
+    //MARK: Text & SubText Method
+    func makeText() {
+        
+    }
+    
+    //MARK: TimeStamp Method
+    func makeTimeStamp(timeStamp: Int) -> NSAttributedString {
+        let time = log.showTimeStamp()
+        let timeStr = "\(time)분전"
+        
+        return NSAttributedString(string: timeStr, attributes: common.commonAttribute)
+    }
     
 }
+
+
