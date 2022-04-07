e86bea3f5a9d67eecef72da21f5cf4ed5c208747
hwicode <khc3796@naver.com>
2022-04-08 02:15:57 +0900

    feat : CardRepository 클래스에 모든 카드를 조회하는 기능 추가 - #4

--- BackEnd/src/main/java/todo/list/repository/CardRepository.java
+++ BackEnd/src/main/java/todo/list/repository/CardRepository.java
@@ -1,8 +1,13 @@
 package todo.list.repository;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import todo.list.domain.Author;
 import todo.list.domain.Card;
+import todo.list.domain.CardStatus;
+
+import java.util.List;
 
 @Repository
 public class CardRepository {
@@ -15,5 +20,20 @@ public class CardRepository {
 
     public void save(Card card) {
 
+    }
+
+    public List<Card> findAll() {
+        return jdbcTemplate.query("Select id, title, contents, card_status, author from card order by create_date desc", cardsRowMapper());
+    }
+
+    private RowMapper<Card> cardsRowMapper() {
+        return (rs, rowNum) -> {
+            Long id = rs.getLong("id");
+            String title = rs.getString("title");
+            String contents = rs.getString("contents");
+            CardStatus cardStatus = CardStatus.valueOf(rs.getString("card_status"));
+            Author author = Author.valueOf(rs.getString("author"));
+            return new Card(id, title, contents, cardStatus, author);
+        };
     }
 }
