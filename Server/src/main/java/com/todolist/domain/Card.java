f7eaa412835e8357c59a337b381051e64629b011
leejohy-0223 <leejohy@naver.com>
2022-04-07 22:37:12 +0900

    feat: Domain 모델 fix (#13)
    - User : 사용자 추가
    - Card
      - 빌더 패턴 추가 및 필요없는 컬럼을 제거
      - 네이밍 컨벤션에 맞게 컬럼명 수정

--- Server/src/main/java/com/todolist/domain/Card.java
+++ Server/src/main/java/com/todolist/domain/Card.java
@@ -2,47 +2,18 @@ package com.todolist.domain;
 
 import java.time.LocalDateTime;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class Card {
 
     private Integer cardId;
+    private Integer userId;
     private String cardTitle;
     private String cardContent;
-    private LocalDateTime addDateTime;
-    private Integer internalOrder;
     private String boardName;
-    private boolean remove;
-
-    public Card(Integer cardId, String cardTitle, String cardContent,
-        LocalDateTime addDateTime, Integer internalOrder, String boardName) {
-        this.cardId = cardId;
-        this.cardTitle = cardTitle;
-        this.cardContent = cardContent;
-        this.addDateTime = addDateTime;
-        this.internalOrder = internalOrder;
-        this.boardName = boardName;
-    }
-
-    public Integer getCardId() {
-        return cardId;
-    }
-
-    public String getCardTitle() {
-        return cardTitle;
-    }
-
-    public String getCardContent() {
-        return cardContent;
-    }
-
-    public LocalDateTime getAddDateTime() {
-        return addDateTime;
-    }
-
-    public Integer getInternalOrder() {
-        return internalOrder;
-    }
-
-    public String getBoardName() {
-        return boardName;
-    }
+    private LocalDateTime createdTime;
+    private boolean removed;
 }
--- Server/src/main/resources/data.sql
+++ Server/src/main/resources/data.sql
@@ -1,19 +1,7 @@
-INSERT INTO `user` (`userId`, `password`) VALUES
+insert into `user` (`userId`, `password`) VALUES
 ('lucid', '0000'),
 ('tany', '1111'),
 ('rosa', '2222'),
 ('beck', '3333')
-
-INSERT INTO card (`userId`, `cardTitle`, `cardContent`, `boardName`, `internalOrder`, `addDateTime`) VALUES
-('lucid', 'title1', 'content1', 'todo', '1', '2022-04-06 22:00:00.00'),
-('lucid', 'title2', 'content2', 'doing', '1', '2022-04-06 22:00:00.00'),
-('lucid', 'title9', 'content9', 'done', '1', '2022-04-06 22:00:00.00'),
-('beck', 'title3', 'content3', 'doing', '1', '2022-04-06 22:00:00.00'),
-('tany', 'title4', 'content4', 'doing', '1', '2022-04-06 22:00:00.00'),
-('tany', 'title5', 'content5', 'doing', '1', '2022-04-06 22:00:00.00'),
-('rosa', 'title6', 'content6', 'done', '1', '2022-04-06 22:00:00.00'),
-('rosa', 'title7', 'content7', 'done', '1', '2022-04-06 22:00:00.00'),
-('beck', 'title8', 'content8', 'done', '1', '2022-04-06 22:00:00.00')
-
 
 
