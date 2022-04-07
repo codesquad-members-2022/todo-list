a7900ed54563731d19c0d40337ebdfddda19745f
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-04-07 17:52:10 +0900

    feat: CardService 클래스 생성
    - findAll() 메서드 선언부 구현

---
+++ backend/src/main/java/com/example/todo/service/CardService.java
@@ -0,0 +1,11 @@
+package com.example.todo.service;
+
+import com.example.todo.controller.card.CardDto;
+import java.util.List;
+
+public class CardService {
+
+	public List<CardDto> findAll() {
+
+	}
+}
