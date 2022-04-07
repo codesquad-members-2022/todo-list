42ef0cb2107ff2e1ddeedd399b7b0b45df64f85e
decay95 <decay95@naver.com>
2022-04-07 21:02:33 +0900

    [remove] 불필요한 데이터 삭제

--- FE/src/JS/todo/app.js
+++ FE/src/JS/todo/app.js
@@ -5,9 +5,13 @@ import Drag from '../drag-drop/drag-drop.js';
 import { fetchRequest } from '../utility/fetchRequest.js';
 
 export async function initTodo() {
+<<<<<<< HEAD
   const workListData = await fetchRequest(
     '/todo-list/FE/src/js/mok-data/mokData.json'
   );
+=======
+  const workListData = await fetchRequest('../src/js/mok-data/mokData.json');
+>>>>>>> ce69d4f ([remove] 불필요한 데이터 삭제)
   const model = new Model(workListData);
   const view = new View();
   new Controller(model, view);
