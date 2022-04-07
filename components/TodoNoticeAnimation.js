237cb839a2f5be359fa8964ded22e1ac61e4f603
Muffin <jinlog9@gmail.com>
2022-04-08 03:40:43 +0900

    refactor: 메뉴 animation 수정

--- components/TodoNoticeAnimation.js
+++ components/TodoNoticeAnimation.js
@@ -4,15 +4,16 @@ export default function TodoNoticeAnimation() {
   this.closeBtnElement = document.querySelector('.alarm__closeBtn');
 
   this.menuAnimation = () => {
-    this.alarmElement.classList.toggle('no-display');
     this.headerMenuElement.classList.toggle('no-display');
   };
 
   this.headerMenuElement.addEventListener('click', () => {
+    this.alarmElement.style.animation = 'fadeInLeft 1s forwards';
     this.menuAnimation();
   });
 
   this.closeBtnElement.addEventListener('click', () => {
+    this.alarmElement.style.animation = 'fadeInRight 1s';
     this.menuAnimation();
   });
 }
