053afb01b2369d8952b5b7458c566cf031f3c2ac
hwicode <khc3796@naver.com>
2022-04-08 02:28:52 +0900

    feat : CardService 클래스 생성 - #4
    - 도메인 객체인 Card를 클라이언트의 요구사항에 맞춰 변화시키기 위해 서비스 클래스를 생성하였다.

--- BackEnd/src/main/java/todo/list/service/CardService.java
+++ BackEnd/src/main/java/todo/list/service/CardService.java
@@ -2,7 +2,11 @@ package todo.list.service;
 
 import org.springframework.stereotype.Service;
 import todo.list.domain.Card;
+import todo.list.domain.CardStatus;
 import todo.list.repository.CardRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CardService {
@@ -17,4 +21,25 @@ public class CardService {
 //        Card card = cardSaveDto.toEntity();
 //        cardRepository.save();
     }
+    
+    public CardResponse findCollections() {
+        List<Card> cards = cardRepository.findAll();
+
+        CardCollectionDto todoCollection = makeStatusCollection(cards, CardStatus.from("TODO"));
+        CardCollectionDto inProgressCollection = makeStatusCollection(cards, CardStatus.from("IN_PROGRESS"));
+        CardCollectionDto doneCollection = makeStatusCollection(cards, CardStatus.from("DONE"));
+
+        return new CardResponse(todoCollection, inProgressCollection, doneCollection);
+    }
+
+    private CardCollectionDto makeStatusCollection(List<Card> cards, CardStatus cardStatus) {
+        List<CardDto> collection = cards.stream()
+                .filter(card -> card.equalsStatus(cardStatus))
+                .map(card -> new CardDto(card))
+                .collect(Collectors.toList());
+
+        return new CardCollectionDto(collection);
+    }
+
+
 }
