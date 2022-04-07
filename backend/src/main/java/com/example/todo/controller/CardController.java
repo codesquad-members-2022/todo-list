709e69b178cbf3d2d5928ba1f08c06b94b34991b
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-04-07 18:35:21 +0900

    refactor: CardController 타입 수정
    - CardDto를 아직 구현하지 않아서 Card로 변경해주었습니다.

--- backend/src/main/java/com/example/todo/controller/CardController.java
+++ backend/src/main/java/com/example/todo/controller/CardController.java
@@ -1,6 +1,7 @@
 package com.example.todo.controller;
 
 import com.example.todo.controller.card.CardDto;
+import com.example.todo.domain.Card;
 import com.example.todo.service.CardService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,7 @@ public class CardController {
 	private final CardService cardService;
 
 	@GetMapping("")
-	public List<CardDto> cards(){
+	public List<Card> cards(){
 		return cardService.findAll();
 	}
 }
