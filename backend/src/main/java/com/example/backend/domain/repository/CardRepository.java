2ca914a752c7ea45d3db43109d4601b7052fb131
seyeong <seyoung7555@naver.com>
2022-04-07 11:32:00 +0900

    feat: 도메인 객체에 카드 수정 로직 추가

--- backend/src/main/java/com/example/backend/domain/repository/CardRepository.java
+++ backend/src/main/java/com/example/backend/domain/repository/CardRepository.java
@@ -3,11 +3,9 @@ package com.example.backend.domain.repository;
 import com.example.backend.domain.Card;
 import com.example.backend.domain.Column;
 import com.example.backend.web.dto.CardsMoveRequestDto;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface CardRepository {
 
     Column findAllDesc();
