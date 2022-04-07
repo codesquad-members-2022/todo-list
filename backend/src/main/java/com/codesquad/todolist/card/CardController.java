ac59aaf797168c37d176a25141a03c44c07638dc
honeySleepr <gruzzimo@naver.com>
2022-04-07 21:24:16 +0900

    test: [#29] CardController 단위 테스트 작성
    - 카드 생성 요청에 대해 2xx 응답이 보내지는지 확인

--- backend/src/main/java/com/codesquad/todolist/card/CardController.java
+++ backend/src/main/java/com/codesquad/todolist/card/CardController.java
@@ -1,5 +1,7 @@
 package com.codesquad.todolist.card;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cards")
 public class CardController {
 
+    private final CardService cardService;
+
+    @Autowired
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    @PostMapping
+    public void createCards() {
+    }
 }
--- backend/src/test/java/com/codesquad/todolist/card/unit/CardControllerTest.java
+++ backend/src/test/java/com/codesquad/todolist/card/unit/CardControllerTest.java
@@ -1,5 +1,44 @@
 package com.codesquad.todolist.card.unit;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.codesquad.todolist.card.CardController;
+import com.codesquad.todolist.card.CardService;
+
+@WebMvcTest(CardController.class)
 public class CardControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CardService cardService;
+
+    @Test
+    @DisplayName("카드를 생성하는 요청이 오면 카드가 생성된다")
+    public void createCardTest() throws Exception {
+
+        // when
+        ResultActions actions = mockMvc.perform(post("/cards")
+            .content(
+                "{" +
+                    "\"columnId\": 1,"
+                    + "\"title\": \"HTML/CSS 공부하기\","
+                    + "\"content\": \"매주 1시간 동안 HTML/CSS 책 읽기\","
+                    + "\"author\": \"web\""
+                    + "}"
+            ));
+
+        // then
+        actions.andExpect(status().is2xxSuccessful());
+    }
 }
