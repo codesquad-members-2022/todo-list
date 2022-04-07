73441bac305a111348ce1c174ea7cc1b7f861678
who-hoo <who.ho3ov@gmail.com>
2022-04-07 22:02:35 +0900

    ✨ 상태(Status), 기기(Device) 타입 추가

---
+++ backend/src/main/java/com/hooria/todo/domain/Status.java
@@ -0,0 +1,24 @@
+package com.hooria.todo.domain;
+
+import java.util.Arrays;
+
+public enum Status {
+    NONE(0), TODO(1), IN_PROGRESS(2), DONE(3);
+
+    private final int code;
+
+    Status(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static Status of(int code) {
+        return Arrays.stream(Status.values())
+            .filter(s -> s.code == code)
+            .findFirst()
+            .orElse(NONE);
+    }
+}
