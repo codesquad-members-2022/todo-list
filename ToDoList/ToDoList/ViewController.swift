7392083cb3ebc6442f7898e96d6ab6fa354abb63
Sol <hskim6521@naver.com>
2022-04-08 00:52:55 +0900

    [#6] Feat: 구현한 CardListViewController 3개를 ViewController의 children으로 추가

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -1,13 +1,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var cardListStackView: UIStackView!
+    
+    private let todoViewController = CardListViewController(title: "해야할 일")
+    private let inProgressViewController = CardListViewController(title: "진행중인 일")
+    private let doneViewController = CardListViewController(title: "완료한 일")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
+        self.addChild(todoViewController)
+        self.addChild(inProgressViewController)
+        self.addChild(doneViewController)
+        
+        layout()
     }
-
-
+    
+    private func layout() {
+        self.cardListStackView.clipsToBounds = true
+        self.children.forEach {
+            self.cardListStackView.addArrangedSubview($0.view)
+        }
+    }
 }
-
