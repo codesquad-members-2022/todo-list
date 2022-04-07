e179a38b7ebc5d3add6a405574cd0d6d7afc8535
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-04-07 18:34:02 +0900

    feat: JdbcCardRepository findAll()메서드 구현
    - jdbcTemplate 사용

--- backend/src/main/java/com/example/todo/repository/JdbcCardRepository.java
+++ backend/src/main/java/com/example/todo/repository/JdbcCardRepository.java
@@ -4,7 +4,11 @@ import com.example.todo.domain.Card;
 import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class JdbcCardRepository implements CardRepository{
 
 	private final JdbcTemplate jdbcTemplate;
@@ -15,6 +19,13 @@ public class JdbcCardRepository implements CardRepository{
 
 	@Override
 	public List<Card> findAll() {
-		return null;
+		return jdbcTemplate.query("select * from card", cardRowMapper());
+	}
+
+	private RowMapper<Card> cardRowMapper(){
+		return (rs, rowNum) -> {
+			Card card = new Card(rs.getString("title"),rs.getString("title"));
+			return card;
+		};
 	}
 }
