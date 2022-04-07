e88cea21a2b4331469834cdf3356e4545b09e56d
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-04-07 18:33:15 +0900

    feat: Cardservice findAll 메서드 구현

--- backend/src/main/java/com/example/todo/service/CardService.java
+++ backend/src/main/java/com/example/todo/service/CardService.java
@@ -1,11 +1,19 @@
 package com.example.todo.service;
 
 import com.example.todo.controller.card.CardDto;
+import com.example.todo.domain.Card;
+import com.example.todo.repository.CardRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class CardService {
 
-	public List<CardDto> findAll() {
+	private final CardRepository cardRepository;
 
+	public List<Card> findAll() {
+		return cardRepository.findAll();
 	}
 }
