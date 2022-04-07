de4c1ec1b10c62b370b4f8ca1cb1806cc8c99ad0
Millie <jay.lee.2280@gmail.com>
2022-04-07 12:31:40 +0900

    Refactor: import scss & create card to column

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,26 @@
-import {icons} from './constants/constant.js';
-import {createHeaderTemplate} from './views/header.js';
-import {createMainTemplate} from './views/main.js';
+import '../scss/main.scss';
+import { icons } from './constants/constant.js';
+import { createHeaderTemplate } from './views/header.js';
+import { createMainElement } from './views/main.js';
+import { createCardTemplate } from './views/card.js';
 
 (() => {
   const headerTemplate = createHeaderTemplate(icons.menu);
   document.body.insertAdjacentHTML('afterbegin', headerTemplate);
-  const mainNode = createMainTemplate(icons);
+  const mainElement = createMainElement(icons);
 
   // 데이터 처리 로직 테스트
-  // [...mainNode.children].forEach(cur => {
+  // [...mainElement.children].forEach(cur => {
   //   const test = cur.querySelector('.title-box__title__count');
   //   test.innerHTML = 10;
   // });
 
-  document.body.appendChild(mainNode);
+  document.body.appendChild(mainElement);
+
+  const todoColumn = mainElement.querySelector('.todo');
+  const todoCardList = todoColumn.querySelector('.task__cards');
+  todoCardList.innerHTML = createCardTemplate(
+    { header: '헤더', main: 'main', footer: 'footer', datetime: '20220407 11:07am' },
+    icons
+  );
 })();
