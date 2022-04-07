3db8a2812497b9e349c0fe5347d4e73f2148853c
Jason <ggj00125@gmail.com>
2022-04-07 16:43:15 +0900

    :wrench: Log.swift 와 LogTableViewCell.swift 파일 생성

---
+++ iOS/todo-list/todo-list/Model/Log.swift
@@ -0,0 +1,22 @@
+//
+//  Log.swift
+//  todo-list
+//
+//  Created by Jason on 2022/04/07.
+//
+
+import Foundation
+
+class Log {
+    
+    //MARK: UserName Area
+    
+    //MARK: Title Area
+    
+    //MARK: Activity Area
+    
+    //MARK: TimeStamp Area
+    
+    //MARK: TaskStatus(From-To) Area
+    
+}
--- iOS/todo-list/todo-list/View/LogTableViewCell.swift
+++ iOS/todo-list/todo-list/View/LogTableViewCell.swift
@@ -10,5 +10,4 @@ import UIKit
 
 class LogTableViewCell: UITableViewCell {
     
-    
 }
