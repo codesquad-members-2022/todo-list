05db1376d4ad612afa6b75f695f6f613eaf203b6
who-hoo <who.ho3ov@gmail.com>
2022-04-08 00:00:17 +0900

    ✨ 앗고의 request 반영 - `@ToString` 어노테이션 추가

--- backend/src/main/java/com/hooria/todo/domain/Card.java
+++ backend/src/main/java/com/hooria/todo/domain/Card.java
@@ -3,8 +3,10 @@ package com.hooria.todo.domain;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 @AllArgsConstructor
 public class Card {
 
