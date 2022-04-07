6a4735434302d8904d3c662fd6c5af9a3cb8c1cd
jun <devjun1023@gmail.com>
2022-04-07 12:13:50 +0900

    [Feat][#16] API 요청
    Contents
    - Card 조회 컨트롤러 DailyPlan 데이터 조회 기능 구현
    - API Service 클래스 생성
    
    Todo
    - CardReadService 기능 구현

---
+++ backend/src/main/java/com/example/backend/controller/card/CardReadController.java
@@ -0,0 +1,24 @@
+package com.example.backend.controller.card;
+
+import com.example.backend.controller.ApiResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.backend.controller.ApiResult.OK;
+
+@RestController
+@RequestMapping("api/read/cards")
+public class CardReadController {
+
+    private final CardReadService cardReadService;
+
+    public CardReadController(CardReadService cardReadService) {
+        this.cardReadService = cardReadService;
+    }
+
+    @GetMapping("")
+    public ApiResult<DailyPlan> getDailyTodoItems() {
+        return OK(new DailyPlan());
+    }
+}
