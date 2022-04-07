0a32f806a45c9e8ba23ad7b5f575a4f1a263f922
leekm0310 <leekm310@gmail.com>
2022-04-07 12:16:45 +0900

    feat : DB 스키마 및 임시데이터 작성
    closes #8

---
+++ todo-list/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into card (title, contents, writer, status) values ('테스트1', '테스트입니다', '얀', 'TODO');
+insert into card (title, contents, writer, status) values ('테스트1', '테스트입니다', '얀', 'TODO');
+insert into card (title, contents, writer, status) values ('테스트1', '테스트입니다', '얀', 'TODO');
+insert into card (title, contents, writer, status) values ('테스트1', '테스트입니다', '얀', 'TODO');
