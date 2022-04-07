4f293bb3b40d7a15f4dcd5dad383afb53eb78cfa
Damagucci-Juice <jsr9874@gmail.com>
2022-04-07 16:21:34 +0900

    feat: ToDo 카드로 들어갈 Table View Cell 을 생성 및 설정

---
+++ ios/TodoApp/TodoApp/Views/ToDoCell.swift
@@ -0,0 +1,39 @@
+//
+//  ToDoCell.swift
+//  TodoApp
+//
+//  Created by YEONGJIN JANG on 2022/04/07.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    @IBOutlet weak var cardView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureUI()
+    }
+
+   
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    private func configureUI() {
+        self.cardView?.layer.cornerRadius = 10
+    }
+
+}
