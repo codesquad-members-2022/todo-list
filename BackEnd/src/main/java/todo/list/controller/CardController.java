3ef0898a8257b2a90422ad6af7f9b9db88aa1119
hwicode <khc3796@naver.com>
2022-04-08 02:29:42 +0900

    feat : CardController 클래스에 모든 카드 조회를 요청하는 메소드 생성 - #4

--- BackEnd/src/main/java/todo/list/controller/CardController.java
+++ BackEnd/src/main/java/todo/list/controller/CardController.java
@@ -1,7 +1,9 @@
 package todo.list.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import todo.list.service.CardResponse;
 import todo.list.service.CardSaveDto;
 import todo.list.service.CardService;
 
@@ -17,5 +19,10 @@ public class CardController {
     @PostMapping("/cards")
     public void save(CardSaveDto cardSaveDto) {
         cardService.save(cardSaveDto);
+    }
+
+    @GetMapping("/cards")
+    public CardResponse getAllCards() {
+        return cardService.findCollections();
     }
 }
