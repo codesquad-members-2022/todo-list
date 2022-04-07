3bcd762bdccb8a78cbb1c0b2f26c9c34faa4b172
Jason <ggj00125@gmail.com>
2022-04-07 16:35:21 +0900

    :wrench: LogTableViewCell.swift 파일 추가

---
+++ iOS/todo-list/todo-list/View/LogTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  LogTableViewCell.swift
+//  todo-list
+//
+//  Created by Jason on 2022/04/07.
+//
+
+import Foundation
+import UIKit
+
+class LogTableViewCell: UITableViewCell {
+    
+    
+}
