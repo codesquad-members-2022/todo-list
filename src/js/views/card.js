d60bfc031ac3d33ee84fce0538293b326ec522d5
Millie <jay.lee.2280@gmail.com>
2022-04-07 15:00:29 +0900

    Feat: create new card template function

--- src/js/views/card.js
+++ src/js/views/card.js
@@ -12,3 +12,25 @@ export const createCardTemplate = (task, icons) => {
   </li>
     `;
 };
+
+export const createNewCardTemplate = icons => {
+  return `
+  <li class="task__card new-card">
+  <div class="card__contents">
+    <header class="card__contents__header">
+      <input class="card__contents__input--header" type="text" placeholder="제목을 입력하세요" />
+    </header>
+    <main class="card__contents__main">
+      <input class="card__contents__input--main" type="text" placeholder="내용을 입력하세요" />
+    </main>
+    <footer class="card__contents__footer footer__buttons">
+      <button class="footer__buttons__cancel">취소</button>
+      <button class="footer__buttons__save">등록</button>
+    </footer>
+  </div>
+  <div class="card__delete-btn">
+    ${icons.delete}
+  </div>
+</li>
+  `;
+};
