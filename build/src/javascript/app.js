b2425913e98ed398e30246fc13e9d76ca70e12f6
SeungHyun <fm10033@gmail.com>
2022-04-07 20:14:09 +0900

    Feat : axios post 함수 구현
    axiosRequest():request에 따라 적합한 axios메소드 호출하는 함수
    postData(): axios post 요청을 처리 함수
    app.js에 테스트 코드 구현

--- build/src/javascript/app.js
+++ build/src/javascript/app.js
@@ -1,3 +1,14 @@
-import { getData } from "./util/util.js";
-const data = await getData("todos");
-console.log(data);
+import { axiosRequest, postData } from "./util/util.js";
+const data = {
+  column: "have-to-do-column",
+  title: "post연습",
+  detail: "GitHub 공부 내용<br>모던 자바스크립트 1장 공부내용",
+  time: "~~~",
+};
+const getData = await axiosRequest("get", "todos");
+console.log(getData);
+const $hamburger = document.querySelector(".hamburger-button");
+$hamburger.addEventListener("click", async function () {
+  const post = await axiosRequest("post", "todos", data);
+  console.log(post);
+});
--- build/src/javascript/util/util.js
+++ build/src/javascript/util/util.js
@@ -1,8 +1,35 @@
 import axios from "https://cdn.skypack.dev/axios";
 
-export async function getData(dataName) {
+export async function getData(URL) {
+  const data = await axios.get(URL).then((response) => response.data);
+  return data;
+}
+
+export async function postData(URL, updatedData) {
+  const data = await axios
+    .post(URL, updatedData)
+    .then((response) => response.data);
+  return data;
+}
+
+export async function axiosRequest(request, dataName, data = {}) {
   const URL = `http://localhost:5000/${dataName}`;
-  const response = await axios.get(URL).then((response) => response.data);
-  const data = await response;
+  let response;
+  switch (request) {
+    case "get":
+      response = await getData(URL);
+      break;
+    case "post":
+      response = await postData(URL, data);
+      break;
+    case "put":
+      break;
+    case "delete":
+      break;
+  }
   return response;
 }
+
+//putData
+
+//deleteData
