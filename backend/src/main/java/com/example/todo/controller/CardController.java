201233ddef31afe5e03ed16484fdddd497c39dba
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-04-07 17:52:57 +0900

    feat: card controller 생성
    - card 목록 조회 findAll 구현

---
+++ backend/src/main/java/com/example/todo/controller/CardController.java
@@ -0,0 +1,22 @@
+package com.example.todo.controller;
+
+import com.example.todo.controller.card.CardDto;
+import com.example.todo.service.CardService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cards")
+@RequiredArgsConstructor
+public class CardController {
+
+	private final CardService cardService;
+
+	@GetMapping("")
+	public List<CardDto> cards(){
+		return cardService.findAll();
+	}
+}
