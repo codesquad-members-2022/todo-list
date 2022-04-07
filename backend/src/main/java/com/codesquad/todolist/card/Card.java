2fe164759767872f7e3c6249268670b993f4f51b
honeySleepr <gruzzimo@naver.com>
2022-04-07 21:25:38 +0900

    feat: [#29] Card 클래스에 빌더 패턴 적용
    - 인자가 많아 빌더패턴을 사용하였으나, 추후에 많은 변동사항이 생길 것이라 예상됨

--- backend/src/main/java/com/codesquad/todolist/card/Card.java
+++ backend/src/main/java/com/codesquad/todolist/card/Card.java
@@ -4,12 +4,60 @@ import java.time.LocalDateTime;
 
 public class Card {
 
+    private final Integer columnId;
+    private final String title;
+    private final String author;
     private Integer cardId;
-    private Integer columnId;
-    private String title;
     private String content;
     private Integer cardOrder;
     private LocalDateTime createdDate;
     private Boolean deleted;
 
+    public Card(Builder builder) {
+        this.cardId = builder.cardId;
+        this.columnId = builder.columnId;
+        this.title = builder.title;
+        this.author = builder.author;
+        this.content = builder.content;
+        this.cardOrder = builder.cardOrder;
+        this.createdDate = builder.createdDate;
+        this.deleted = builder.deleted;
+    }
+
+    public static class Builder {
+        private final Integer columnId;
+        private final String title;
+        private final String author;
+        private Integer cardId;
+        private String content;
+        private Integer cardOrder = 1;
+        private LocalDateTime createdDate;
+        private Boolean deleted = false;
+
+        public Builder(Integer columnId, String title, String author) {
+            this.columnId = columnId;
+            this.title = title;
+            this.author = author;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder date(LocalDateTime dateTime) {
+            this.createdDate = dateTime;
+            return this;
+        }
+
+        public Builder cardOrder(Integer cardOrder) {
+            this.cardOrder = cardOrder;
+            return this;
+        }
+
+        public Builder deleted(Boolean deleted) {
+            this.deleted = deleted;
+            return this;
+        }
+    }
 }
