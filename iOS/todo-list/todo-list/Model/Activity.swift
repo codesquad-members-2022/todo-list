a5de51667c8a2e5b24cadbfa8f2f2e93fec28a4c
Jason <ggj00125@gmail.com>
2022-04-07 16:46:14 +0900

    :wrench: Activity.swfit파일 생성 및 enum 타입으로 정리

---
+++ iOS/todo-list/todo-list/Model/Activity.swift
@@ -0,0 +1,15 @@
+//
+//  Activity.swift
+//  todo-list
+//
+//  Created by Jason on 2022/04/07.
+//
+
+import Foundation
+
+enum Activity: Decodable {
+    case Move
+    case Add
+    case Delete
+    case Update
+}
