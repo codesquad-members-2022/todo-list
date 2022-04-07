8a618e909b693f6b7d0f691e9ae8d0891e54f26d
vans <spentmny@nate.com>
2022-04-07 14:51:09 +0900

    [#5] Feat : CardService 메소드 구현
    id로 해당하는 card찾는 findOne()메소드 구현
    모든 card를 찾는 findAll()메소드 구현
    id로 해당하는 card 상태값 변경하는 delet()메소드 구현

--- BE/src/main/java/kr/codesquad/todolist/service/CardService.java
+++ BE/src/main/java/kr/codesquad/todolist/service/CardService.java
@@ -5,6 +5,12 @@ import kr.codesquad.todolist.dto.CardDto;
 import kr.codesquad.todolist.dto.CardResponse;
 import kr.codesquad.todolist.repository.CardRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CardService {
@@ -15,8 +21,25 @@ public class CardService {
         this.cardRepository = cardRepository;
     }
 
-    public CardResponse create(CardDto cardDto){
+    public CardResponse create(CardDto cardDto) {
         Card saved = cardRepository.save(cardDto.toEntity());
         return CardResponse.from(saved);
     }
+
+    public CardResponse findOne(Long id) {
+        Card card = cardRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        return CardResponse.from(card);
+    }
+
+    public List<CardResponse> findAll() {
+        List<Card> allCard = cardRepository.findAll();
+        return allCard.stream()
+                .map(CardResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    public boolean delete(Long id) {
+        return cardRepository.delete(id);
+    }
+
 }
