9f537f621db9e626104a34609e95ba620b79b7a9
hwicode <khc3796@naver.com>
2022-04-08 02:20:27 +0900

    feat : 도메인 객체인 Card 생성 - #4
    - CardRepository에서 가져온 정보를 저장하기 위해 만듦

--- BackEnd/src/main/java/todo/list/domain/Card.java
+++ BackEnd/src/main/java/todo/list/domain/Card.java
@@ -1,13 +1,41 @@
 package todo.list.domain;
 
-import java.time.LocalDateTime;
-
 public class Card {
     private Long id;
     private String title;
     private String contents;
     private CardStatus status;
-    private LocalDateTime createDateTime;
     private Author author;
 
+    public Card(Long id, String title, String contents, CardStatus status, Author author) {
+        this.id = id;
+        this.title = title;
+        this.contents = contents;
+        this.status = status;
+        this.author = author;
+    }
+
+    public boolean equalsStatus(CardStatus cardStatus) {
+        return status == cardStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public CardStatus getStatus() {
+        return status;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
 }
