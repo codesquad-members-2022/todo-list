1b26cadd9ae7e02b13401f2d91e9d5a66e6a12d0
honeySleepr <gruzzimo@naver.com>
2022-04-08 03:01:00 +0900

    feat: [#29] CardRepository에 카드 저장, 카드id로 조회기능 추가
    - create : 생성된 카드를 DB에 저장
    - findById : 카드 id로 DB의 카드 조회

--- backend/src/main/java/com/codesquad/todolist/card/CardRepository.java
+++ backend/src/main/java/com/codesquad/todolist/card/CardRepository.java
@@ -1,18 +1,45 @@
 package com.codesquad.todolist.card;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CardRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public CardRepository(JdbcTemplate jdbcTemplate) {
+    @Autowired
+    public CardRepository(NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public Card create(Card newCard) {
+        String sql = "insert into card(card_id, column_id, author, created_date, title, content, card_order, deleted)"
+            + " values(:cardId, :columnId, :author, :createdDate, :title, :content, :cardOrder, :deleted)";
+        jdbcTemplate.update(sql, new BeanPropertySqlParameterSource(newCard));
         return newCard;
+    }
+
+    public Optional<Card> findById(int cardId) {
+        String sql = "select * from card where card_id = :cardId and deleted = false";
+        Card card = jdbcTemplate.queryForObject(sql,
+            new MapSqlParameterSource().addValue("cardId", cardId),
+            getCardRowMapper());
+        return Optional.ofNullable(card);
+    }
+
+    private RowMapper<Card> getCardRowMapper() {
+        return (rs, rowNum) ->
+            new Card.Builder(rs.getInt("columnId"),
+                rs.getString("title"),
+                rs.getString("author"))
+                .content(rs.getString("content"))
+                .build();
     }
 }
