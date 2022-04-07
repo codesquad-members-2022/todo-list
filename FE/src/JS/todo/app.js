3743c74ebba6e0147aba4d59b2352fe098d6dbfa
seungchanHan <seungchanhan00@gmail.com>
2022-04-07 19:44:07 +0900

    [refactor] todo mvc 리펙토링

--- FE/src/JS/todo/app.js
+++ FE/src/JS/todo/app.js
@@ -1,12 +1,13 @@
 import Model from './Model.js';
 import View from './view.js';
 import Controller from './Controller.js';
-import { getData } from '../mok-data/data.js';
 import Drag from '../drag-drop/drag-drop.js';
 import { fetchRequest } from '../utility/fetchRequest.js';
 
 export async function initTodo() {
-  const workListData = await fetchRequest('/works?userId=ikjo');
+  const workListData = await fetchRequest(
+    '/todo-list/FE/src/js/mok-data/mokData.json'
+  );
   const model = new Model(workListData);
   const view = new View();
   new Controller(model, view);
