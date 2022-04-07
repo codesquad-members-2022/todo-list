b6e0d002a793b2042657ebf88fce993b68080e1e
Jason <ggj00125@gmail.com>
2022-04-08 01:37:06 +0900

    :wrench: Log Model 초기 코드 작성

--- iOS/todo-list/todo-list/Model/Log.swift
+++ iOS/todo-list/todo-list/Model/Log.swift
@@ -9,14 +9,52 @@ import Foundation
 
 class Log {
     
-    //MARK: UserName Area
+    private(set) var userName:String
+    private(set) var title:String    // Task()에서 받아와서 보여줄 수정사항
     
-    //MARK: Title Area
+    init(userName: String, title: String) {
+        self.userName = userName
+        self.title = title
+    }
     
     //MARK: Activity Area
+    func showActivity() -> Activate {
+        let dumySituation:Array<String> = ["이동", "등록", "삭제", "변경"]
+        
+        switch dumySituation {
+        case ["이동"]:
+            return Activate.Move
+        case ["등록"]:
+            return Activate.Add
+        case ["삭제"]:
+            return Activate.Delete
+        case ["변경"]:
+            return Activate.Update
+        default:
+            return Activate.Move
+        }
+    }
     
     //MARK: TimeStamp Area
+    func showTimeStamp() -> Int {
+        var count:Int = 0
+        let react = showActivity()
+        
+        if react != nil {
+            count += 1
+        }
+        
+        return count
+    }
     
     //MARK: TaskStatus(From-To) Area
+    func showTaskStatus() -> Array<String> {
+        let dumyStatus:Array<String> = ["todo", "inProcess", "done"]
+        var dumyStorage = [String]()
+        
+        // 입력된 부분이 From(Ex. 해야할 일) 과 To(Ex. 하고있는일)이 들어오면 처리
+        
+        return dumyStorage
+    }
     
 }
