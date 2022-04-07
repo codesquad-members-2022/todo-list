2e54af079d8f290f0f0a29afbd5d27e08d72aeca
PhilSoGooood <peelhw@gmail.com>
2022-04-07 15:54:49 +0900

    feat: Log 클래스 추가
    사용자의 각 활동기록을 담당하는 Log 클래스 추가
    title: 카드의 제목
    prevColumnName: 카드 이동 전 칼럼 이름
    currentColumnName: 카드 이동 후 칼럼 이름
    actionType: 액션 내용

---
+++ backend/src/main/java/com/example/backend/domain/Log.java
@@ -0,0 +1,62 @@
+package com.example.backend.domain;
+
+public class Log {
+	private Long id;
+	private String title;
+	private String prevColumnName;
+	private String currentColumnName;
+	private String actionType;
+	private String createdDate;
+
+	public Log(Long id, String title, String prevColumnName, String currentColumnName, String actionType, String createdDate) {
+		this.id = id;
+		this.title = title;
+		this.prevColumnName = prevColumnName;
+		this.currentColumnName = currentColumnName;
+		this.actionType = actionType;
+		this.createdDate = createdDate;
+	}
+
+	public static class Builder {
+		private Long id;
+		private String title;
+		private String prevColumnName;
+		private String currentColumnName;
+		private String actionType;
+		private String createdDate;
+
+		public Builder id(Long id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder title(String title) {
+			this.title = title;
+			return this;
+		}
+
+		public Builder prevColumnName(String prevColumnName) {
+			this.prevColumnName = prevColumnName;
+			return this;
+		}
+
+		public Builder currentColumnName(String currentColumnName) {
+			this.currentColumnName = currentColumnName;
+			return this;
+		}
+
+		public Builder actionType(String actionType) {
+			this.actionType = actionType;
+			return this;
+		}
+
+		public Builder createdDate(String createdDate) {
+			this.createdDate = createdDate;
+			return this;
+		}
+
+		public Log build() {
+			return new Log(id, title, prevColumnName, currentColumnName, actionType, createdDate);
+		}
+	}
+}
