f13135c8bc391d9cc4ec0c92835ac2941fdd917c
Millie <jay.lee.2280@gmail.com>
2022-04-07 14:57:36 +0900

    Feat: fetch tasks & insert cards in the columns

--- src/js/index.js
+++ src/js/index.js
@@ -3,24 +3,28 @@ import { icons } from './constants/constant.js';
 import { createHeaderTemplate } from './views/header.js';
 import { createMainElement } from './views/main.js';
 import { createCardTemplate } from './views/card.js';
+import { fetchData } from './utils/util';
 
 (() => {
   const headerTemplate = createHeaderTemplate(icons.menu);
   document.body.insertAdjacentHTML('afterbegin', headerTemplate);
   const mainElement = createMainElement(icons);
-
-  // 데이터 처리 로직 테스트
-  // [...mainElement.children].forEach(cur => {
-  //   const test = cur.querySelector('.title-box__title__count');
-  //   test.innerHTML = 10;
-  // });
-
   document.body.appendChild(mainElement);
 
-  const todoColumn = mainElement.querySelector('.todo');
-  const todoCardList = todoColumn.querySelector('.task__cards');
-  todoCardList.innerHTML = createCardTemplate(
-    { header: '헤더', main: 'main', footer: 'footer', datetime: '20220407 11:07am' },
-    icons
-  );
+  const fetchTasks = async () => {
+    try {
+      const tasks = await fetchData('http://localhost:3000/tasks');
+      tasks.forEach(task => {
+        const column = mainElement.querySelector(`.${task.status}`);
+        const cardList = column.querySelector('.task__cards');
+        cardList.insertAdjacentHTML('beforeend', createCardTemplate(task, icons));
+        const taskCount = column.querySelector('.title-box__title__count');
+        taskCount.textContent = cardList.children.length;
+      }, '');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  fetchTasks();
 })();
