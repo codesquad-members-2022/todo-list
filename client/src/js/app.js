ec18fd11024b0500c85f00d29b2a2e11cfdb15fe
mogooee <92701121+mogooee@users.noreply.github.com>
2022-04-07 11:12:03 +0900

    HTML, CSS 작업 - Card (#22)
    * feat: todo-list 입력창 높이 자동 조절
    todo-list를 입력하거나 지울 때 입력창의 높이를 자동으로 조절하는 기능 구현
    Related to: #20
    
    * design: column button hover color
    칼럼 타이틀의 +,x 버튼을 hover할 때 버튼 색상 변경
    Related to: #20
    
    * design: 수정카드 디자인 구현
    Related to: #20

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,11 +1,13 @@
 import { $ } from "./utils/utils.js";
 import { headInit } from "./header/view.js";
 import { sidebarInit } from "./sidebar/view.js";
+import { mainInit } from "./main/view.js";
 const header = $("header");
 const aside = $("aside");
 
 const init = () => {
   headInit(header);
+  mainInit();
   sidebarInit(aside);
 };
 
