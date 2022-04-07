c4439a28cfc2a6d135e246982c5f869d8b987cf4
seyeong <seyoung7555@naver.com>
2022-04-07 14:41:33 +0900

    feat: Swagger를 이용해 api 문서에 주석 추가
    - 각 Api에 대한 간략한 설명을 추가하였습니다.
    - 각 Api에서 요청받을 때 사용되는 변수의 설명을 추가하였습니다.

--- backend/src/main/java/com/example/backend/web/dto/CardUpdateRequestDto.java
+++ backend/src/main/java/com/example/backend/web/dto/CardUpdateRequestDto.java
@@ -1,7 +1,11 @@
 package com.example.backend.web.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class CardUpdateRequestDto {
+    @ApiModelProperty(example = "수정할 카드 제목")
     private String title;
+    @ApiModelProperty(example = "수정할 카드 내용")
     private String content;
 
     public CardUpdateRequestDto(String title, String content) {
