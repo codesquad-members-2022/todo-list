85bb9e47e2e31f3389bdc58b7247372673821ba8
inust33 <inust33@gmail.com>
2022-04-07 19:10:59 +0900

    #14- Refactor: SCSS로 변경 완료

--- core/TodoList.js
+++ core/TodoList.js
@@ -21,7 +21,7 @@ export class TodoList extends Component {
                           fill="#BDBDBD" />
                     </svg>
                 </span>
-        <span class="delete">
+                <span class="delete">
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                           d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"
