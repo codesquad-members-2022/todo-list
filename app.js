9810e8a1fd8ccfca7734028b1b40dd58aa7d59e2
Muffin <jinlog9@gmail.com>
2022-04-09 02:21:34 +0900

    refactor: app.js 코드 리팩토링

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 import TodoNoticeAnimation from './components/TodoNoticeAnimation.js';
 import TodoColumn from './components/TodoColumn.js';
 import Todo from './components/Todo.js';
-import TodoCount from './components/TodoCount.js';
 const columns = ['todo', 'ing', 'complete'];
 
 const app = () => {
-  columns.forEach(value => {
-    new TodoColumn(value).render();
-  });
+  const todoColumn = new TodoColumn(columns[0]).render();
+  const ingColumn = new TodoColumn(columns[1]).render();
+  const completeColumn = new TodoColumn(columns[2]).render();
 
   new TodoNoticeAnimation();
 
@@ -21,32 +20,11 @@ const app = () => {
 
 const dataLoad = todos => {
   // todos 배열에서 status(ing,todo,complete)를 각각 필터링하여 그에 맞는 배열에 넣는다.
-
-  const todoArr = todos.filter(todo => todo.status === columns[0]);
-  const ingArr = todos.filter(todo => todo.status === columns[1]);
-  const completeArr = todos.filter(todo => todo.status === columns[2]);
-
-  todoArr.forEach(todo => {
+  todos.forEach(todo => {
     const newTodo = new Todo(todo);
-    document.querySelector(`.${columns[0]}`).insertAdjacentHTML('afterend', newTodo.render());
+    document.querySelector(`.${todo.status}`).insertAdjacentHTML('afterend', newTodo.render());
     newTodo.run();
   });
-
-  ingArr.forEach(todo => {
-    const newTodo = new Todo(todo);
-    document.querySelector(`.${columns[1]}`).insertAdjacentHTML('afterend', newTodo.render());
-    newTodo.run();
-  });
-
-  completeArr.forEach(todo => {
-    const newTodo = new Todo(todo);
-    document.querySelector(`.${columns[2]}`).insertAdjacentHTML('afterend', newTodo.render());
-    newTodo.run();
-  });
-
-  new TodoCount(columns[0], todoArr.length).render();
-  new TodoCount(columns[1], ingArr.length).render();
-  new TodoCount(columns[2], completeArr.length).render();
 };
 
 app();
