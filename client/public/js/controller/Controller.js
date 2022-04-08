4a7cf49f2011b28868b6d5f57ded5d71765dd6be
Sangbeom Heo <mansaout@gmail.com>
2022-04-08 14:19:06 +0900

    Refactor: 컨트롤러 구조 변경 기존의 클래스 -> 함수

--- client/public/js/controller/Controller.js
+++ client/public/js/controller/Controller.js
@@ -1,15 +1,3 @@
-export default class Controller {
-  constructor(store, { headerView, asideView, columnsView }) {
-    this.store = store;
+import { qs } from "../utils/helpers.js";
 
-    this.headerView = headerView;
-    this.asideView = asideView;
-    this.columnsView = columnsView;
-
-    this.subscribeViewEvents();
-  }
-
-  subscribeViewEvents() {
-    // 뷰에서 발생하는 이벤트를 수신
-  }
-}
+function subscribeEvents() {}
