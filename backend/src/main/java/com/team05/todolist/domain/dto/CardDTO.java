845a22e0f9a81f91ccfa6306d8f2823de782ef46
Jaehong Choe <qnalsfj@gmail.com>
2022-04-08 17:22:03 +0900

    [BE-pio] feat: CardDTO section 필드 추가
    - CardService에서 Card인스턴스를 생성할 때 생성자로 section값을 넣어주기 위함.
    
    Related to #5

--- backend/src/main/java/com/team05/todolist/domain/dto/CardDTO.java
+++ backend/src/main/java/com/team05/todolist/domain/dto/CardDTO.java
@@ -1,5 +1,6 @@
 package com.team05.todolist.domain.dto;
 
+import com.team05.todolist.domain.Section;
 import lombok.Getter;
 
 @Getter
@@ -9,11 +10,13 @@ public class CardDTO {
 	private Integer orderIndex;
 	private String title;
 	private String content;
+	private String section;
 
-	public CardDTO(Integer orderIndex, String title, String content) {
+	public CardDTO(Integer orderIndex, String title, String content, String section) {
 		this.orderIndex = orderIndex;
 		this.title = title;
 		this.content = content;
+		this.section = section;
 	}
 
 	public void setCardId(Integer cardId) {
