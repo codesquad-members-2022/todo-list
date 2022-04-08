fb30e6bba46139fd5466b8d66950cfdaaa9be36e
naneun <rmtlrksdp@naver.com>
2022-04-08 11:54:33 +0900

    ♻️ 간단한 코드 정리

--- backend/src/test/java/com/hooria/todo/controller/ActivityLogControllerMockTest.java
+++ backend/src/test/java/com/hooria/todo/controller/ActivityLogControllerMockTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ActivityLogController.class)
-class ActivityLogControllerTest {
+class ActivityLogControllerMockTest {
 
     @Autowired
     MockMvc mvc;
