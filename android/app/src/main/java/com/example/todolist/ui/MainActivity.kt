1c949732f25c6c58fcc062055448c658592bbe29
hwj <jminie@naver.com>
2022-04-08 14:08:23 +0900

    feat: 카드 메뉴 인터페이스 구현-#19

--- android/app/src/main/java/com/example/todolist/ui/MainActivity.kt
+++ android/app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.todolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
+import android.view.*
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.example.todolist.R
@@ -10,7 +12,7 @@ import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.model.Status
 import com.example.todolist.ui.common.ViewModelFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
     private val viewModel: TaskViewModel by viewModels { ViewModelFactory() }
     private lateinit var binding: ActivityMainBinding
     private val historyAdapter: HistoryAdapter by lazy { HistoryAdapter() }
@@ -70,5 +72,22 @@ class MainActivity : AppCompatActivity() {
         binding.btnClose.setOnClickListener {
             binding.dlDrawer.closeDrawer(Gravity.RIGHT)
         }
+    }
+
+    private fun showPopup(view: View) {
+        val wrapper = ContextThemeWrapper(this, R.style.PopupWindow)
+        val popup = PopupMenu(wrapper, view)
+        popup.menuInflater.inflate(R.menu.popup, popup.menu)
+        popup.setOnMenuItemClickListener(this)
+        popup.show()
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.popup_go_done -> Toast.makeText(this, "popup_go_done", Toast.LENGTH_LONG).show()
+            R.id.popup_modify -> Toast.makeText(this, "popup_modify", Toast.LENGTH_LONG).show()
+            R.id.popup_delete -> Toast.makeText(this, "popup_delete", Toast.LENGTH_LONG).show()
+        }
+        return item != null
     }
 }
