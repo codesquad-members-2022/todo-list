68d1b058f85283075ce643f738e399e2cedef789
sally-ksh <sally.devz@gmail.com>
2022-04-08 23:49:57 +0900

    feat: CardService 의 readOf() : 카드 하나 조회
    - todoId, userId

--- backend/todolist/src/main/java/kr/codesquad/todolist/card/CardService.java
+++ backend/todolist/src/main/java/kr/codesquad/todolist/card/CardService.java
@@ -9,8 +9,15 @@ import lombok.RequiredArgsConstructor;
 public class CardService {
 	private final CardDao cardDao;
 
-	public CardDto.WriteResponse createCard(CardDto.WriteRequest request) {
+	public CardDto.RedirectInfo createCard(CardDto.WriteRequest request) {
 		Card card = request.toEntity();
-		return new CardDto.WriteResponse(cardDao.save(card));
+		Card cardInfo = cardDao.save(card);
+		return new CardDto.RedirectInfo(cardInfo.getTodoId(), cardInfo.getUserId());
+	}
+
+	public CardDto.WriteResponse readOf(Long id, Long userId) {
+		Card cardInfo = cardDao.findByIdAndUserId(id, userId)
+			.orElseThrow(() -> new IllegalArgumentException("card id 해당 정보 없습니다."));
+		return new CardDto.WriteResponse(cardInfo);
 	}
 }
