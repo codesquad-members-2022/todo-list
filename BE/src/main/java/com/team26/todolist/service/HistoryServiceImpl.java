4d3285974dd1676ac1aa23e4690a4fbbc9815fab
dukcode <duk9741@gmail.com>
2022-04-08 14:52:32 +0900

    feat: HistoryService 구현
    1. HistoryService 인터페이스 추가
    2. HistoryServiceImpl 구현
    
    end

---
+++ BE/src/main/java/com/team26/todolist/service/HistoryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.team26.todolist.service;
+
+import com.team26.todolist.domain.Card;
+import com.team26.todolist.domain.CardAction;
+import com.team26.todolist.domain.History;
+import com.team26.todolist.dto.response.HistoryResponse;
+import com.team26.todolist.repository.HistoryRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HistoryServiceImpl implements HistoryService {
+
+    private final HistoryRepository historyRepository;
+
+    public HistoryServiceImpl(HistoryRepository historyRepository) {
+        this.historyRepository = historyRepository;
+    }
+
+    @Override
+    public List<HistoryResponse> findHistories() {
+        List<History> histories = historyRepository.findAll();
+        return histories.stream().map(HistoryResponse::of).collect(Collectors.toList());
+    }
+
+    @Override
+    public void saveHistory(CardAction cardAction, Card cardBefore, Card cardNow) {
+        // TODO : Card 클래스에 getter 필요
+    }
+}
