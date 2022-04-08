4981880742ac3e4091c0339fbaa75245a1d8eeec
Sangbeom Heo <mansaout@gmail.com>
2022-04-08 10:13:41 +0900

    Feat: 화면에 엘리먼트를 추가하는 렌더링 함수모음 객체 renderer 작성

---
+++ client/public/js/views/renderer.js
@@ -0,0 +1,26 @@
+import { qs, insertElement } from "../utils/helpers.js";
+import { createColumn } from "./column/column.js";
+import { createItem } from "./card/item.js";
+
+const renderer = {
+  allColumns: (columnList) => {
+    console.log(columnList);
+    columnList.forEach(renderer.column);
+  },
+
+  allItems: (itemList) => {
+    itemList.forEach(renderer.item);
+  },
+
+  column: ({ id, title }) => {
+    const columnListEl = qs(".column-list");
+    insertElement(columnListEl, "beforeend", createColumn({ id, title }));
+  },
+
+  item: ({ id, columnId, title, content }) => {
+    const columnEl = qs(`[data-column='${columnId}'`);
+    insertElement(columnEl, "beforeend", createItem({ id, title, content }));
+  },
+};
+
+export { renderer };
