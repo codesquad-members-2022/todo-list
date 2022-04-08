f3d4c08c122dc9594dad1c104b8c763ca8638cff
Sangbeom Heo <mansaout@gmail.com>
2022-04-08 14:10:00 +0900

    Feat: 히스토리 내역 데이터 추가

--- client/public/js/tempStorage.js
+++ client/public/js/tempStorage.js
@@ -33,10 +33,26 @@ const database = [
       },
     ],
     history: [
-      { date: new Date(), action: "add", content: "등록했습니다." },
-      { date: new Date(), action: "remove", content: "삭제했습니다." },
-      { date: new Date(), action: "update", content: "변경했습니다." },
-      { date: new Date(), action: "move", content: "이동했습니다." },
+      {
+        date: new Date("2022-04-06"),
+        username: "yellow",
+        action: "add",
+        content: "<strong>해야할 일</strong>에 <strong>GitHub 공부하기</strong>를 <strong>등록</strong>하였습니다.",
+      },
+      {
+        date: new Date("2022-04-07"),
+        username: "yellow",
+        action: "add",
+        content:
+          "<strong>해야할 일</strong>에 <strong>블로그에 포스팅할 것</strong>를 <strong>등록</strong>하였습니다.",
+      },
+      {
+        date: new Date("2022-04-08"),
+        username: "yellow",
+        action: "add",
+        content:
+          "<strong>하고 있는 일</strong>에 <strong>HTML/CSS 공부하기</strong>를 <strong>등록</strong>하였습니다.",
+      },
     ],
   },
   {
