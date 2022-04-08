28f63f56000faf7f03887109cd6c775830a5a777
dukcode <duk9741@gmail.com>
2022-04-08 14:50:15 +0900

    feat: HistoryRepository 추가
    1. HistoryRepository 인터페이스 추가
    2. HistoryRepositoryImpl 구현체 추가 (TODO : save 구현)
    
    end

---
+++ BE/src/main/java/com/team26/todolist/repository/HistoryRepository.java
@@ -0,0 +1,13 @@
+package com.team26.todolist.repository;
+
+import com.team26.todolist.domain.History;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HistoryRepository {
+
+    List<History> findAll();
+    History findById(Long id);
+    History save(History history);
+}
---
+++ BE/src/main/java/com/team26/todolist/repository/HistoryRepositoryImpl.java
@@ -0,0 +1,68 @@
+package com.team26.todolist.repository;
+
+import com.team26.todolist.domain.CardAction;
+import com.team26.todolist.domain.CardStatus;
+import com.team26.todolist.domain.History;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HistoryRepositoryImpl implements HistoryRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public HistoryRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<History> findAll() {
+        String sql = "SELECT * FROM HISTORY";
+        return jdbcTemplate.query(sql, historyRowMapper());
+    }
+
+    @Override
+    public History findById(Long id) {
+        String sql = "SELECT * FROM HISTORY WHERE id = ?";
+        List<History> result = jdbcTemplate.query(sql, historyRowMapper(), id);
+
+        return result.stream().findAny().orElse(null);
+    }
+
+
+    @Override
+    public History save(History history) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("history").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("card_action", history.getCardAction());
+        parameters.put("user_id", history.getUserId());
+        parameters.put("card_title", history.getCardTitle());
+        parameters.put("card_title_before", history.getCardTitleBefore());
+        parameters.put("card_status", history.getCardStatus().name());
+        parameters.put("card_status_before", history.getCardStatusBefore().name());
+        parameters.put("created_at", history.getCreatedAt());
+
+        Long key = (Long)jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        history.initId(key);
+        return history;
+    }
+
+    private RowMapper<History> historyRowMapper() {
+        return (rs, rowNum) ->
+                History.builder(CardAction.valueOf(rs.getString("card_action")),
+                                rs.getString("user_id"), rs.getString("card_title"),
+                                CardStatus.valueOf(rs.getString("card_status")),
+                                rs.getTimestamp("created_at").toLocalDateTime())
+                        .cardTitleBefore(rs.getString("card_title_before"))
+                        .cardStatusBefore(CardStatus.valueOf(rs.getString("card_status_before")))
+                        .build();
+    }
+}
