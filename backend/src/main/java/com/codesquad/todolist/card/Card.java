78e4708e6a4996aa11805d3ddf53bf75ea8cb885
Kang Changgu (Miller) <rkckr1@naver.com>
2022-04-08 14:46:24 +0900

    fix: Card 클래스 내 conflict 해결

--- backend/src/main/java/com/codesquad/todolist/card/Card.java
+++ backend/src/main/java/com/codesquad/todolist/card/Card.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 
 public class Card {
 
-    private final Integer columnId;
-    private final String title;
-    private final String author;
     private Integer cardId;
+    private Integer columnId;
+    private String title;
     private String content;
+    private String author;
+    private Integer cardOrder;
     private Integer order;
     private LocalDateTime createdDate;
     private Boolean deleted;
@@ -18,17 +19,52 @@ public class Card {
     }
 
     public Card(Integer cardId, Integer columnId, String title, String content, String author,
-        Integer cardOrder, LocalDateTime createdDate) {
+        Integer order, LocalDateTime createdDate) {
         this.cardId = cardId;
         this.columnId = columnId;
         this.title = title;
         this.content = content;
         this.author = author;
-        this.order = cardOrder;
+        this.order = order;
         this.createdDate = createdDate;
     }
 
-    public void setCardId(Integer cardId) {
+    public void update(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Integer getCardId() {
+        return cardId;
+    }
+
+    public Integer getColumnId() {
+        return columnId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getCardOrder() {
+        return cardOrder;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCardId(int cardId) {
         this.cardId = cardId;
     }
+
 }
