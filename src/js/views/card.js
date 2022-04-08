15d8b4fcffdd8df1974246c61c305794acbc90e8
youryu0212 <youryu0212@naver.com>
2022-04-09 01:46:08 +0900

    Feat: create insertAllCardToColumn function

--- src/js/views/card.js
+++ src/js/views/card.js
@@ -8,18 +8,11 @@ export const insertCardToColumn = (column, icons, parent = document) => {
   });
 };
 
-// export const createCards = (mainElement, store, icons) => {
-//   store.forEach(cur => {
-//     const columnClassName = cur.className;
-//     const column = mainElement.querySelector(`.${columnClassName}`);
-//     const cardList = column.querySelector('.task__cards');
-//     const taskCount = column.querySelector('.title-column__title__count');
-//     taskCount.textContent = cur.total;
-//     cur.tasks.forEach(task => {
-//       cardList.insertAdjacentHTML('beforeend', createCardTemplate(task, icons));
-//     });
-//   });
-// };
+export const insertAllCardToColumn = (parent, icons) => {
+  parent.getStore().forEach(column => {
+    insertCardToColumn(column, icons, parent.element);
+  });
+};
 
 export const createCardTemplate = (task, icons) => {
   return `
@@ -34,26 +27,4 @@ export const createCardTemplate = (task, icons) => {
     </div>
   </li>
     `;
-};
-
-export const createNewCardTemplate = icons => {
-  return `
-  <li class="task__card new-card">
-  <div class="card__contents">
-    <header class="card__contents__header">
-      <input class="card__contents__input--header" type="text" placeholder="제목을 입력하세요" />
-    </header>
-    <main class="card__contents__main">
-      <textarea class="card__contents__input--main" maxlength="500" placeholder="내용을 입력하세요"></textarea>
-    </main>
-    <footer class="card__contents__footer footer__buttons">
-      <button class="footer__buttons__cancel">취소</button>
-      <button class="footer__buttons__save">등록</button>
-    </footer>
-  </div>
-  <div class="card__delete-btn">
-    ${icons.delete}
-  </div>
-</li>
-  `;
 };
