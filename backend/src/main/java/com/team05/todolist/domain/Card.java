c6683b11fc012d387f18a14bfec0177f491e2aae
Jaehong Choe <qnalsfj@gmail.com>
2022-04-08 17:17:06 +0900

    [BE-pio] feat: Card class 수정
    - lombok 제거
    section getter를 enum 필드의 값을 가져오도록 변경하면서 lombok을 제거.
    - deleteYN 필드 추가
    
    Related to #5

--- backend/src/main/java/com/team05/todolist/domain/Card.java
+++ backend/src/main/java/com/team05/todolist/domain/Card.java
@@ -1,28 +1,43 @@
 package com.team05.todolist.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
 public class Card {
 
 	private Integer id;
 	private Integer orderIndex;
-	private Integer delete;
+	private Integer deleteYN;
 	private String title;
 	private String content;
 	private Section section;
 
-	public Card(Integer orderIndex, Integer delete, String title, String content, String section) {
+	public Card(Integer orderIndex, Integer deleteYN, String title, String content, String section) {
 		this.orderIndex = orderIndex;
-		this.delete = delete;
+		this.deleteYN = deleteYN;
 		this.title = title;
 		this.content = content;
-		this.section = Section.valueOf(section);
+		this.section = Section.getSection(section);
 	}
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public Integer getOrderIndex() {
+		return orderIndex;
+	}
+
+	public Integer getDeleteYN() {
+		return deleteYN;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getSectionType() {
+		return section.getSectionType();
 	}
 }
