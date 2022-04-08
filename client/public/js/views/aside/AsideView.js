eecc2e67977ae0254c281c8601129eb8e5467c92
Jwu <sju02048@naver.com>
2022-04-08 10:03:45 +0900

    정적페이지 card,alert,layer (#19)
    * Design: icon svg 추가
    
    * Design: 활동기록 html 추가
    
    * Design: column, card, history css
    
    * 아직 못한 html&css
    - 카드생성박스
    - 취소 Alert창
    - 추가기능 html

--- client/public/js/views/aside/AsideView.js
+++ client/public/js/views/aside/AsideView.js
@@ -8,3 +8,10 @@ export default class AsideView extends View {
 }
 
 // Header의 asideButton 클릭 시 나타나는 UI. 활동기록을 포함
+qs(".header--history-btn").addEventListener("click", (e) => {
+  qs(".history__bar").classList.add("show");
+});
+
+qs(".history--close-btn").addEventListener("click", (e) => {
+  qs(".history__bar").classList.remove("show");
+});
