f9324155c00ce57f859003c35b7b8c6a8e1b94be
ldldz <qkrqudrjs4350@gmail.com>
2022-04-10 01:03:17 +0900

    [#23]Feat: 하드코딩한 columns 데이터 서버 데이터로 변경

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
+import { getColumnTitles } from "./api.js";
 import Column from "./components/Column.js";
 import Header from "./components/Header.js";
 import Component from "./core/Component.js";
 
 export default class App extends Component {
-  setup() {
+  async setup() {
+    const columnTitles = await getColumnTitles();
     this.state = {
-      columns: [{ title: "해야할 일" }, { title: "하고 있는 일" }, { title: "완료한 일" }],
+      columnTitles: columnTitles,
     };
+    this.render();
   }
   template() {
     return `
       <header></header>
       <main class="flex">
-        <div class="column"></div>
-        <div class="column"></div>
-        <div class="column"></div>
-        <div class="aaa"></div>
+        ${'<div class="column"></div>'.repeat(this.state.columnTitles?.length)}
       </main>
     `;
   }
@@ -24,7 +24,7 @@ export default class App extends Component {
     const $columns = this.$target.querySelectorAll(".column");
     new Header($header);
     $columns.forEach(($column, index) => {
-      new Column($column, null, this.state.columns[index]);
+      new Column($column, null, this.state.columnTitles[index]);
     });
   }
 }
