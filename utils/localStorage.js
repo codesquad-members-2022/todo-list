84c4a7cb3a4d7e474464728e09f6ae464bfcc4e2
suuzin <8oozin@gmail.com>
2022-04-09 23:38:07 +0900

    feat : 로컬스토리지 id로 객체 키와 값을 변경하는 함수

--- utils/localStorage.js
+++ utils/localStorage.js
@@ -1,3 +1,15 @@
 export const getLocalStorageByKey = key => {
   return JSON.parse(localStorage.getItem(key));
 };
+
+export const editLocalStorageById = (editLocalStorageObj, id) => {
+  if (!id) return;
+  const objIndex = getLocalStorageByKey('todos').findIndex(e => e.id === id);
+  const todos = getLocalStorageByKey('todos');
+
+  for (const [key, value] of Object.entries(editLocalStorageObj)) {
+    todos[objIndex][key] = value;
+  }
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
