2a5cc5e02fc4591738e2100516c482751cafec94
nak253 <nwon4413@naver.com>
2022-04-10 15:37:05 +0900

    feat: EventController클래스에 Event목록을 조회하는 Get요청을 수행하는 메서드 작성

---
+++ be/src/main/java/todolist/controller/EventController.java
@@ -0,0 +1,25 @@
+package todolist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import todolist.domain.Event;
+import todolist.service.EventService;
+
+import java.util.List;
+
+@Controller
+public class EventController {
+
+    private final EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/events")
+    public List<Event> getEvents() {
+        return eventService.getEventList();
+    }
+}
