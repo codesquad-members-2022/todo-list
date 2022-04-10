9922161e66ed536adbec3206ca4a2d21c85500d4
함석호 <kalejuice02@gmail.com>
2022-04-10 17:36:09 +0900

    feat : 각 이벤트 메서드를 가지고 있는 EventPublisher 클래스 작성 및 Eve정tService 클래스의 addEvent 메서드 수정

--- be/src/main/java/todolist/service/EventService.java
+++ be/src/main/java/todolist/service/EventService.java
@@ -2,30 +2,42 @@ package todolist.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import todolist.domain.Action;
-import todolist.domain.Event;
-import todolist.dto.ResponseCardDto;
+import todolist.domain.event.Action;
+import todolist.domain.event.Event;
+import todolist.dto.card.ResponseCardDto;
 import todolist.repository.EventMemoryRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class EventService {
 
     private EventMemoryRepository repository;
+    private Map<Action, Function<ResponseCardDto,Event>> eventHandlingMethods = new HashMap<>();
 
     @Autowired
     public EventService(EventMemoryRepository repository) {
         this.repository = repository;
+        initEventMethods();
     }
 
     public void addEvent(ResponseCardDto responseCardDto, Action action) {
-        Long cardId = responseCardDto.getId();
-        String cardSection = responseCardDto.getSection();
-        repository.save(new Event(cardId, cardSection, action));
+        Function<ResponseCardDto, Event> eventFunction = eventHandlingMethods.get(action);
+        Event event = eventFunction.apply(responseCardDto);
+        repository.save(event);
     }
 
     public List<Event> getEventList() {
         return repository.findAll();
+    }
+
+    private void initEventMethods() {
+        eventHandlingMethods.put(Action.ADD, EventPublisher.add);
+        eventHandlingMethods.put(Action.UPDATE, EventPublisher.update);
+        eventHandlingMethods.put(Action.MOVE, EventPublisher.move);
+        eventHandlingMethods.put(Action.REMOVE, EventPublisher.remove);
     }
 }
