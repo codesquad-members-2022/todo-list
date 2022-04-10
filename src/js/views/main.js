6bc5abf893e58b3d4b10f3fdf92080583b076111
youryu0212 <youryu0212@naver.com>
2022-04-10 16:19:37 +0900

    Refactor: createMainObj to createMainElement

--- src/js/views/main.js
+++ src/js/views/main.js
@@ -1,17 +1,5 @@
-import { fetchData } from '../utils/util.js';
-
-export const createMainObj = async url => {
-  const store = await fetchData(url);
-  const getStore = () => {
-    return store;
-  };
-  // const setStore = (item) =>{
-  //   store.push(item);
-  // }
-  return (() => {
-    const element = document.createElement('main');
-    const main = { element, getStore };
-    main.element.classList.add('main');
-    return main;
-  })();
+export const createMainElement = () => {
+  const mainElement = document.createElement('main');
+  mainElement.classList.add('main');
+  return mainElement;
 };
