40c24a35dc35f0ff196ceed73784ff69251f844d
ju-kkim <wndus8198@naver.com>
2022-04-11 04:02:27 +0900

    feat: [#33] Header Template 추가

---
+++ frontend/src/js/Utils/template.js
@@ -0,0 +1,12 @@
+const headerTemplate = () => {
+  return `
+  <header class="header">
+    <h1 class="header_title">TO-DO LIST</h1>
+    <button class="menu_btn">
+      <img src="./src/img/menu_icon.png" alt="활동기록 메뉴" />
+    </button>
+  </header>
+  `;
+};
+
+export { headerTemplate };
