6ba5b9e5541fcb771b32b9b426782dc010f29794
함석호 <kalejuice02@gmail.com>
2022-04-10 14:45:04 +0900

    feat : EventMemoryRepository 클래스 작성

---
+++ be/src/main/java/todolist/repository/EventMemoryRepository.java
@@ -0,0 +1,25 @@
+package todolist.repository;
+
+import org.springframework.stereotype.Repository;
+import todolist.domain.Event;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class EventMemoryRepository {
+
+    private static List<Event> store = new ArrayList<>();
+    private static AtomicLong id = new AtomicLong();
+
+    public void save(Event event) {
+        event.setId(id.incrementAndGet());
+        store.add(event);
+    }
+
+    public List<Event> findAll() {
+        return Collections.unmodifiableList(store);
+    }
+}
