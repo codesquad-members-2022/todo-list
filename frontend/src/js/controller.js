db4b666e7bc770f1a0eeeab09940f47bc17a485f
ju-kkim <wndus8198@naver.com>
2022-04-11 04:38:33 +0900

    feat: [#33][#43] menuBtn, closeBtn 클릭핸들러, 히스토리 애니메이션 호출

--- frontend/src/js/controller.js
+++ frontend/src/js/controller.js
@@ -1,14 +1,18 @@
 export default class Controller {
-  constructor({ header }) {
+  constructor({ header, history }) {
     this.header = header;
+    this.history = history;
     this.init();
   }
 
   init() {
     this.header.view.eventInit(this.menuBtnClickHandler.bind(this));
+    this.history.view.eventInit(this.menuBtnClickHandler.bind(this));
   }
 
   menuBtnClickHandler() {
     this.header.model.updateStatus();
+    const menuStatus = this.header.model.getMenuStatus();
+    this.history.view.animation(menuStatus);
   }
 }
