f20e1ebc464fe8d2cad8136e408947902131a32e
who-hoo <who.ho3ov@gmail.com>
2022-04-10 16:16:32 +0900

    🐛 카드 생성 DTO 변환 안되는 에러 fix
    - Getter 추가(getter를 이용하여 자동 생성하기 때문에 추가)
    - 빈 생성자 추가(Jackson 라이브러리가 빈 생성자가 없는 모델을 생성하는 방법을 모르기 때문에 빈 생성자가 없을 시에 에러 발생)
    - [TODO] 빈 생성자 추가하는 방법 외에 `@JsonProperty("field_name")` 추가하는 방법 있음! 리앗고와 상의해서 어떤 방법으로 통일할지 정하자!

--- backend/src/main/java/com/hooria/todo/dto/AddCardParam.java
+++ backend/src/main/java/com/hooria/todo/dto/AddCardParam.java
@@ -1,14 +1,20 @@
 package com.hooria.todo.dto;
 
 import com.hooria.todo.domain.Card;
+import lombok.Getter;
 
+@Getter
 public class AddCardParam {
 
-    private final String status;
-    private final String title;
-    private final String content;
-    private final String userId;
-    private final String device;
+    private String status;
+    private String title;
+    private String content;
+    private String userId;
+    private String device;
+
+    // TODO : 리아코와 상의 후 빈생성자, 어노테이션 추가 중 어떤 방법을 사용할지 정하기
+    public AddCardParam() {
+    }
 
     public AddCardParam(String status, String title, String content, String userId,
         String device) {
