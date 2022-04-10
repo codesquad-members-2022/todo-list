f014d130cb365ae5e089ed6ede7e9c7cc8f88dfd
Jaehong Choe <qnalsfj@gmail.com>
2022-04-10 18:52:12 +0900

    [BE-pio] feat: Swagger 세팅 및 적용

--- backend/src/main/java/com/team05/todolist/domain/dto/CardDTO.java
+++ backend/src/main/java/com/team05/todolist/domain/dto/CardDTO.java
@@ -1,15 +1,23 @@
 package com.team05.todolist.domain.dto;
 
 import com.team05.todolist.domain.Section;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
+@ApiModel(value="CardDTO : 카드 정보를 가지고 있는 모델")
 @Getter
 public class CardDTO {
 
+	@ApiModelProperty(hidden = true)
 	private Integer cardId;
+	@ApiModelProperty(value="카드 인덱스", required = true)
 	private Integer orderIndex;
+	@ApiModelProperty(value="카드 제목", required = true)
 	private String title;
+	@ApiModelProperty(value="카드 내용", required = true)
 	private String content;
+	@ApiModelProperty(value="카드 섹션 정보 [todo /doing /done]", required = true)
 	private String section;
 
 	public CardDTO(Integer orderIndex, String title, String content, String section) {
--- backend/src/main/java/com/team05/todolist/domain/dto/LogDTO.java
+++ backend/src/main/java/com/team05/todolist/domain/dto/LogDTO.java
@@ -1,15 +1,25 @@
 package com.team05.todolist.domain.dto;
 
 import com.team05.todolist.domain.Event;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import java.time.LocalDateTime;
 
+@ApiModel(value = "LogDTO: 카드 CRUD 로그")
 public class LogDTO {
 
+	@ApiModelProperty(value = "로그 id")
 	private Integer logId;
+	@ApiModelProperty(value = "카드 CRUD 분류")
 	private Event logEvent;
+	@ApiModelProperty(value = "로그 생성 시간")
 	private LocalDateTime logTime;
+	@ApiModelProperty(value = "카드 제목")
 	private String title;
+	@ApiModelProperty(value = "카드 이동 전 section")
 	private String prevSection;
+	@ApiModelProperty(value = "카드 이동 후 section")
 	private String section;
 
 	public LogDTO(Integer logId, String logEvent, LocalDateTime logTime, String title,
