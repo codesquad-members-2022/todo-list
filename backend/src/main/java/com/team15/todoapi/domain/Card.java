57f61efc26ac2fbaf0114f4361a4a08e8c997e77
mybloom <baejungeun11@gmail.com>
2022-04-11 11:18:13 +0900

    refactor: Card 멤버변수 카멜케이스 적용하여 이름 변경 및 Getter와 생성자 롬복으로 구현
    - 생성자는 롬복 이용하여 private으로 셋팅
    - @AllArgsConstructor(access = AccessLevel.PRIVATE)

--- backend/src/main/java/com/team15/todoapi/domain/Card.java
+++ backend/src/main/java/com/team15/todoapi/domain/Card.java
@@ -1,56 +1,19 @@
 package com.team15.todoapi.domain;
 
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Card {
 
 	private Long id;
 	private String title;
 	private String content;
-	private Long member_id;
-	private LocalDateTime created_at;
-	private LocalDateTime modified_at;
-	private boolean delete_flag;
+	private String userId;
+	private LocalDateTime modifiedAt;
 	private int section;
 
-	public Card(String title, String content) {
-		this.title = title;
-		this.content = content;
-		this.created_at = LocalDateTime.now();
-		this.modified_at = LocalDateTime.now();
-		this.delete_flag = false;
-		this.section = 1;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public Long getMember_id() {
-		return member_id;
-	}
-
-	public LocalDateTime getCreated_at() {
-		return created_at;
-	}
-
-	public LocalDateTime getModified_at() {
-		return modified_at;
-	}
-
-	public boolean isDelete_flag() {
-		return delete_flag;
-	}
-
-	public int getSection() {
-		return section;
-	}
 }
