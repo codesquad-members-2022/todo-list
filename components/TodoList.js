c51496c1563186614d24eec2e7846846aba6da6f
inust33 <inust33@gmail.com>
2022-04-11 17:06:14 +0900

    Build: webpack-dev-server 및 css loader 설정

--- components/TodoList.js
+++ components/TodoList.js
@@ -63,10 +63,11 @@ export class TodoList extends View {
     const card = this.select(`.todo-card[data-idx="0"]`);
     todos.forEach(
       (todo, idx) =>
-        new TodoCard(this.select(`.todo-card[data-idx="${idx}"]`), {
-          todo,
-          idx
-        })
+        todo.selected ? new TodoForm() :
+          new TodoCard(this.select(`.todo-card[data-idx="${idx}"]`), {
+            todo,
+            idx
+          })
     );
   }
   setEvent(){
