afced10b7ba1baf5f29e40b0b3352520d21f23c5
Serin-Kim <serin9864@gmail.com>
2022-04-11 11:23:09 +0900

    Build: webpack devSever 설정

--- webpack.common.js
+++ webpack.common.js
@@ -1,42 +1,38 @@
-
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./app.ts",
+  entry: './app.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-
-  },
-  resolve: {
-    extensions: ['', '.js','.ts']
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/, use: ['babel-loader', 'ts-loader'], exclude: /node_modules/
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-
       {
         test: /\.css$/, // .css 확장자로 끝나는 모든 파일
-        use: ["style-loader", "css-loader"] // css-loader를 적용한다
+        use: ['style-loader', 'css-loader'], // css-loader를 적용한다
       },
       {
         test: /\.png$/, // .png 확장자로 마치는 모든 파일
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
-          name: "[name].[ext]?[hash]" // 파일명 형식
-        }
-      }
-    ]
+          publicPath: './dist/', // prefix를 아웃풋 경로로 지정
+          name: '[name].[ext]?[hash]', // 파일명 형식
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html"
-    })
-  ]
-
+      template: './index.html',
+    }),
+  ],
 };
-
