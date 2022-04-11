bb5ffafff0ff2377304343e0c53127106fd8d1a1
naneun <rmtlrksdp@naver.com>
2022-04-11 19:06:13 +0900

    ♻️ AddCardParam @NoArgsConstructor 어노테이션 추가

--- backend/src/main/java/com/hooria/todo/dto/AddCardParam.java
+++ backend/src/main/java/com/hooria/todo/dto/AddCardParam.java
@@ -3,8 +3,10 @@ package com.hooria.todo.dto;
 import com.hooria.todo.domain.Card;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AddCardParam {
 
