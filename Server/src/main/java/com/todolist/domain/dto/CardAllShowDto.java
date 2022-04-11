b1cd5d31b639811574251ca743641c398f9d65b9
leejohy-0223 <leejohy@naver.com>
2022-04-12 01:40:57 +0900

    feat: HttpServletRequest에서 쿠키를 통해 userId를 가져오도록 적용 & Map을 Dto로 캡슐화하여 반환하도록 변경 (#40)

---
+++ Server/src/main/java/com/todolist/domain/dto/CardAllShowDto.java
@@ -0,0 +1,15 @@
+package com.todolist.domain.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public class CardAllShowDto {
+    private Map<String, List<CardInformationDto>> cardMap;
+
+    public CardAllShowDto(Map<String, List<CardInformationDto>> cardMap) {
+        this.cardMap = cardMap;
+    }
+}
--- Server/src/main/java/com/todolist/exception/ExceptionType.java
+++ Server/src/main/java/com/todolist/exception/ExceptionType.java
@@ -10,7 +10,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ExceptionType {
 
-    EXAMPLE(HttpStatus.NOT_FOUND, "등록되지 않은 card Id입니다.");
+    NO_COOKIE(HttpStatus.NOT_FOUND, "쿠키가 없습니다. 사용자 정보를 먼저 요청하세요."),
+    NO_USER_ID_IN_COOKIE(HttpStatus.NOT_FOUND, "Cookie에 사용자 정보가 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
