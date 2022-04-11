537bd2035d8b7d3f6c7f409118dba90fad54b3b4
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-12 03:24:24 +0900

    [#27] Refactor: MemoCanvasView 내부의 MemoContainerView 하위 뷰 레이아웃 관련 로직 모두 제거
    - MemoContainerView의 관리를 MemoTableViewController에서 각각 하고 있기 때문에 MemoCanvasView 클래스가 더 이상 Dictionary로 참조할 필요가 없음
    - MemoContainerStackView의 오토레이아웃 설정 책임도 굳이 여기서 가지고 있을 필요 없이 뷰컨트롤러에서 가지도록 우선 제거

--- todo-list-ios/TODOList/TODOList/Views/Memo/MemoCanvasView.swift
+++ todo-list-ios/TODOList/TODOList/Views/Memo/MemoCanvasView.swift
@@ -11,43 +11,5 @@ class MemoCanvasView: UIView {
         stackView.distribution = .fillEqually
         return stackView
     }()
-    
-    private (set) var containerViews: [Identifier: MemoContainerView] = [:]
-    
-    required override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        addViews()
-        setConstraints()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        
-        addViews()
-        setConstraints()
-    }
-    
-    private func addViews() {
-        addSubview(memoContainerStackView)
-        for identifier in Identifier.allCases {
-            let containerView = MemoContainerView()
-            containerView.translatesAutoresizingMaskIntoConstraints = false
-            containerViews[identifier] = containerView
-            memoContainerStackView.addArrangedSubview(containerView)
-        }
-    }
-    
-    private func setConstraints() {
 
-        memoContainerStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 48).isActive = true
-        memoContainerStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        memoContainerStackView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.75).isActive = true
-        memoContainerStackView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
-        
-        for ( _ , containerView) in containerViews {
-            containerView.topAnchor.constraint(equalTo: memoContainerStackView.topAnchor).isActive = true
-            containerView.bottomAnchor.constraint(equalTo: memoContainerStackView.bottomAnchor).isActive = true
-        }
-    }
 }
