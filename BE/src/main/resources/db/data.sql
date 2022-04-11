458eaebf86a01831ca40a81278ef573d37a018a3
“kukim” <kukim.dev@gmail.com>
2022-04-11 11:57:34 +0900

    feat: History 테이블, dummy 데이터 생성 쿼리 작성, H2 DB In-Memory mode 변경

--- BE/src/main/resources/db/data.sql
+++ BE/src/main/resources/db/data.sql
@@ -7,3 +7,7 @@ VALUES ('Github 공부하기', 'add, commit, push', 'sam', 'todo', '2022-04-06T1
         '2022-04-07T20:30:00.000+09:00'),
        ('여자친구와 데이트', '초밥 먹으러 가기', 'sam', 'done', '2022-04-08T15:30:00.000+09:00',
         '2022-04-08T15:30:00.000+09:00');
+
+INSERT INTO HISTORY (TODOID, TODOTITLE, ACTION, FROMSTATUS, TOSTATUS, CREATEDAT)
+VALUES (1, 'Github 공부하기', 'add', '', '', '2022-04-06T15:30:00.000+09:00'),
+       (1, 'Github 공부하기', 'move', 'todo', 'doing', '2022-04-06T16:30:00.000+09:00');
--- BE/src/main/resources/db/schema.sql
+++ BE/src/main/resources/db/schema.sql
@@ -1,12 +1,26 @@
-DROP TABLE IF EXISTS TODO;
+DROP TABLE IF EXISTS TODO CASCADE;
 CREATE TABLE TODO
 (
     id           BIGINT NOT NULL AUTO_INCREMENT COMMENT 'Todo의 고유한 id',
-    title        VARCHAR(255) COMMENT '제목',
-    contents     VARCHAR(500) COMMENT '본문',
-    user         VARCHAR(100) COMMENT 'Todo 생성한 사용자',
-    status       VARCHAR(10) COMMENT 'Todo의 상태(todo, doing, done)',
-    created_time TIMESTAMP COMMENT 'Todo 생성 시간',
-    updated_time TIMESTAMP COMMENT 'Todo 업데이트 시간',
+    title        VARCHAR(255)                   COMMENT '제목',
+    contents     VARCHAR(500)                   COMMENT '본문',
+    user         VARCHAR(100)                   COMMENT 'Todo 생성한 사용자',
+    status       VARCHAR(10)                    COMMENT 'Todo의 상태(todo, doing, done)',
+    created_time TIMESTAMP                      COMMENT 'Todo 생성 시간',
+    updated_time TIMESTAMP                      COMMENT 'Todo 업데이트 시간',
     PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS HISTORY;
+CREATE TABLE HISTORY
+(
+    id           BIGINT NOT NULL AUTO_INCREMENT COMMENT 'History의 고유한 id',
+    todoId       BIGINT                         COMMENT '외래키 : Todo의 고유한 id',
+    todoTitle    VARCHAR(255)                   COMMENT 'todoId에 해당하는 title',
+    action       VARCHAR(10)                    COMMENT 'Todo의 상태(todo, doing, done)',
+    fromStatus   VARCHAR(10)                    COMMENT 'todo_id의 action (add, remove, update, move)',
+    toStatus     VARCHAR(10)                    COMMENT 'Todo의 상태(todo, doing, done)',
+    createdAt    TIMESTAMP                      COMMENT 'Todo 생성 시간',
+    PRIMARY KEY  (id),
+    FOREIGN KEY  (todoId) REFERENCES TODO(id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
