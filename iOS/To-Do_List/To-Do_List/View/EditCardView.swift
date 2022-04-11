97ec0fb8513a2e0fb859157bbf6bfdd155a304ea
P_SeoB <akdlsjtm21@naver.com>
2022-04-11 21:13:08 +0900

    [feat][#25] Add setHeadLineStyle func & Enum Type

--- iOS/To-Do_List/To-Do_List/View/EditCardView.swift
+++ iOS/To-Do_List/To-Do_List/View/EditCardView.swift
@@ -47,6 +47,11 @@ class EditCardView:UIView {
         setup()
     }
     
+    func setHeadLineLabel(editStyle:EditStyle) {
+        self.headLineLabel.text = editStyle.description
+    }
+    
+    
     private func addViews() {
         [headLineLabel,headLineInputTextField,contentInputTextField,cancelButton,confirmButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
@@ -76,4 +81,17 @@ class EditCardView:UIView {
     
     }
     
+}
+
+enum EditStyle:CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .add:
+            return "새로운 카드 추가"
+        case .editContent:
+            return "카드 수정"
+        }
+    }
+    case add
+    case editContent
 }
