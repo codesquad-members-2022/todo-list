e0be89c1c69d48c3b4a1ee9e60cf27e2f3b9ce90
seungchanHan <seungchanhan00@gmail.com>
2022-04-11 20:03:51 +0900

    style : 파일 이름 변경

--- FE/src/JS/todo/todo-model.js
+++ FE/src/JS/todo/todo-model.js
@@ -4,17 +4,14 @@ export default class Model {
   constructor(workListData) {
     this.userId = workListData.userId;
     this.workList = {};
-    this.categoryId= {};
+    this.categoryId = {};
     this.init(workListData);
   }
 
   init(workListData) {
-    for (let category of workListData.workList) {
+    for (const category of workListData.workList) {
       this.workList[category.categoryName] = category.works;
       this.categoryId[category.categoryName] = category.categoryId;
     }
-    console.log(this.workList)
-    console.log(this.categoryId)
   }
 }
-
