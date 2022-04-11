88c4fc4703355fdff1f5a016ad431770b35af1fd
ak2j38 <k2j38@naver.com>
2022-04-11 23:41:09 +0900

    fix: 커스텀 예외 수정
    1. RuntimeException 상속받도록 수정
    
    end
    
    Co-authored-by: dukcode <duk9741@gmail.com>

--- BE/src/main/java/com/team26/todolist/exception/EmptyCardStatusException.java
+++ BE/src/main/java/com/team26/todolist/exception/EmptyCardStatusException.java
@@ -1,6 +1,6 @@
 package com.team26.todolist.exception;
 
-public class EmptyCardStatusException extends Exception {
+public class EmptyCardStatusException extends RuntimeException {
     public EmptyCardStatusException(String message) {
         super(message);
     }
