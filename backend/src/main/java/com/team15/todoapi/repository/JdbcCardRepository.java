1590206d79857840248d22ad658d7fd97cc5dc1e
mybloom <baejungeun11@gmail.com>
2022-04-11 23:28:38 +0900

    refactor: JdbcTemplate에서 NamedParameterJdbcTemplate로 변경
    - ? placeholder 대신 변수명 사용하도록 JdbcTemplate 구현체 변경

--- backend/src/main/java/com/team15/todoapi/repository/JdbcCardRepository.java
+++ backend/src/main/java/com/team15/todoapi/repository/JdbcCardRepository.java
@@ -3,17 +3,20 @@ package com.team15.todoapi.repository;
 import com.team15.todoapi.domain.Card;
 import java.util.List;
 import javax.sql.DataSource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class JdbcCardRepository implements CardRepository{
+public class JdbcCardRepository implements CardRepository {
 
-	private final JdbcTemplate jdbcTemplate;
+	private final NamedParameterJdbcTemplate jdbcTemplate;
 
 	public JdbcCardRepository(DataSource dataSource) {
-		jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 	}
 
 	@Override
@@ -23,23 +26,30 @@ public class JdbcCardRepository implements CardRepository{
 				+ "title, "
 				+ "content, "
 				+ "modified_at, "
-				+ "user_id, "
+				+ "member_id, "
 				+ "card_section_code_id "
 				+ "from card , member "
 				+ "where card.member_id = member.id "
 				+ "and  delete_flag = false "
-				+ "order by modified_at desc", mapper());
+				+ "order by modified_at desc", rowMapper);
 	}
 
-	private RowMapper<Card> mapper(){
-		return (rs, rowNum) -> {
-			Card card = Card.of(rs.getLong("id"),
+	@Override
+	public int add(Card card) {
+		BeanPropertySqlParameterSource namedParameters = new BeanPropertySqlParameterSource(card);
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+
+		String sql = "INSERT INTO card "
+			+ "(id, title, content, created_at, modified_at, delete_flag, member_id, card_section_code_id) "
+			+ "VALUES (:id, :title, :content, now(), now(), false, :memberId, :section)";
+
+		return jdbcTemplate.update(sql, namedParameters, keyHolder, new String[]{"id"});
+	}
+
+	private static RowMapper<Card> rowMapper = (rs, rowNum) -> Card.of(rs.getLong("id"),
 				rs.getString("title"),
 				rs.getString("content"),
-				rs.getString("user_id"),
+				rs.getLong("member_id"),
 				rs.getTimestamp("modified_at").toLocalDateTime(),
 				rs.getInt("card_section_code_id"));
-			return card;
-		};
-	}
 }
