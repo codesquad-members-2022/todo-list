742de5dafc4745f6598016d0a6ddd4d81a7ddd49
함석호 <kalejuice02@gmail.com>
2022-04-11 12:05:14 +0900

    refactor : EventPublisher 클래스에 작성했던 static 메서드를 Action Enum 클래스로 이동

--- be/src/main/java/todolist/domain/event/Action.java
+++ be/src/main/java/todolist/domain/event/Action.java
@@ -1,5 +1,23 @@
 package todolist.domain.event;
 
+import todolist.dto.event.EventDto;
+
+import java.util.function.BiFunction;
+
 public enum Action {
-    ADD,MOVE,UPDATE, REMOVE
+    ADD((dto, action) -> new Event(dto.getTitle(), dto.getCurrentSection(), action)),
+    MOVE((dto, action) -> new Event(dto.getTitle(),dto.getPrevSection(), dto.getCurrentSection(), action)),
+    UPDATE((dto, action) -> new Event(dto.getTitle(), dto.getCurrentSection(), action)),
+    REMOVE((dto, action) -> new Event(dto.getTitle(), dto.getCurrentSection(), action));
+
+
+    private BiFunction<EventDto, Action, Event> eventFunction;
+
+    Action(BiFunction<EventDto, Action, Event> eventFunction) {
+        this.eventFunction = eventFunction;
+    }
+
+    public Event publish(EventDto eventDto) {
+        return eventFunction.apply(eventDto, this);
+    }
 }
