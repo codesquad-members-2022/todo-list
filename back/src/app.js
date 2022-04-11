2f44c12561bc83155958a688e45ba30932e660cc
Sangjin Park <58503584+healtheloper@users.noreply.github.com>
2022-04-11 21:56:56 +0900

    [BE] cors 해결

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
 
+import cors from "./middlewares/cors";
 import router from "./router";
 import swagger from "./swagger/swagger";
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(cors);
 app.use("/api-docs", swagger);
 app.use("/api", router);
 
