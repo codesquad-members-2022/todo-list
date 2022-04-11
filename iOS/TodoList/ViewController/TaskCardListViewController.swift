1745e4dac3349690f194b31a577adc5d3e5c6c77
sungju-kim <wnwn0719@gmail.com>
2022-04-11 14:41:59 +0900

    [#4] refactor: Task Card List 로직 변경

--- iOS/TodoList/ViewController/TaskCardListViewController.swift
+++ iOS/TodoList/ViewController/TaskCardListViewController.swift
@@ -2,23 +2,22 @@ import UIKit
 
 class TaskCardListViewController: UIViewController {
     
+    @IBOutlet var taskCardList: TaskCardListView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCell()
     }
-        
+    
     private func setupCell() {
-        if let taskListView = Bundle.main.loadNibNamed(NameSpace.nib.taskCardListView, owner: self, options: nil)?.first as? TaskCardListView {
-            taskListView.setTableView(with: self)
-            taskListView.translatesAutoresizingMaskIntoConstraints = false
-            
-            NSLayoutConstraint.activate([
-                taskListView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
-                taskListView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
-                taskListView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
-                taskListView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
-            ])
-        }
+        self.taskCardList.setTableView(with: self)
+        self.taskCardList.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.taskCardList.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            self.taskCardList.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            self.taskCardList.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            self.taskCardList.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+        
     }
     
 }
@@ -27,10 +26,10 @@ extension TaskCardListViewController: UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 15
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: NameSpace.identifier.taskCardViewCell, for: indexPath) as? TaskCardViewCell else { return UITableViewCell() }
         return cell
     }
-
+    
 }
