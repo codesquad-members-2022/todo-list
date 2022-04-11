ddb3b68bd91965af729717fa0787f50868becdb6
mybloom <baejungeun11@gmail.com>
2022-04-11 11:08:39 +0900

    chore: Controller에 @Slf4j로 로깅추가, DEBUG모드 작동
    - 현재는 DEBUG모드로 작동

--- backend/src/main/java/com/team15/todoapi/controller/CardController.java
+++ backend/src/main/java/com/team15/todoapi/controller/CardController.java
@@ -4,10 +4,12 @@ import com.team15.todoapi.domain.Card;
 import com.team15.todoapi.service.CardService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/cards")
 @RequiredArgsConstructor
