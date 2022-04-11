375a00563459c8e69884e14f47109fd70723eebb
inust33 <inust33@gmail.com>
2022-04-11 12:26:45 +0900

    Build: webpack dev-server 및 로더 설정

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,12 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { path } = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
-  },
+    static: "/dist",
+    compress: true
+  }
 });
