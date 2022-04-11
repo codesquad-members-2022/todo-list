2d5914673342aede12d7e9b08da55f5d7c0fefe1
Serin-Kim <serin9864@gmail.com>
2022-04-11 11:23:09 +0900

    Build: webpack devSever 설정

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,10 @@
-const { path } = require("path");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "/dist",
-    compress: true
-  }
+    static: './dist',
+  },
 });
