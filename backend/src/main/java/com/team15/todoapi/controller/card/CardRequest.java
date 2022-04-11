1867a5b4cd291e72ba41b80ebfe62b730847948f
mybloom <baejungeun11@gmail.com>
2022-04-11 23:25:25 +0900

    feat: Card Request용 DTO 추가
    - 카드 생성 시, request parameter로 받아 올 객체

--- backend/src/main/java/com/team15/todoapi/controller/card/CardRequest.java
+++ backend/src/main/java/com/team15/todoapi/controller/card/CardRequest.java
@@ -1,5 +1,16 @@
 package com.team15.todoapi.controller.card;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CardRequest {
+
+	private String title;
+	private String content;
+	private String userId;
+	private int section;
 
 }
