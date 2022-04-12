1fd9637e4de6248780cb493befa04ba39785878d
SeungHyun <fm10033@gmail.com>
2022-04-13 01:36:03 +0900

    fact :  삭제기능 model, controller, view 구현

--- build/src/javascript/controller/cardController.js
+++ build/src/javascript/controller/cardController.js
@@ -1,11 +1,12 @@
 import { getTodos } from "../model/cardModel.js";
 import { renderColumn } from "../view/cardView.js";
-
+import { handleAlertEvent } from "../view/deleteCardView.js";
 async function renderTodos() {
   const todos = await getTodos();
   renderColumn("#have-to-do-column", todos.haveToDoColumn);
   renderColumn("#doing-column", todos.doingColumn);
   renderColumn("#done-column", todos.doneColumn);
+  handleAlertEvent();
 }
 
 export { renderTodos };
