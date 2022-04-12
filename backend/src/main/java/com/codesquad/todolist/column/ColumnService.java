b64fe2156327e0ed6d7015e173c4e65a5f7e619b
Kang Changgu (Miller) <rkckr1@naver.com>
2022-04-13 06:33:27 +0900

    feat: [#53] Column 별 Card 출력을 위한 ColumnController, ColumnService 추가
    - ColumnService 의 `findAll()` 메서드에서 Column 별로 Card 리스트 구성 후 정렬

---
+++ backend/src/main/java/com/codesquad/todolist/column/ColumnService.java
@@ -0,0 +1,60 @@
+package com.codesquad.todolist.column;
+
+import com.codesquad.todolist.card.Card;
+import com.codesquad.todolist.card.CardRepository;
+import com.codesquad.todolist.column.dto.ColumnResponse;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ColumnService {
+
+    private final ColumnRepository columnRepository;
+    private final CardRepository cardRepository;
+
+    public ColumnService(ColumnRepository columnRepository, CardRepository cardRepository) {
+        this.columnRepository = columnRepository;
+        this.cardRepository = cardRepository;
+    }
+
+    public List<ColumnResponse> findAll() {
+        List<Column> columns = columnRepository.findAll();
+
+        List<Card> cards = cardRepository.findAll();
+
+        Map<Integer, List<Card>> allCardMap = cards.stream()
+            .collect(Collectors.groupingBy(Card::getColumnId));
+
+        for (Column column : columns) {
+            List<Card> columnCards = allCardMap.get(column.getColumnId());
+
+            if (columnCards == null || columnCards.size() == 0) {
+                continue;
+            }
+
+            Map<Integer, Card> columnCardMap = columnCards.stream()
+                .collect(Collectors.toMap(Card::getNextId, Function.identity()));
+
+            Card card = Optional.ofNullable(columnCardMap.remove(null))
+                .orElseThrow(() -> new IllegalStateException("조건을 만족하는 카드가 없습니다."));
+
+            List<Card> sortedCards = new ArrayList<>(List.of(card));
+
+            while (columnCardMap.size() > 0) {
+                card = columnCardMap.remove(card.getCardId());
+                sortedCards.add(card);
+            }
+            Collections.reverse(sortedCards);
+            column.setCards(sortedCards);
+        }
+        return columns.stream()
+            .map(ColumnResponse::from)
+            .collect(Collectors.toList());
+    }
+}
