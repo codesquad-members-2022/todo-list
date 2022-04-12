38d45f1b1f5d468dcc0c4ef8d2ad95e52f2dedd9
Sol <hskim6521@naver.com>
2022-04-12 12:39:02 +0900

    [#18] Style: Card 객체의 id 속성 타입 변경 및 ID 타입 제거, 카드리스트에서의 카드 셀의 위치를 서버에 알릴 때 사용될 indexOrder 속성 추가

--- ToDoList/ToDoList/Model/Card.swift
+++ ToDoList/ToDoList/Model/Card.swift
@@ -1,16 +1,15 @@
 import Foundation
 
 struct Card: Cardable {
-    private let id: ID
     private(set) var title: String
     private(set) var body: String
     private(set) var listName: String
     private(set) var caption: Caption
+    private(set) var indexOrder: Int?
     private let createdTime: Date
     private var updatedTime: Date
     
     init(title: String, body: String, caption: Caption, listName: String) {
-        self.id = ID()
         self.title = title
         self.body = body
         self.caption = caption
@@ -22,6 +21,9 @@ struct Card: Cardable {
     mutating func moveList(to newListName: String) {
         self.listName = newListName
         self.updatedTime = .now
+    }
+    mutating func setIndexOrder(in cardsCount: Int) {
+        self.indexOrder = (cardsCount+1) * 1000
     }
 }
 
