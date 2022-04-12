d190caf1c0ce207c5d1afb2956edb60a9f322ce8
Song TaeHwan <axoghks@gmail.com>
2022-04-12 23:52:17 +0900

    feat: TodoCell 에 swipe to delete 기능 추가

--- ios/TodoApp/TodoApp/Controllers/TodoListViewController.swift
+++ ios/TodoApp/TodoApp/Controllers/TodoListViewController.swift
@@ -10,22 +10,28 @@ import OSLog
 
 class TodoListViewController: UIViewController {
     // TODO: Todo 모델 정의 & 소유
-
+    
+    @IBOutlet weak var badgeLabel: UILabel!
     @IBOutlet private weak var badgeView: UIView!
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var addToDoButton: UIButton!
+    
+    // Temporary usage, must be removed in the future
+    private var viewModel = Array(repeating: 0, count: 10)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureUI()
         self.addDelegates()
     }
-
+    
     private func addDelegates() {
-        tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
     
     private func configureUI() {
+        self.badgeLabel?.text = String(self.viewModel.count)
         self.badgeView.clipsToBounds = true
         self.badgeView.layer.cornerRadius = self.badgeView.frame.size.height / 2
         
@@ -37,11 +43,6 @@ class TodoListViewController: UIViewController {
 }
 
 extension TodoListViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TodoCell.identifier) as? TodoCell else {
             Logger.view.error("Fail to get a cell instance of TodoCell in \(#function), \(#fileID)")
@@ -49,5 +50,40 @@ extension TodoListViewController: UITableViewDataSource {
         }
         
         return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.viewModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+}
+
+extension TodoListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.tableView.beginUpdates()
+            self.viewModel.remove(at: indexPath.section)
+            self.tableView.deleteRows(at: [indexPath], with: .left)
+            self.tableView.deleteSections(IndexSet(integer: indexPath.section), with: .left)
+            self.badgeLabel?.text = String(self.viewModel.count)
+            self.tableView.endUpdates()
+        }
     }
 }
