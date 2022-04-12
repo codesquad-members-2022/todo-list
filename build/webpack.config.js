0a903668afcb697c82d085370fd7d32a7854133a
SeungHyun <fm10033@gmail.com>
2022-04-12 14:42:59 +0900

    Refactor : bundle.js찾지 못하는 오류 수정

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development",
   entry: "./src/javascript/app.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "src/dist"),
     filename: "bundle.js",
   },
   devtool: "eval-source-map",
