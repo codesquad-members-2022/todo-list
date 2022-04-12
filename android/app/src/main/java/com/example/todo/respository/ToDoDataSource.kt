9c02ecaea7d0355401ce08b0ccbb0beb1d46a38c
shim506 <shim506@naver.com>
2022-04-12 15:57:41 +0900

    Feat: Todo 레파지토리 Add 부분 구현
    - 레파지토리 , 라피자토리 소스 에 대한 구현

--- android/app/src/main/java/com/example/todo/respository/ToDoDataSource.kt
+++ android/app/src/main/java/com/example/todo/respository/ToDoDataSource.kt
@@ -1,7 +1,11 @@
 package com.example.todo.respository
 
 import com.example.todo.model.TodoItem
+import com.example.todo.network.CardIndex
+import retrofit2.Response
 
 interface ToDoDataSource {
-
+    suspend fun getTodoId(newItem: TodoItem): Response<CardIndex>
+    suspend fun getInProgressId(newItem: TodoItem): Response<CardIndex>
+    suspend fun getDoneId(newItem: TodoItem): Response<CardIndex>
 }
--- android/app/src/main/java/com/example/todo/respository/ToDoRemoteDataSource.kt
+++ android/app/src/main/java/com/example/todo/respository/ToDoRemoteDataSource.kt
@@ -1,8 +1,23 @@
 package com.example.todo.respository
 
+import android.util.Log
 import com.example.todo.model.TodoItem
+import com.example.todo.network.CardIndex
 import com.example.todo.network.RetrofitClient
+import retrofit2.Response
 
-class ToDoRemoteDataSource(private val retrofitClient: RetrofitClient,): ToDoDataSource {
+class ToDoRemoteDataSource(private val retrofitClient: RetrofitClient) : ToDoDataSource {
+    override suspend fun getTodoId(newItem: TodoItem): Response<CardIndex> {
+        return retrofitClient.getCardIdx(newItem.title, newItem.content, "Jay", "todo")
+    }
+
+    override suspend fun getInProgressId(newItem: TodoItem): Response<CardIndex> {
+        return retrofitClient.getCardIdx(newItem.title, newItem.content, "Jay", "inProgress")
+    }
+
+    override suspend fun getDoneId(newItem: TodoItem): Response<CardIndex> {
+        return retrofitClient.getCardIdx(newItem.title, newItem.content, "Jay", "done")
+        //val result = retrofitClient.getCardIdx(newItem.title, newItem.content, "Jay", "done")
+    }
 
 }
