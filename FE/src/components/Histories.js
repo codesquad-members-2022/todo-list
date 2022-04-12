b0b57abb5945732ca321b96a19968e15b9ea7b9d
yongseongjeon <yongseong.jeon95@gmail.com>
2022-04-12 15:04:12 +0900

    refactor: 더미 데이터를 불러와서 사용

--- FE/src/components/Histories.js
+++ FE/src/components/Histories.js
@@ -1,43 +1,27 @@
-import HistoryCard from './HistoryCard.js';
-import { historyStore } from '../store/HistoryStore.js';
+import HistoryCard from "./HistoryCard.js";
+import { historyStore } from "../store/HistoryStore.js";
+import { GET_HISTORY } from "../dummyData.js";
 
 export default class Histories {
   constructor(target) {
     this.target = target;
-    const histories = [
-      {
-          "cardAction": "MOVE",
-          "userId": "test1",
-          "cardTitle": "카드 제목 1",
-          "cardTitleBefore": "카드 제목 1",
-          "cardStatus": "TODO",
-          "cardStatusBefore": "ONGOING",
-          "createdAt": "2022-04-11T10:52:29.248343"
-      },
-      {
-          "cardAction": "UPDATE",
-          "userId": "test2",
-          "cardTitle": "카드 제목 2",
-          "cardTitleBefore": "카드 제목 이전 2",
-          "cardStatus": "DONE",
-          "cardStatusBefore": "DONE",
-          "createdAt": "2022-04-11T10:52:29.24877"
-      },
-    ];
-    this.historyCardComponents = histories.map((history) => new HistoryCard(history));
-    historyStore.initState('histories');
-    historyStore.subscribe('histories', this.render.bind(this));
-    historyStore.setState('histories', histories);
+    this.historyCardComponents = GET_HISTORY.map(
+      (history) => new HistoryCard(history)
+    );
+    historyStore.initState("history");
+    historyStore.subscribe("history", this.render.bind(this));
+    historyStore.setState("history", GET_HISTORY);
   }
-  init() {
-    
-  }
+  init() {}
 
   template() {
     return `
         <button class="history__btn--close">x</button>
-        ${this.historyCardComponents.reduce((prev, h) =>prev + h.template(), '')}
-    `
+        ${this.historyCardComponents.reduce(
+          (prev, h) => prev + h.template(),
+          ""
+        )}
+    `;
   }
   render() {
     this.target.innerHTML = this.template();
