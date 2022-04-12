aeb6f0da3b48716bb05ca0ab8edb043b15095521
PhilSoGooood <peelhw@gmail.com>
2022-04-12 11:48:30 +0900

    feat: Log 테이블 변경
    상의 결과 type구분 없이 String으로 저장하기로 하여 변경하였습니다.
    칼럼과 action 등은 text에 저장합니다.

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -15,9 +15,7 @@ CREATE INDEX column_deleted_comp ON CARD(column_name, deleted);
 CREATE TABLE ACTION_LOG
 (
     id                  int AUTO_INCREMENT PRIMARY KEY,
-    title               varchar(50) NOT NULL,
-    cur_column_name     ENUM('to_do', 'in_progress', 'done'),
-    prev_column_name    ENUM('to_do', 'in_progress', 'done') default NULL,
-    action_type         ENUM('add', 'remove', 'update', 'move'),
+    author              varchar(20) NOT NULL,
+    text                varchar(500) NOT NULL,
     created_date        DATETIME DEFAULT CURRENT_TIMESTAMP
 );
