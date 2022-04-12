9cb5284ea3a2923623e6c774ea2807651dfaf03d
Muffin <jinlog9@gmail.com>
2022-04-12 17:25:26 +0900

    feat: 모달 UI 개선, 기능 구현

--- components/Modal.js
+++ components/Modal.js
@@ -1,17 +1,27 @@
 import { getLocalStorageByKey } from '../utils/localStorage.js';
 import { $ } from '../utils/dom.js';
+import { popupCloseText, popupOkText } from '../constants/modal.js';
 export default class Modal {
-  constructor(id) {
+  constructor(id, title, handleMinusCount) {
     this.id = id;
+    this.title = title;
+    this.onOk = false;
+    this.handleMinusCount = handleMinusCount;
   }
 
   render = () => {
     return /*html */ `
       <div class="modal">
-        <p>정말 삭제하시겠습니까?</p>
-        <button class="modal--close">취소</button>
-        <button class="modal--remove">삭제</button>
+        <p class="modal__title">${this.title}</p>
+        <div class="modal-button__wrapper">
+          <button class="modal__button modal--close">${popupCloseText}</button>
+          <button class="modal__button modal--remove">${popupOkText}</button>
+        </div>
       </div>`;
+  };
+
+  getOnOK = () => {
+    return this.onOk;
   };
 
   showModal = () => {
@@ -25,8 +35,13 @@ export default class Modal {
   };
 
   deleteButton = () => {
+    this.onOk = true;
+    /** TODO
+     * TODO: onOk 속성으로 아래 코드는 모달 클래스가 아닌 상위 클래스에서 해주도록 변경필요
+     * */
     const filteredTodos = getLocalStorageByKey('todos').filter(e => e.id !== Number(this.id));
     localStorage.setItem('todos', JSON.stringify(filteredTodos));
+    this.handleMinusCount();
 
     document.getElementById(`${this.id}`)?.remove();
     this.closeButton();
