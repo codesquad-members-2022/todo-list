1ddfd2e16ed26e33cecac8c81ca05a7b355b3cf3
dlwnstjrzz <95525638+dlwnstjrzz@users.noreply.github.com>
2022-04-12 23:36:27 +0900

    Feat: "드래그앤드랍 구현"
    드래그앤드랍 컬럼간 이동 구현함.
    
    Related to: #10

--- client/src/js/dragAndDrop.js
+++ client/src/js/dragAndDrop.js
@@ -1,4 +1,4 @@
-import { closest } from './util';
+import { $, $$, closest } from './util';
 
 export class DragAndDrop {
   constructor() {
@@ -9,6 +9,8 @@ export class DragAndDrop {
 
     this.dragCard = null;
     this.clonedDragCard = null;
+    this.cardPositionInfo = [];
+    this.moveCardState = false;
   }
 
   init() {
@@ -21,33 +23,135 @@ export class DragAndDrop {
     if (closest('.default', e.target)) {
       this.isDragging = true;
       this.dragCard = closest('.default', e.target);
+      this.column = closest('.column-list', e.target);
     }
   };
 
   mouseupEventHandler = (e) => {
     this.isDragging = false;
+    this.moveCardState = false;
     if (this.isClone) {
       this.clonedDragCard.remove();
       this.dragCard.classList.replace('place', 'default');
       this.isClone = null;
+      this.cardPositionInfo = [];
     }
   };
 
   mousemoveEventHandler = (e) => {
-    if (this.isDragging) {
-      if (!this.isClone) {
-        this.clonedDragCard = this.dragCard.cloneNode(true);
-        closest('.column-list', e.target).appendChild(this.clonedDragCard);
-        this.clonedDragCard.classList.add('start-drag');
-        this.dragCard.classList.replace('default', 'place');
-        this.clonedDragCard.classList.replace('default', 'drag');
-        this.isClone = true;
-      }
+    if (!this.isDragging) return;
 
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
-      this.clonedDragCard.style.left = mouseX + 'px';
-      this.clonedDragCard.style.top = mouseY + 'px';
+    if (!this.isClone) {
+      this.setCardPosition();
+      this.cloneCard(e);
     }
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    this.clonedDragCard.style.left = mouseX + 'px';
+    this.clonedDragCard.style.top = mouseY + 'px';
+
+    const cloneCardClientRect = this.clonedDragCard.getBoundingClientRect();
+    const columnClientRect = this.column.getBoundingClientRect();
+
+    const draggedX = cloneCardClientRect.x;
+    const draggedY = cloneCardClientRect.y;
+
+    if (
+      columnClientRect.x - cloneCardClientRect.x >
+      cloneCardClientRect.width / 2
+    ) {
+      $(
+        '.column-list',
+        this.column.parentElement.previousElementSibling
+      ).appendChild(this.dragCard);
+    } else if (
+      columnClientRect.x + columnClientRect.width - cloneCardClientRect.x <
+      cloneCardClientRect.width / 2
+    ) {
+      $(
+        '.column-list',
+        this.column.parentElement.nextElementSibling
+      ).appendChild(this.dragCard);
+    }
+
+    this.cardPositionInfo.forEach((el) => {
+      const prevCard = el.x - draggedX > 0 ? cloneCardClientRect : el;
+      const curCard = el.x - draggedX > 0 ? el : cloneCardClientRect;
+
+      const higherCard = el.y - draggedY > 0 ? cloneCardClientRect : el;
+      const lowerCard = el.y - draggedY > 0 ? el : cloneCardClientRect;
+
+      if (
+        this.dragCard.getBoundingClientRect().x === el.x &&
+        !this.moveCardState
+      ) {
+        if (prevCard.x + prevCard.width - curCard.x > prevCard.width / 2) {
+          if (
+            cloneCardClientRect.y < el.y &&
+            higherCard.y + higherCard.height - lowerCard.y >
+              lowerCard.height / 2
+          ) {
+            el.positionedCard.insertAdjacentElement('afterend', this.dragCard);
+          }
+
+          if (
+            cloneCardClientRect.y > el.y &&
+            higherCard.y + higherCard.height - lowerCard.y >
+              higherCard.height / 2
+          ) {
+            el.positionedCard.insertAdjacentElement(
+              'beforebegin',
+              this.dragCard
+            );
+          }
+        }
+      } else {
+        this.moveCardState = true;
+
+        if (prevCard.x + prevCard.width - curCard.x > prevCard.width / 2) {
+          if (
+            cloneCardClientRect.y < el.y &&
+            higherCard.y + higherCard.height - lowerCard.y >
+              lowerCard.height / 2
+          ) {
+            el.positionedCard.insertAdjacentElement(
+              'beforebegin',
+              this.dragCard
+            );
+          }
+
+          if (
+            cloneCardClientRect.y > el.y &&
+            higherCard.y + higherCard.height - lowerCard.y >
+              higherCard.height / 2
+          ) {
+            el.positionedCard.insertAdjacentElement('afterend', this.dragCard);
+          }
+        }
+      }
+    });
   };
+
+  setCardPosition() {
+    for (const li of $$('.list_item')) {
+      this.cardPositionInfo.push({
+        x: li.getBoundingClientRect().x,
+        y: li.getBoundingClientRect().y,
+        width: li.getBoundingClientRect().width,
+        height: li.getBoundingClientRect().height,
+        positionedCard: li,
+      });
+    }
+  }
+
+  cloneCard(e) {
+    this.clonedDragCard = this.dragCard.cloneNode(true);
+    closest('.column-list', e.target).appendChild(this.clonedDragCard);
+    this.clonedDragCard.classList.add('start-drag');
+    this.clonedDragCard.classList.replace('default', 'drag');
+    this.dragCard.classList.replace('default', 'place');
+
+    this.isClone = true;
+  }
 }
