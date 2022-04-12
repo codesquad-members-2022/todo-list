7e2355b903679a2d1c222f6fcdb25a15d0759505
bangdler <zbthz90@gmail.com>
2022-04-12 17:47:42 +0900

    refactor: mouseMoveOnDraggingEventHandler 내부 로직을 작은 함수로 쪼갬

--- js/components/schedule/scheduleCardDrag.js
+++ js/components/schedule/scheduleCardDrag.js
@@ -1,8 +1,100 @@
 const $main = document.querySelector("#main");
-
 let dragCard;
-let afterImageCard;
-let $scheduleCardBelow;
+let afterimageCard;
+let $scheduleCardBelowDragPointer;
+const TOP = "top";
+const BOTTOM = "bottom";
+
+const getDragDirection = (element, y) => {
+    const box = element.getBoundingClientRect();
+    const offset = y - (box.top + box.height / 2);
+    return offset < 0 ? TOP : BOTTOM;
+};
+
+const getElementBelowDragPointer = (event) => {
+    dragCard.style.display = "none";
+    const $elementBelowDragPointer = document.elementFromPoint(
+        event.clientX,
+        event.clientY
+    );
+    dragCard.style.display = "flex";
+
+    return $elementBelowDragPointer;
+};
+
+const appendAfterImageCardInEmptyCardContainer = ($cardContainer) => {
+    if ($cardContainer.children.length) {
+        return;
+    }
+    const tempAfterImageCard = afterimageCard;
+    afterimageCard.remove();
+    $cardContainer.appendChild(tempAfterImageCard);
+    afterimageCard = tempAfterImageCard;
+};
+
+const moveAfterImageCard2DragPoint = (
+    $scheduleCardBelowDragPointer,
+    location
+) => {
+    const tempAfterImageCard = afterimageCard;
+    switch (location) {
+        case TOP: {
+            afterimageCard.remove();
+            $scheduleCardBelowDragPointer.before(tempAfterImageCard);
+            afterimageCard = tempAfterImageCard;
+            break;
+        }
+        case BOTTOM: {
+            afterimageCard.remove();
+            $scheduleCardBelowDragPointer.after(tempAfterImageCard);
+            afterimageCard = tempAfterImageCard;
+            break;
+        }
+    }
+};
+
+const mouseMoveOnDraggingEventHandler = (event) => {
+    relocateDragCard(event.pageX, event.pageY);
+    const $elementBelowDragPointer = getElementBelowDragPointer(event);
+
+    if (!$elementBelowDragPointer) {
+        return;
+    }
+
+    if (
+        $elementBelowDragPointer.classList.contains(
+            "schedule-column__cards-container"
+        )
+    ) {
+        appendAfterImageCardInEmptyCardContainer($elementBelowDragPointer);
+        return;
+    }
+
+    if (!$elementBelowDragPointer.classList.contains("schedule-card")) {
+        return;
+    }
+
+    $scheduleCardBelowDragPointer = $elementBelowDragPointer;
+    const dragDirection = getDragDirection(
+        $scheduleCardBelowDragPointer,
+        event.pageY
+    );
+    moveAfterImageCard2DragPoint($scheduleCardBelowDragPointer, dragDirection);
+};
+
+const removeMouseUpEvent = () => {
+    $main.removeEventListener("mouseup", mouseUpOnDraggingEventHandler);
+};
+
+const mouseUpOnDraggingEventHandler = () => {
+    $main.removeEventListener("mousemove", mouseMoveOnDraggingEventHandler);
+    removeMouseUpEvent();
+};
+
+const addMouseEventOnDragCard = () => {
+    $main.addEventListener("mousemove", mouseMoveOnDraggingEventHandler);
+    $main.addEventListener("mouseup", mouseUpOnDraggingEventHandler);
+};
 
 const isValid2Drag = (target, selectedCard) => {
     const dragCard = target.closest(".schedule-drag-card");
@@ -13,62 +105,6 @@ const isValid2Drag = (target, selectedCard) => {
 const relocateDragCard = (pageX, pageY) => {
     dragCard.style.left = `${pageX - dragCard.offsetWidth / 2}px`;
     dragCard.style.top = `${pageY - dragCard.offsetHeight / 2}px`;
-};
-
-const addMouseEventOnDragCard = () => {
-    $main.addEventListener("mousemove", mouseMoveOnDraggingEventHandler);
-    $main.addEventListener("mouseup", mouseUpOnDraggingEventHandler);
-};
-
-const getLocation = (element, y) => {
-    const box = element.getBoundingClientRect();
-    const offset = y - (box.top + box.height / 2);
-    return offset < 0 ? "top" : "bottom";
-};
-
-const mouseUpOnDraggingEventHandler = () => {
-    $main.removeEventListener("mousemove", mouseMoveOnDraggingEventHandler);
-    removeMouseUpEvent();
-};
-
-const removeMouseUpEvent = () => {
-    $main.removeEventListener("mouseup", mouseUpOnDraggingEventHandler);
-};
-
-const mouseMoveOnDraggingEventHandler = (event) => {
-    relocateDragCard(event.pageX, event.pageY);
-
-    dragCard.style.display = "none";
-    const $newScheduleCardBelow = document.elementFromPoint(
-        event.clientX,
-        event.clientY
-    );
-    dragCard.style.display = "flex";
-
-    if (
-        !$newScheduleCardBelow ||
-        !$newScheduleCardBelow.classList.contains("schedule-card")
-    ) {
-        return;
-    }
-
-    $scheduleCardBelow = $newScheduleCardBelow;
-    const location = getLocation($scheduleCardBelow, event.pageY);
-    const tempAfterImageCard = afterImageCard;
-    switch (location) {
-        case "top": {
-            afterImageCard.remove();
-            $scheduleCardBelow.before(tempAfterImageCard);
-            afterImageCard = tempAfterImageCard;
-            break;
-        }
-        case "bottom": {
-            afterImageCard.remove();
-            $scheduleCardBelow.after(tempAfterImageCard);
-            afterImageCard = tempAfterImageCard;
-            break;
-        }
-    }
 };
 
 export const mouseDownEventHandler = (event) => {
@@ -82,12 +118,11 @@ export const mouseDownEventHandler = (event) => {
     const CARD_AFTER_IMAGE = "schedule-card--afterimage";
     const DRAG_CARD = "schedule-drag-card";
     dragCard = selectedCard.cloneNode(true);
-    const $cardsContainer = target.closest(".schedule-column__cards-container");
 
     selectedCard.classList.replace(CARD, CARD_AFTER_IMAGE);
     dragCard.classList.replace(CARD, DRAG_CARD);
-    $cardsContainer.appendChild(dragCard);
-    afterImageCard = selectedCard;
+    $main.appendChild(dragCard);
+    afterimageCard = selectedCard;
 
     relocateDragCard(event.pageX, event.pageY);
     addMouseEventOnDragCard();
