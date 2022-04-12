c1e37915d589c3c4b3e57661198524a41f98837c
yongseongjeon <yongseong.jeon95@gmail.com>
2022-04-12 15:03:16 +0900

    feat: 더미 데이터 추가

---
+++ FE/src/dummyData.js
@@ -0,0 +1,44 @@
+export const GET_CARD = [
+  {
+    id: 1,
+    userId: "damon",
+    title: "github 공부하기",
+    contents: "add, commit, push 공부",
+    cardStatusName: "해야할 일",
+  },
+  {
+    id: 2,
+    userId: "ader",
+    title: "공부하기",
+    contents: "뭐든 좀 하기",
+    cardStatusName: "해야할 일",
+  },
+  {
+    id: 3,
+    userId: "honux",
+    title: "운동하기",
+    contents: "주짓수 하러가기",
+    cardStatusName: "해야할 일",
+  },
+];
+
+export const GET_HISTORY = [
+  {
+    cardAction: "MOVE",
+    userId: "test1",
+    cardTitle: "카드 제목 1",
+    cardTitleBefore: "카드 제목 1",
+    cardStatus: "TODO",
+    cardStatusBefore: "ONGOING",
+    createdAt: "2022-04-11T10:52:29.248343",
+  },
+  {
+    cardAction: "UPDATE",
+    userId: "test2",
+    cardTitle: "카드 제목 2",
+    cardTitleBefore: "카드 제목 이전 2",
+    cardStatus: "DONE",
+    cardStatusBefore: "DONE",
+    createdAt: "2022-04-11T10:52:29.24877",
+  },
+];
