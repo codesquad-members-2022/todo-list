96c65c49a1e7fa06d16ae8fc1326aa4937734573
Kyle Choi <tmdfl1027@gmail.com>
2022-04-12 11:23:53 +0900

    [#24] TaskController 200 응답 시 리스폰스 객체 타입 삭제
    200 응답 시 body에 Task 객체 없이 헤더만 응답하는 것으로 변경하여 해당 내용 삭제.

--- todo-list-server/src/main/java/kr/codesquad/todo/controller/TaskController.java
+++ todo-list-server/src/main/java/kr/codesquad/todo/controller/TaskController.java
@@ -20,7 +20,7 @@ public class TaskController {
 
     @ApiOperation(value = "추가", notes = "일 목록에 태스크를 추가합니다.\n 1)태스크 제목(title), 2)태스크 내용(content), 3)작성자 닉네임, 4)태스크 상태(status)는 필수값입니다. ")
     @ApiResponses({
-            @ApiResponse(code = 200, message = "요청이 성공했습니다.", response = Task.class),
+            @ApiResponse(code = 200, message = "요청이 성공했습니다."),
             @ApiResponse(code = 400, message = "잘못 된 요청 입니다."),
             @ApiResponse(code = 500, message = "서버에서 발생한 에러입니다.")
     })
