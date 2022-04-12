444ebd7007660695bd19c5374ac8562428efeccb
“kukim” <kukim.dev@gmail.com>
2022-04-12 16:27:48 +0900

    refactor: Todo, History 컨트롤러의 불필요한 autowired 제거, @RequestMapping("api") -> "/api" 변경
    - TodoController의 TodoService final 추가

--- BE/src/main/java/codesquad/be/todoserver/controller/HistoryController.java
+++ BE/src/main/java/codesquad/be/todoserver/controller/HistoryController.java
@@ -3,18 +3,16 @@ package codesquad.be.todoserver.controller;
 import codesquad.be.todoserver.domain.History;
 import codesquad.be.todoserver.service.HistoryService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 public class HistoryController {
 
 	private final HistoryService historiesService;
 
-	@Autowired
 	public HistoryController(HistoryService historiesService) {
 		this.historiesService = historiesService;
 	}
--- BE/src/main/java/codesquad/be/todoserver/controller/TodoController.java
+++ BE/src/main/java/codesquad/be/todoserver/controller/TodoController.java
@@ -2,18 +2,17 @@ package codesquad.be.todoserver.controller;
 
 import codesquad.be.todoserver.domain.Todo;
 import codesquad.be.todoserver.service.TodoService;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 public class TodoController {
 
-	private TodoService todoService;
+	private final TodoService todoService;
 
 	public TodoController(TodoService todoService) {
 		this.todoService = todoService;
