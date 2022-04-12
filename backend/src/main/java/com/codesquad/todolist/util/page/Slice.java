f72138c720cda94c5780ca51dfb78dc9ceadb4d0
Kang Changgu (Miller) <rkckr1@naver.com>
2022-04-12 22:55:53 +0900

    feat: [#49] 페이징을 위한 Slice, Criteria 객체 추가

---
+++ backend/src/main/java/com/codesquad/todolist/util/page/Slice.java
@@ -0,0 +1,27 @@
+package com.codesquad.todolist.util.page;
+
+import java.util.List;
+
+public class Slice<T> {
+
+    private final boolean hasNext;
+    private final List<T> data;
+
+    public Slice(List<T> data, Criteria criteria) {
+        System.out.println("size :" + data.size());
+
+        int index = Math.min(data.size(), criteria.getSize());
+
+        this.hasNext = data.size() > criteria.getSize();
+        this.data = data.subList(0, index);
+    }
+
+    public boolean getHasNext() {
+        return hasNext;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+}
