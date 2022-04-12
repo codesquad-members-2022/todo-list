e0d380e407ec539156e6e34a4b7bae892cd34d68
soralee <sora2821@gmail.com>
2022-04-12 11:48:22 +0900

    Refactor : PR 피드백 dev 브랜치에 반영

--- build/src/javascript/util/util.js
+++ build/src/javascript/util/util.js
@@ -1,48 +1,44 @@
-import axios from "https://cdn.skypack.dev/axios";
+// import axios from "https://cdn.skypack.dev/axios";
 
 export async function getData(URL) {
-  const data = await axios.get(URL).then((response) => response.data);
+  const response = await axios.get(URL);
+  const data = response.data;
   return data;
 }
 
 export async function postData(URL, updatedData) {
-  const data = await axios
-    .post(URL, updatedData)
-    .then((response) => response.data);
-  return data;
+  await axios.post(URL, updatedData);
 }
 
 export async function putData(URL, updatedData) {
-  const data = await axios
-    .put(URL, updatedData)
-    .then((response) => response.data);
-  return data;
+  await axios.put(URL, updatedData);
 }
 
 export async function deleteData(URL) {
   await axios.delete(URL);
 }
 
-export async function axiosRequest(request, dataName, data = {}) {
-  const URL = `http://localhost:5000/${dataName}`;
-  const idURL = URL + `/${data.id}`;
+export function axiosRequest(method, pathName, controlledData = {}) {
+  const URL = `http://localhost:5000/${pathName}`;
+  const idURL = URL + `/${controlledData.id}`;
   let response;
-  switch (request) {
+  switch (method) {
     case "get":
-      response = await getData(URL);
+      const sortingURL = URL + "?_sort=";
+      response = getData(URL);
       return response;
     case "post":
-      await postData(URL, data);
+      postData(URL, controlledData);
       break;
     case "put":
-      await putData(idURL, data);
+      putData(idURL, controlledData);
       break;
     case "delete":
-      await deleteData(idURL);
+      deleteData(idURL);
       break;
   }
 }
 
-export function removeText($) {
-  $.innerText = "";
+export function removeText({ target }) {
+  target.innerText = "";
 }
