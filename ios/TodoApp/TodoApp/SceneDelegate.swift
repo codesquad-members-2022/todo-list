3aa709f59d1fb5633e29ccb395f54e1704b7a4a8
Song TaeHwan <axoghks@gmail.com>
2022-04-12 12:33:21 +0900

    refactor: ScceneDelegate 에서 코드로 앱 초기화 작업 및 Main.storyboard 삭제

--- ios/TodoApp/TodoApp/SceneDelegate.swift
+++ ios/TodoApp/TodoApp/SceneDelegate.swift
@@ -12,7 +12,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(windowScene: scene)
+        
+        let todoContainer = UIStoryboard(name: "TodoListContainerViewController", bundle: nil).instantiateInitialViewController() as? TodoListContainerViewController
+        
+        // TODO: Column 데이터 Fetching
+        // TODO: 컨테이너에 TodoListViewController 주입하기
+
+        self.window?.rootViewController = todoContainer
+        self.window?.makeKeyAndVisible()
+        
     }
 }
 
