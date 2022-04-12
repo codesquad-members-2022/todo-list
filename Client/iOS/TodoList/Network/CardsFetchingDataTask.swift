eac8fc4341848c9c669cf24aaf3e1571b873cc47
Jinsujin <sylpid003@gmail.com>
2022-04-12 14:43:48 +0900

    feat: 서버에 user 정보 요청 하는 API 구현 (#22)
    Response 의 header 에 있는 쿠키 정보를 가져와 HTTPCookieStorage 에 저장(set)

--- Client/iOS/TodoList/Network/CardsFetchingDataTask.swift
+++ Client/iOS/TodoList/Network/CardsFetchingDataTask.swift
@@ -31,6 +31,31 @@ class DataTask: SessionDataTask {
         super.init(as: urlString, using: delegate, in: queue, type: type)
     }
     
+    func fetchUser(completionHandler: @escaping (Result<String,DataTaskError>) -> Void) {
+        guard let url = api.toURL(type: .user) else {
+            completionHandler(.failure(.invalidURL))
+            return
+        }
+        let request = URLRequest(url: url)
+        self.session.dataTask(with: request) { data, response, error in
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+               let fields = httpResponse.allHeaderFields as? [String: String] else {
+                completionHandler(.failure(.notConvertdecode))
+                return
+            }
+
+            let cookies = HTTPCookie.cookies(withResponseHeaderFields: fields, for: url)
+            HTTPCookieStorage.shared.setCookies(cookies, for: response?.url, mainDocumentURL: nil)
+            guard let cookie_userId = cookies.filter({ $0.name == "userId" }).first else {
+                completionHandler(.failure(.notConvertdecode))
+                return
+            }
+            completionHandler(.success(cookie_userId.value))
+        }.resume()
+    }
+    
+    
     func fetchAll<T: Codable>(dataType: T.Type, completionHandler: @escaping (Result<T,DataTaskError>) -> Void) {
         guard let url = api.toURL(type: .all) else {
             completionHandler(.failure(.invalidURL))
@@ -89,7 +114,7 @@ protocol ServerAPI {
 extension ServerAPI {
     func getUrlString(type: URLType) -> String {
         switch type {
-        case .all:
+        case .all, .user:
             return endpoint+"\(type)"
         }
     }
@@ -104,10 +129,13 @@ extension ServerAPI {
 
 enum URLType: CustomStringConvertible {
     case all
+    case user
     var description: String {
         switch self {
         case .all:
             return "/todolist"
+        case .user:
+            return "/user"
         }
     }
 }
