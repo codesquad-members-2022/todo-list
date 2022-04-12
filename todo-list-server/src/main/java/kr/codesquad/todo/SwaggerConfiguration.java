63fe3973147f526b9f3ff148ac32520ef7320f48
Kyle Choi <tmdfl1027@gmail.com>
2022-04-12 11:23:02 +0900

    [#24] Swagger Controller 경로 변경

--- todo-list-server/src/main/java/kr/codesquad/todo/SwaggerConfiguration.java
+++ todo-list-server/src/main/java/kr/codesquad/todo/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package kr.codesquad.todo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -20,7 +21,7 @@ public class SwaggerConfiguration {
                 .useDefaultResponseMessages(false)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.list.todo.controller"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
