4dd37f3f6b3e0b4d525f2490be20bce982ffe09a
naneun <rmtlrksdp@naver.com>
2022-04-12 19:06:29 +0900

    ♻️ 간단한 코드 정리

--- backend/src/main/java/com/hooria/todo/repository/ActivityLogRepository.java
+++ backend/src/main/java/com/hooria/todo/repository/ActivityLogRepository.java
@@ -4,10 +4,8 @@ import com.hooria.todo.domain.Action;
 import com.hooria.todo.domain.ActivityLog;
 import com.hooria.todo.domain.Status;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
