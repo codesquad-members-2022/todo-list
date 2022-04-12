c0256bdddea6f873356d0aaae255a866d4573cf7
jun <devjun1023@gmail.com>
2022-04-12 12:51:43 +0900

    [Feat][#16] History 응답 클래스 설계
    Contents
    - History Response 내용 결정
    - DTO 생성
    
    Todo
    - Controller 구현

--- backend/src/main/java/com/example/backend/domain/history/History.java
+++ backend/src/main/java/com/example/backend/domain/history/History.java
@@ -5,10 +5,25 @@ import java.time.LocalDateTime;
 public class History {
     private Long id;
     private String writer;
+    private String title;
     private String content;
     private String font;
     private LocalDateTime createdAt;
+    private LocalDateTime lastModifiedAt;
     private Action action;
     private Long memberId;
     private boolean visible;
+
+    public History(String writer, String title, String content, String font, Action action, Long memberId) {
+        LocalDateTime now = LocalDateTime.now();
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+        this.font = font;
+        this.createdAt = now;
+        this.lastModifiedAt = now;
+        this.action = action;
+        this.memberId = memberId;
+        this.visible = true;
+    }
 }
