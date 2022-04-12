036ca4804c7c12b8e22ae732c80908696e92fc85
Kang Changgu (Miller) <rkckr1@naver.com>
2022-04-13 06:31:56 +0900

    feat: [#53] CardService 내 비즈니스 로직 수정 및 메서드별 HistoryResponse 반환
    - CardService 의 각 메서드에서 저장된 History 를 HistoryResponse 로 반환 추가
    - CardController 의 각 메서드에서 응답 시 HistoryResponse 반환

--- backend/src/main/java/com/codesquad/todolist/card/CardService.java
+++ backend/src/main/java/com/codesquad/todolist/card/CardService.java
@@ -6,8 +6,10 @@ import com.codesquad.todolist.card.dto.CardUpdateRequest;
 import com.codesquad.todolist.history.HistoryRepository;
 import com.codesquad.todolist.history.ModifiedFieldRepository;
 import com.codesquad.todolist.history.domain.Action;
+import com.codesquad.todolist.history.domain.Field;
 import com.codesquad.todolist.history.domain.History;
 import com.codesquad.todolist.history.domain.ModifiedField;
+import com.codesquad.todolist.history.dto.HistoryResponse;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -25,18 +27,26 @@ public class CardService {
         this.modifiedFieldRepository = modifiedFieldRepository;
     }
 
-    public Card create(CardCreateRequest request) {
+    public HistoryResponse create(CardCreateRequest request) {
         Card card = request.toEntity();
-        return cardRepository.create(card);
+        cardRepository.create(card);
+
+        // History 저장
+        History history = new History(card.getCardId(), Action.CREATE);
+        historyRepository.create(history);
+
+        return getHistoryResponse(history.getHistoryId());
     }
 
-    public History update(Integer cardId, CardUpdateRequest request) {
+    public HistoryResponse update(Integer cardId, CardUpdateRequest request) {
         Card card = cardRepository.findById(cardId)
             .orElseThrow(() -> new IllegalArgumentException("카드를 찾을 수 없습니다."));
 
+        // Card 필드 업데이트 및 저장
         card.update(request.getTitle(), request.getContent(), request.getAuthor());
         cardRepository.update(card);
 
+        // History 저장 및 ModifiedField 저장
         History history = new History(card.getCardId(), Action.UPDATE);
         historyRepository.create(history);
 
@@ -46,31 +56,56 @@ public class CardService {
         }
         modifiedFieldRepository.createAll(modifiedFields);
 
-        return historyRepository.findById(history.getHistoryId())
-            .orElseThrow(() -> new IllegalArgumentException("히스토리를 찾을 수 없습니다."));
+        return getHistoryResponse(history.getHistoryId());
     }
 
-    public void delete(Integer cardId) {
+    public HistoryResponse delete(Integer cardId) {
         Card card = cardRepository.findById(cardId)
             .orElseThrow(() -> new IllegalArgumentException("카드를 찾을 수 없습니다."));
 
+        // Column 내 Card 간 링크 및 삭제
         cardRepository.linkPrev(card);
         cardRepository.deleteTarget(card);
+
+        // History 저장
+        History history = new History(card.getCardId(), Action.DELETE);
+        historyRepository.create(history);
+
+        return getHistoryResponse(history.getHistoryId());
     }
 
-    public void move(Integer cardId, CardMoveRequest request) {
-        Card card = cardRepository.findById(cardId)
+    public HistoryResponse move(Integer cardId, CardMoveRequest request) {
+        Card oldCard = cardRepository.findById(cardId)
             .orElseThrow(() -> new IllegalArgumentException("카드를 찾을 수 없습니다."));
 
-        // 이동할 카드의 기존 (old) 위치에서, 다음 (next) 노드의 카드 ID
-        Integer oldNextId = card.getNextId();
+        Card newCard = oldCard.clone();
+        newCard.move(request.getColumnId(), request.getNextId());
 
-        // 이동할 카드의 기존 (prev) 위치에서, 이전 노드를 이동할 카드의 다음 노드와 연결
-        cardRepository.linkPrev(card);
+        // Column 내 Card 간 링크 및 이동
+        cardRepository.linkPrev(oldCard);
+        cardRepository.linkNext(newCard);
+        cardRepository.moveTarget(newCard);
 
-        // 이동할 카드의 다음 (next) 위치에서, 이전 노드를 이동한 카드에 연결
-        card.move(request.getColumnId(), request.getNextId());
-        cardRepository.linkNext(card);
-        cardRepository.moveTarget(card);
+        // History 저장 및 ModifiedField 저장
+        History history = new History(newCard.getCardId(), Action.MOVE);
+        historyRepository.create(history);
+
+        ModifiedField modifiedField = new ModifiedField(history.getHistoryId(), Field.COLUMN,
+            oldCard.getColumnId().toString(),
+            newCard.getColumnId().toString());
+        modifiedFieldRepository.create(modifiedField);
+
+        return getHistoryResponse(history.getHistoryId());
+    }
+
+    private HistoryResponse getHistoryResponse(Integer historyId) {
+        History history = historyRepository.findById(historyId)
+            .orElseThrow(() -> new IllegalArgumentException("히스토리를 찾을 수 없습니다."));
+
+        if (history.getAction() == Action.MOVE || history.getAction() == Action.UPDATE) {
+            List<ModifiedField> modifiedFields = modifiedFieldRepository.findByHistoryId(historyId);
+            history.setFields(modifiedFields);
+        }
+        return HistoryResponse.from(history);
     }
 }
