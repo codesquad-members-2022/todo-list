f45333923aa25be1ababae755b22ab2fba0cb0ef
jindonyy <rsefa94@gmail.com>
2022-04-12 16:16:17 +0900

    Feat: #17 column view 구현

---
+++ front/static/resources/js/views/component/column.js
@@ -0,0 +1,30 @@
+class Column {
+  constructor(props) {
+    this.props = props;
+    //props -> status, cards
+  }
+
+  template(cards) {
+    return `<div class="column">
+            <div class="column__top-wrap">
+              <div class="column__title-wrap">
+                <h2 class="column__title">${this.props.title}</h2>
+                <span class="card-num">${this.props.cards.length}</span>
+              </div>
+              <div class="column__button-wrap">
+                <button type="button" class="button--add card-button--add">
+                  <span class="hidden-text">컬럼 추가 버튼</span>
+                </button>
+                <button type="button" class="button--delete column__button--delete">
+                  <span class="hidden-text">컬럼 삭제 버튼</span>
+                </button>
+              </div>
+            </div>
+            <ul class="cards">
+            ${cards}
+            </ul>
+          </div>`;
+  }
+}
+
+export { Column };
