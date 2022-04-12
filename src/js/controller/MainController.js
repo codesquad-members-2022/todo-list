31274cc231e11c43ca2e3aff866d9c00c4129915
godhyeongman <aaaa4000@naver.com>
2022-04-12 15:28:57 +0900

    Feat: mainController 뼈대 구현

---
+++ src/js/controller/MainController.js
@@ -0,0 +1,22 @@
+import { renderEmptyCard } from "../view/EmptyCardView.js";
+
+function addCard2Column(event, store) {
+  const { target } = event;
+  const parentColumn = target.closest(".column__zone");
+  const hasEmptyCard = parentColumn.querySelector(".testNewCard");
+  if (hasEmptyCard) return;
+  renderEmptyCard(parentColumn, createCardState, store);
+}
+
+function init(store) {
+  const columnZone = document.querySelector(".column__zone");
+  initColumn();
+  onClickColumnAddBtn(columnZone, addCard2Column, store);
+}
+
+const createColumnController = (store) => {
+  const columnStore = store;
+  init(columnStore);
+};
+
+export { createColumnController };
