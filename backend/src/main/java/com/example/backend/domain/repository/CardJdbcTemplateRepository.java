69447b42e71c2b0d018df882418aa4315b911e6a
PhilSoGooood <peelhw@gmail.com>
2022-04-13 15:23:44 +0900

    feat: Card 수정 기능 및 수정 로그 생성
    CardService.save()에서 해당 id가 있으면 카드 업데이트, 없으면
    카드 생성 기능 실행하고 해당 로그를 생성함.

--- backend/src/main/java/com/example/backend/domain/repository/CardJdbcTemplateRepository.java
+++ backend/src/main/java/com/example/backend/domain/repository/CardJdbcTemplateRepository.java
@@ -6,85 +6,105 @@ import com.example.backend.web.dto.CardListResponseDto;
 import com.example.backend.web.dto.CardMoveRequestDto;
 import com.example.backend.web.dto.Column;
 import com.example.backend.web.dto.Columns;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Repository
 public class CardJdbcTemplateRepository implements CardRepository {
-    private final JdbcTemplate jdbcTemplate;
-    private final SimpleJdbcInsert simpleJdbcInsert;
+	private final JdbcTemplate jdbcTemplate;
+	private final SimpleJdbcInsert simpleJdbcInsert;
 
-    public CardJdbcTemplateRepository(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
-                .withTableName("CARD")
-                .usingGeneratedKeyColumns("id");
-    }
+	public CardJdbcTemplateRepository(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+			.withTableName("CARD")
+			.usingGeneratedKeyColumns("id");
+	}
 
-    @Override
-    public Columns findAllDesc() {
-        Columns columns = new Columns();
-        for (ColumnName column : ColumnName.values()) {
-            String columnName = column.getName();
-            Column resultColumn = new Column(columnName);
+	@Override
+	public Columns findAllDesc() {
+		Columns columns = new Columns();
+		for (ColumnName column : ColumnName.values()) {
+			String columnName = column.getName();
+			Column resultColumn = new Column(columnName);
 
-            resultColumn.addCards(findResponseDtosByColumnName(columnName));
-            columns.addColumn(resultColumn);
-        }
-        return columns;
-    }
+			resultColumn.addCards(findResponseDtosByColumnName(columnName));
+			columns.addColumn(resultColumn);
+		}
+		return columns;
+	}
 
-    private List<CardListResponseDto> findResponseDtosByColumnName(String columnName) {
-        return jdbcTemplate.query("SELECT id, title, content, author_system FROM CARD WHERE COLUMN_NAME = ? and DELETED = 0 ORDER BY ORDER_INDEX DESC",
-                (rs, rowNum) -> {
-                    long id = rs.getLong("id");
-                    String title = rs.getString("title");
-                    String content = rs.getString("content");
-                    String authorSystem = rs.getString("author_system");
-                    return new CardListResponseDto(id, title, content, authorSystem);
-                }, columnName);
-    }
+	private List<CardListResponseDto> findResponseDtosByColumnName(String columnName) {
+		return jdbcTemplate.query(
+			"SELECT id, title, content, author_system FROM CARD WHERE COLUMN_NAME = ? and DELETED = 0 ORDER BY ORDER_INDEX DESC",
+			(rs, rowNum) -> {
+				long id = rs.getLong("id");
+				String title = rs.getString("title");
+				String content = rs.getString("content");
+				String authorSystem = rs.getString("author_system");
+				return new CardListResponseDto(id, title, content, authorSystem);
+			}, columnName);
+	}
 
-    @Override
-    public CardListResponseDto save(Card card) {
-        String columnName = card.getColumnName();
-        Integer orderIndex = getOrderIndex(columnName);
+	@Override
+	public CardListResponseDto save(Card card) {
+		if (!Objects.isNull(card.getId())) {
+			jdbcTemplate.update("UPDATE CARD SET title=?, content=? WHERE id=?", card.getTitle(), card.getContent(),
+				card.getId());
+			return new CardListResponseDto(card.getId(), card.getTitle(), card.getContent(), card.getAuthorSystem());
+		}
+		String columnName = card.getColumnName();
+		Integer orderIndex = getOrderIndex(columnName);
 
-        Map<String, Object> params = new HashMap<>();
-        params.put("title", card.getTitle());
-        params.put("content", card.getContent());
-        params.put("column_name", columnName);
-        params.put("author_system", card.getAuthorSystem());
-        params.put("order_index", orderIndex);
-        params.put("deleted", false);
-        long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
-        return new CardListResponseDto(id, card.getTitle(), card.getContent(), card.getAuthorSystem());
-    }
+		Map<String, Object> params = new HashMap<>();
+		params.put("title", card.getTitle());
+		params.put("content", card.getContent());
+		params.put("column_name", columnName);
+		params.put("author_system", card.getAuthorSystem());
+		params.put("order_index", orderIndex);
+		params.put("deleted", false);
+		long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+		return new CardListResponseDto(id, card.getTitle(), card.getContent(), card.getAuthorSystem());
+	}
 
-    private Integer getOrderIndex(String columnName) {
-        return jdbcTemplate.queryForObject(
-                "SELECT count(*) FROM CARD WHERE column_name = ?", Integer.class, columnName);
-    }
+	private Integer getOrderIndex(String columnName) {
+		return jdbcTemplate.queryForObject(
+			"SELECT count(*) FROM CARD WHERE column_name = ?", Integer.class, columnName);
+	}
 
-    @Override
-    public Optional<Card> findById(Long id) {
-        return Optional.empty();
-    }
+	@Override
+	public Optional<Card> findById(Long id) {
+		List<Card> cardList = jdbcTemplate.query("SELECT id, title, content, author_system FROM CARD WHERE id = ?",
+			(rs, rowNum) -> {
+				long userId = rs.getLong("id");
+				String title = rs.getString("title");
+				String content = rs.getString("content");
+				String authorSystem = rs.getString("author_system");
+				return new Card.Builder().title(title).content(content).id(userId).authorSystem(authorSystem).build();
+			}, id);
+		return cardList.stream().findAny();
+	}
 
-    @Override
-    public Long deleteById(Long id) {
-        return null;
-    }
+	@Override
+	public Long deleteById(Long id) {
+		return null;
+	}
 
-    @Override
-    public Long move(CardMoveRequestDto dto) {
-        return null;
-    }
+	@Override
+	public Long move(CardMoveRequestDto dto) {
+		return null;
+	}
 }
