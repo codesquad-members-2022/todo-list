8a053720b32af6e07d19c6da433a12d938ceca14
yongseongjeon <yongseong.jeon95@gmail.com>
2022-04-13 16:40:12 +0900

    feat: + 버튼 클릭시 카드 추가 폼 생성

--- FE/src/components/Column.js
+++ FE/src/components/Column.js
@@ -1,10 +1,13 @@
 import { GET_CARD } from "../dummyData.js";
+import { $ } from "../utils.js";
+import { CardModificationForm } from "./CardModificationForm.js";
 import TaskCard from "./TaskCard.js";
 
 export default class Column {
   constructor(columnTitle) {
     this.columnTitle = columnTitle;
     this.taskCardComponents = GET_CARD.map((card) => new TaskCard(card));
+    this.isOpenModificationForm = false;
   }
   template() {
     return `
@@ -15,12 +18,15 @@ export default class Column {
             <strong class="work__counter">${this.getCardLength()}</strong>
           </div>
           <div class="work__btn">
-            <div class="work__btn--add"></div>
+            <div 
+              data-name="${this.columnTitle}" 
+              class="work__btn--add ${this.columnTitle}-btn">
+            </div>
             <div class="work__btn--remove"></div>
           </div>
         </div>
         <div class="work__body">
-          <ul class="work__list">
+          <ul class="work__list ${this.columnTitle}-list">
             ${this.taskCardComponents.reduce(
               (prev, cur) =>
                 prev + `<div class="${cur.id}">${cur.template()}</div>`,
@@ -33,5 +39,31 @@ export default class Column {
   }
   getCardLength() {
     return this.taskCardComponents.length;
+  }
+  addEvent() {
+    $("body")
+      .querySelector(`.${this.columnTitle}-btn`)
+      .addEventListener("click", (e) => this.handleCardAddition(e));
+  }
+
+  handleCardAddition(e) {
+    if (!this.isOpenModificationForm) {
+      this.showModificationForm(e);
+      this.isOpenModificationForm = true;
+      return;
+    }
+    this.hideModificationForm(e);
+    this.isOpenModificationForm = false;
+  }
+  showModificationForm(e) {
+    const columnName = e.target.dataset.name;
+    const list = $(`.${columnName}-list`);
+    list.insertAdjacentHTML(
+      "afterbegin",
+      new CardModificationForm().template()
+    );
+  }
+  hideModificationForm(e) {
+    $(".task-deactivate").remove();
   }
 }
