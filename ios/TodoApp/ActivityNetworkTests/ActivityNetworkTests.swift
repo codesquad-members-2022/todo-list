bea4992e20bf50923509691b97ff49996d4bc0ac
Damagucci-Juice <jsr9874@gmail.com>
2022-04-13 15:13:36 +0900

    test: Network 통신이 되는지 확인하기 위해 작성

--- ios/TodoApp/ActivityNetworkTests/ActivityNetworkTests.swift
+++ ios/TodoApp/ActivityNetworkTests/ActivityNetworkTests.swift
@@ -1,32 +1,10 @@
-//
-//  ActivityNetworkTests.swift
-//  ActivityNetworkTests
-//
-//  Created by YEONGJIN JANG on 2022/04/13.
-//
-
 import XCTest
+@testable import TodoApp
 
 class ActivityNetworkTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testShouldNetworkReturnData() throws {
+        let _ = NetworkHandler.getData(resource: "https://1dc4c2f3-00b4-446d-a22a-d6920eaee622.mock.pstmn.io/history")
+        sleep(10)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
