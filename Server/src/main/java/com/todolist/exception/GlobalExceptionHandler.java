372900cb0c0945c099045d2fcb2c5f6611899db2
leejohy-0223 <leejohy@naver.com>
2022-04-13 16:45:27 +0900

    refactor: 예외 handling (#45)
    - 변경된 예외 객체 및 custom 예외를 catch할 수 있도록 적용

--- Server/src/main/java/com/todolist/exception/GlobalExceptionHandler.java
+++ Server/src/main/java/com/todolist/exception/GlobalExceptionHandler.java
@@ -1,67 +1,75 @@
 package com.todolist.exception;
 
-import lombok.extern.slf4j.Slf4j;
+import static com.todolist.exception.ExceptionType.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<RestResponse>> MethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<RestResponse> globalExceptionResolver(GlobalException exception) {
 
-        // 인스턴스 변수로 변경 고려 -> 여러 개의 예외가 발생할 경우에는 ?
-        List<RestResponse> responses = new ArrayList<>();
+        ExceptionType exceptionType = exception.getExceptionType();
+        RestResponse restResponse = RestResponse.of(exceptionType.getMessage());
+        return new ResponseEntity<>(restResponse, exceptionType.getHttpStatus());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<RestResponse> converterExceptionResolver() {
+        RestResponse restResponse = RestResponse.of(INVALID_TYPE.getMessage());
+        return new ResponseEntity<>(restResponse, INVALID_TYPE.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RestResponse> MethodArgumentNotValidException(
+        MethodArgumentNotValidException exception) {
+        Map<String, String> map = new HashMap<>();
 
         for (FieldError error : exception.getFieldErrors()) {
             log.error("errorFieldName : {}, error message : {}", error.getField(), error.getDefaultMessage());
-
-            RestResponse restResponse = RestResponse.methodArgumentNotValidException(error.getField(), error.getDefaultMessage());
-            responses.add(restResponse);
+            map.put(error.getField(), error.getDefaultMessage());
         }
 
-        return new ResponseEntity<>(responses, HttpStatus.BAD_REQUEST);
+        RestResponse restResponse = RestResponse.of(map);
+        return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<List<RestResponse>> MethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
-        List<RestResponse> responses = new ArrayList<>();
-        String errorFieldName = exception.getName();
+    public ResponseEntity<RestResponse> MethodArgumentTypeMismatchException(
+        MethodArgumentTypeMismatchException exception) {
+        Map<String, String> map = new HashMap<>();
 
-        // MethodArgumentTypeMismatchException 클래스에 따로 errorMessage 받아오는 메서드가 없어서 직접 Enum 에서 받아서 넣음
-        String errorMessage = ExceptionType.CARD_TYPE_MISMATCH.getMessage();
+        map.put("errorFieldName", exception.getName());
+        map.put("errorMessage", ExceptionType.CARD_TYPE_MISMATCH.getMessage());
 
-        log.error("errorFieldName : {}, errorMessage : {}", errorFieldName, errorMessage);
-
-        RestResponse restResponse = RestResponse.methodArgumentTypeMismatchException(errorFieldName, errorMessage);
-        responses.add(restResponse);
-
-        return new ResponseEntity<>(responses, HttpStatus.BAD_REQUEST);
+        RestResponse restResponse = RestResponse.of(map);
+        return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
 
-    // cardService.path() try-catch 부분에서 예외 캐치
     @ExceptionHandler(NotFoundCardException.class)
-    public ResponseEntity<List<RestResponse>> NotFoundCardException(NotFoundCardException exception) {
-        List<RestResponse> responses = new ArrayList<>();
-        String errorFiledName = exception.getErrorFiledName();
+    public ResponseEntity<RestResponse> NotFoundCardException(NotFoundCardException exception) {
+        Map<String, String> map = new HashMap<>();
+        map.put("errorFieldName", exception.getErrorFieldName());
+        map.put("errorMessage", exception.getExceptionType().getMessage());
 
-        // 직접 만든 커스텀 예외라서 에러 메세지 받아오는 메서드 따로 만들었음
-        String errorMessage = exception.getExceptionType();
+        RestResponse restResponse = RestResponse.of(map);
 
-        log.error("errorFiledName = {}, error message = {}", errorFiledName, errorMessage);
-
-        RestResponse restResponse = RestResponse.notFoundCardException(errorFiledName, errorMessage);
-        responses.add(restResponse);
-
-        return new ResponseEntity<>(responses, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
+
 }
