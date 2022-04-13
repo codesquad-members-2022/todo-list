79cd48a986f4aecc806f6f4a7dcfe492b19ca2cc
khj <hongjung.kim.dev@gmail.com>
2022-04-13 15:25:53 +0900

    feat: [#39] 드래그앤 드롭 도착 잔상 및 맨끝에 추가 제외한 기본 기능 구현

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -4,6 +4,7 @@ import css from '../scss/style.scss';
 import Controller from './Controller/Controller.js';
 import { Header } from './Header/main.js';
 import { History } from './History/main.js';
+import { dragNdrop } from './dragNdrop';
 
 (function () {
   const controller = new Controller({ Header, History });
@@ -16,3 +17,5 @@ import { History } from './History/main.js';
     },
   });
 })();
+
+dragNdrop();
