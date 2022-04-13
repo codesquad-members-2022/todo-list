b6f01f363e4b7dfb4cb5fa8d0bbbd15811d6ed73
PhilSoGooood <peelhw@gmail.com>
2022-04-13 16:57:05 +0900

    refactor: Action_log 테이블 구조 수정
    과거 iOS팀과 회의 후 로그 내용을 String 한줄로 보내주도록 수정했던 것을
    Log table의 각 데이터를 조합해서 String 한줄로 dto에 저장하여 보내도록 로직을 수정하였습니다.

--- backend/src/main/java/com/example/backend/domain/Log.java
+++ backend/src/main/java/com/example/backend/domain/Log.java
@@ -1,37 +1,38 @@
 package com.example.backend.domain;
 
-import com.example.backend.web.dto.LogSaveRequestDto;
-
 public class Log {
 	private Long id;
 	private String author;
-	private String text;
+	private String title;
+	private String prevColumnName;
+	private String curColumnName;
+	private ActionType actionType;
 	private String createdDate;
 
-	public static Log from(LogSaveRequestDto dto) {
-		Log log = new Log();
-		log.author = "sam";
-		log.text = log.createText(dto);
-		return log;
+	public Log(Long id, String author, String title, String prevColumnName, String curColumnName,
+		ActionType actionType, String createdDate) {
+		this.id = id;
+		this.author = author;
+		this.title = title;
+		this.prevColumnName = prevColumnName;
+		this.curColumnName = curColumnName;
+		this.actionType = actionType;
+		this.createdDate = createdDate;
 	}
 
-	private String createText(LogSaveRequestDto dto) {
-		String title = dto.getTitle();
-		String prevColumnName = dto.getPrevColumnName();
-		String currentColumnName = dto.getCurrentColumnName();
-		ActionType actionType = dto.getActionType();
+	public String createText() {
 
 		StringBuilder sb = new StringBuilder();
 		switch (actionType.getName()) {
 			case ("add"):
-				sb.append(currentColumnName)
+				sb.append(curColumnName)
 					.append("에 ")
 					.append(title)
 					.append("을(를) 등록하였습니다");
 				return sb.toString();
 
 			case ("remove"):
-				sb.append(currentColumnName)
+				sb.append(curColumnName)
 					.append("의 ")
 					.append(title)
 					.append("이(가) 삭제되었습니다.");
@@ -47,7 +48,7 @@ public class Log {
 					.append("을(를) ")
 					.append(prevColumnName)
 					.append("에서 ")
-					.append(currentColumnName)
+					.append(curColumnName)
 					.append("(으)로 이동하였습니다.");
 				return sb.toString();
 		}
@@ -62,11 +63,73 @@ public class Log {
 		return author;
 	}
 
-	public String getText() {
-		return text;
+	public String getTitle() {
+		return title;
+	}
+
+	public String getPrevColumnName() {
+		return prevColumnName;
+	}
+
+	public String getCurColumnName() {
+		return curColumnName;
+	}
+
+	public ActionType getActionType() {
+		return actionType;
 	}
 
 	public String getCreatedDate() {
 		return createdDate;
 	}
+
+	public static class Builder {
+		private Long id;
+		private String author;
+		private String title;
+		private String prevColumnName;
+		private String curColumnName;
+		private ActionType actionType;
+		private String createdDate;
+
+		public Builder id(Long id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder author(String author) {
+			this.author = author;
+			return this;
+		}
+
+		public Builder title(String title) {
+			this.title = title;
+			return this;
+		}
+
+		public Builder prevColumnName(String prevColumnName) {
+			this.prevColumnName = prevColumnName;
+			return this;
+		}
+
+		public Builder curColumnName(String curColumnName) {
+			this.curColumnName = curColumnName;
+			return this;
+		}
+
+		public Builder actionType(ActionType actionType) {
+			this.actionType = actionType;
+			return this;
+		}
+
+		public Builder createdDate(String createdDate) {
+			this.createdDate = createdDate;
+			return this;
+		}
+
+		public Log build() {
+			return new Log(id, author, title, prevColumnName, curColumnName, actionType, createdDate);
+		}
+	}
+
 }
--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -15,7 +15,10 @@ CREATE INDEX column_deleted_comp ON CARD(column_name, deleted);
 CREATE TABLE ACTION_LOG
 (
     id                  int AUTO_INCREMENT PRIMARY KEY,
-    author              varchar(20) NOT NULL,
-    text                varchar(500) NOT NULL,
+    author              varchar(20) NOT NULL DEFAULT 'sam',
+    title               varchar(50) NOT NULL,
+    cur_column_name     ENUM('to_do', 'in_progress', 'done'),
+    prev_column_name    ENUM('to_do', 'in_progress', 'done') default NULL,
+    action_type         ENUM('add', 'remove', 'update', 'move'),
     created_date        DATETIME DEFAULT CURRENT_TIMESTAMP
 );
