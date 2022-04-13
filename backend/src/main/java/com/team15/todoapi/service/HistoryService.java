c7d21d799c03811aa96d6713c18a5e983ac0b70c
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-04-13 20:41:02 +0900

    feat: HistoryService 생성
    - userId에 해당하는 활동 로그를 받아오기위해 findAll메서드 구현

---
+++ backend/src/main/java/com/team15/todoapi/service/HistoryService.java
@@ -0,0 +1,31 @@
+package com.team15.todoapi.service;
+
+import com.team15.todoapi.controller.history.HistoryResponse;
+import com.team15.todoapi.domain.History;
+import com.team15.todoapi.domain.Member;
+import com.team15.todoapi.repository.HistoryRepository;
+import com.team15.todoapi.repository.MemberRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HistoryService {
+
+	private final HistoryRepository historyRepository;
+	private final MemberRepository memberRepository;
+
+	public List<HistoryResponse> findAll(String userId) {
+		Member member = selectMemberInfo(userId);
+
+		List<History> histories = historyRepository.findAll(member.getId());
+		return histories.stream().map(HistoryResponse::from).collect(Collectors.toList());
+	}
+
+	private Member selectMemberInfo(String userId) {
+		Member member = memberRepository.findByUserId(userId);
+		return member;
+	}
+}
