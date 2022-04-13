36a3753ae9ae528ec54b126f4a6aaa2a78199fdc
seungchanHan <seungchanhan00@gmail.com>
2022-04-13 14:57:46 +0900

    feat : 서버와 새로운 데이터 요청 및 응답

--- FE/src/JS/todo/todo-model.js
+++ FE/src/JS/todo/todo-model.js
@@ -14,4 +14,17 @@ export default class Model {
       this.categoryId[category.categoryName] = category.categoryId;
     }
   }
+
+  async addCardData(headerValue) {
+    const newCardData = await fetchRequest('/works', 'POST', headerValue);
+
+    // const a = {
+    //   id: 4,
+    //   title: '호눅스 강의 수강',
+    //   content: '멀티 스레드 개념 및 동작 원리 이해',
+    //   createdDateTime: '2022-04-12T18:20:51',
+    // };
+
+    return newCardData;
+  }
 }
