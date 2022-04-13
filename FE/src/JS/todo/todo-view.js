71799f04be98026fb516cf6999c2b30fca4b8aff
seungchanHan <seungchanhan00@gmail.com>
2022-04-13 15:00:17 +0900

    feat : 카드 추가 이벤트 및 삭제 이벤트 구현

--- FE/src/JS/todo/todo-view.js
+++ FE/src/JS/todo/todo-view.js
@@ -1,5 +1,9 @@
 import { $, $$ } from '../utility/util.js';
-import { makeCategory, makeCard, addCardTemplate } from './todo-view-template.js';
+import {
+  makeCategory,
+  makeCard,
+  addCardTemplate,
+} from './todo-view-template.js';
 
 export default class View {
   constructor() {
@@ -10,40 +14,72 @@ export default class View {
     this.columnList.insertAdjacentHTML('beforeend', makeCategory(categoryName));
   }
 
-  renderTodoCard(todoInfo, data) {
-    const $container = this.findCurrentContainer(todoInfo.category);
+  renderTodoCard(todoInfo, data, newCard = false) {
+    const container = this.findCurrentContainer(todoInfo.category);
 
-    $container.insertAdjacentHTML('afterBegin', makeCard(todoInfo, data));
+    if (newCard) {
+      container.insertAdjacentHTML('afterBegin', makeCard(todoInfo, data));
+    } else {
+      container.insertAdjacentHTML('beforeEnd', makeCard(todoInfo, data));
+    }
 
-    this.changeCategoryCount(todoInfo, $container);
+    this.changeCategoryCount(todoInfo, container);
+    this.removeCardBtnEvent();
   }
 
   findCurrentContainer = (categoryName) => $(`.container${categoryName}`);
 
-  changeCategoryCount(todoInfo, $container) {
-    $(`.itemCount${todoInfo.category}`).innerHTML =
-      $container.childElementCount;
+  changeCategoryCount(todoInfo, container) {
+    $(`.itemCount${todoInfo.category}`).innerHTML = container.childElementCount;
   }
 
   addCardTemplateEvent() {
     const addBtn = $$('.addBtn');
-    addBtn.forEach(el => (el.addEventListener('click', this.showCardTemplate)));
+    addBtn.forEach((el) => el.addEventListener('click', this.showCardTemplate));
   }
 
-  showCardTemplate = ({target}) => {
+  showCardTemplate = ({ target }) => {
     const columnName = target.closest('.column-item').id;
     const column = this.findCurrentContainer(columnName);
-    column.insertAdjacentHTML('afterBegin', addCardTemplate())
-    this.addCardBtnEvent()
-  }
+    column.insertAdjacentHTML('afterBegin', addCardTemplate());
+    this.addCardBtnEvent();
+  };
 
   addCardBtnEvent() {
     const registerBtn = $('.registerBtn');
-    const cancleBtn = $('.cancleBtn');
-    cancleBtn.addEventListener('click', this.cancelCardTemplate);
-   }
+    registerBtn.addEventListener('click', this.getHeaderValue);
+    const cancelBtn = $('.cancelBtn');
+    cancelBtn.addEventListener('click', this.cancelCardTemplate);
+  }
 
-  cancelCardTemplate = ({target}) => {
+  cancelCardTemplate = ({ target }) => {
     target.closest('.column-addBtn').remove();
+  };
+
+  removeCardBtnEvent() {
+    const deleteBtn = $('.delete-btn');
+    deleteBtn.addEventListener('click', this.showCheckBox);
+  }
+
+  showCheckBox() {
+    Swal.fire({
+      title: '정말 취소 하시겠습니까?',
+      text: '다시 되돌릴 수 없습니다. 신중하세요.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '확인',
+      cancelButtonText: '취소',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          '리스트가 삭제되었습니다',
+          '확인 버튼을 눌러주세요',
+          'success'
+        );
+        console.log(1);
+      }
+    });
   }
 }
