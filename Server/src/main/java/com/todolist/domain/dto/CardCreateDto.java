8d4b85d09555f76fe383f4d43a3233b15f9ae429
ByeoungJoon Jeon <juni8453@naver.com>
2022-04-13 14:50:20 +0900

    feat : Card 객체 검증 추가(#41)
    @NotNull, @NotBlank 어노테이션을 활용해 검증하도록 구현

--- Server/src/main/java/com/todolist/domain/dto/CardCreateDto.java
+++ Server/src/main/java/com/todolist/domain/dto/CardCreateDto.java
@@ -6,11 +6,26 @@ import com.todolist.domain.Card;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 public class CardCreateDto {
+
+    // @NotBlank 는 String 에서 사용
+    @NotNull(message = "userId 값이 비었습니다.")
     private Integer userId;
+
+    @NotBlank(message = "카드 제목을 입력해주세요.")
+    @Size(min = 1, max = 30)
     private String cardTitle;
+
+    @NotBlank(message = "카드 내용을 입력해주세요.")
+    @Size(min = 1, max = 500)
     private String cardContent;
+
+    @NotBlank(message = "boardName 값이 비었습니다.")
     private String boardName;
 
     public CardCreateDto(Integer userId, String cardTitle, String cardContent, String boardName) {
--- Server/src/main/java/com/todolist/domain/dto/CardPatchDto.java
+++ Server/src/main/java/com/todolist/domain/dto/CardPatchDto.java
@@ -2,10 +2,18 @@ package com.todolist.domain.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 public class CardPatchDto {
 
+    @NotBlank(message = "카드 제목을 입력해주세요.")
+    @Size(min = 1, max = 30)
     private final String cardTitle;
+
+    @NotBlank(message = "카드 내용을 입력해주세요.")
+    @Size(min = 1, max = 500)
     private final String cardContent;
 
     public CardPatchDto(String cardTitle, String cardContent) {
