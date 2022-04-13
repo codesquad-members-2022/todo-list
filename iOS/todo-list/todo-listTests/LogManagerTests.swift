fc02b38719bc5c49ae71d94e947ec868143f22bd
Jason <ggj00125@gmail.com>
2022-04-13 09:41:14 +0900

    💡필요없는 주석 삭제

--- iOS/todo-list/todo-listTests/LogManagerTests.swift
+++ iOS/todo-list/todo-listTests/LogManagerTests.swift
@@ -10,11 +10,6 @@ import XCTest
 
 class LogManagerTests: XCTestCase {
     
-//    LogManager를 테스트하려면 API로 부터 Log를 담아올 수 있는지 확인해야하는데
-//    서버가 불안정하니 Local환경에서 LogManager가 Log를 담아낼 수 있는 역할에 대해
-//    확인이 필요하다... Local은 동기환경...
-//
-//    Log를 Load해야하는데 dummyData를 활용해야 할 것 같은데...
     private let logManager = LogManager()
     
     func testLoad() {
