10d718daf527d930296708400cd7473a589a5aab
ByeoungJoon Jeon <juni8453@naver.com>
2022-04-13 14:38:44 +0900

    refactor : ExceptionType Enum 클래스 수정(#41)
    잘못된 클라이언트 요청이 왔을 때 적절한 에러 메시지를 응답하기 위한 Enum 타입을 추가하고 필요없는 로직은 제거했습니다.

--- Server/src/main/java/com/todolist/exception/ExceptionType.java
+++ Server/src/main/java/com/todolist/exception/ExceptionType.java
@@ -1,22 +1,18 @@
 package com.todolist.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ExceptionType {
 
     NO_COOKIE(HttpStatus.NOT_FOUND, "쿠키가 없습니다. 사용자 정보를 먼저 요청하세요."),
-    NO_USER_ID_IN_COOKIE(HttpStatus.NOT_FOUND, "Cookie에 사용자 정보가 없습니다.");
+    NO_USER_ID_IN_COOKIE(HttpStatus.NOT_FOUND, "Cookie 에 사용자 정보가 없습니다."),
+    NO_FOUND_CARD(HttpStatus.BAD_REQUEST, "해당 Card 를 찾을 수 없습니다. Card 번호를 확인해주세요."),
+    CARD_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "cardId 타입이 맞지 않습니다. Card 번호를 확인해주세요.");
 
     private final HttpStatus httpStatus;
     private final String message;
-
-    public ResponseEntity<String> getResponse() {
-        return new ResponseEntity<>(message, httpStatus);
-    }
 }
