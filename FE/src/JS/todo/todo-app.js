a0ffe17060a50748ba5d64afe294f9af92d711be
seungchanHan <seungchanhan00@gmail.com>
2022-04-13 14:56:08 +0900

    refactor : 패치 요청 url 수정

--- FE/src/JS/todo/todo-app.js
+++ FE/src/JS/todo/todo-app.js
@@ -5,17 +5,10 @@ import Drag from '../drag-drop/drag-drop.js';
 import { fetchRequest } from '../utility/fetchRequest.js';
 
 export async function initTodo() {
-<<<<<<< HEAD
-  const workListData = await fetchRequest(
-    '/todo-list/FE/src/js/mok-data/mokData.json'
-  );
-=======
-  const workListData = await fetchRequest('../src/js/mok-data/mokData.json');
->>>>>>> ce69d4f ([remove] 불필요한 데이터 삭제)
+  const workListData = await fetchRequest('/work?userId=ikjo');
   const model = new Model(workListData);
   const view = new View();
   const drag = new Drag();
-  
-  new Controller(model, view, drag);
 
+  new Controller(model, view, drag);
 }
