c60e4781a25f082219890705e46da98844e8d8f5
Sangbeom Heo <mansaout@gmail.com>
2022-04-13 18:17:37 +0900

    Feat: 컬럼 추가 버튼 클릭 시, 컬럼추가 폼 생성 / 취소 버튼 클릭 시 삭제
    - 이미 생성된 기존 폼이 있다면 기존 폼 삭제 후 생성
    - 취소 버튼 클릭 시 폼 삭제

--- client/public/js/handler/eventHandler.js
+++ client/public/js/handler/eventHandler.js
@@ -1,5 +1,5 @@
-import { qs, on, delegate } from "../utils/helpers.js";
-import * as renderer from "../views/renderer.js";
+import { qs, on, delegate, getParentElementByDataset } from "../utils/helpers.js";
+import { renderItemForm } from "../views/renderer.js";
 
 export function bindEvents() {
   const el = {
@@ -11,55 +11,59 @@ export function bindEvents() {
 
   const selector = {
     addItemBtn: ".column__header--add-card",
-    registItemBtn: ".card__btn--regist",
     removeItemBtn: ".card--delete-card",
-    cancelItemFormBtn: ".card__btn--cancel",
     removeColumnBtn: ".column__header--delete-card",
+    itemForm: ".card__register-form",
+    itemFormInput: ".card-box .card__content--title",
+    itemFormTextarea: ".card-box .card__content--text",
+    // itemFormRegistBtn: ".card-box .card__btn--regist",
+    itemFormCancelBtn: ".card-box .card__btn--cancel",
   };
 
   on(el.showHistoryBtn, "click", showHistoryBar);
   on(el.hideHistoryBtn, "click", hideHistoryBar);
 
+  delegate(el.columnList, "submit", selector.itemForm, (event) => registItem(event));
   delegate(el.columnList, "click", selector.addItemBtn, (event) => showItemForm(event));
-  delegate(el.columnList, "click", selector.registItemBtn, (event) => registItem(event));
+  delegate(el.columnList, "click", selector.itemFormCancelBtn, removeItemForm);
   delegate(el.columnList, "click", selector.removeItemBtn, (event) => removeItem(event));
-  delegate(el.columnList, "click", selector.cancelItemFormBtn, (event) => cancelItemForm(event));
   delegate(el.columnList, "click", selector.removeColumnBtn, (event) => removeColumn(event));
-}
 
-function showItemForm(event) {
-  console.log("showItemForm");
-  console.log(event.target);
-}
-  function toggleHistoryBar() {
   function showHistoryBar() {
     el.historyBar.classList.toggle("show");
     // 히스토리 상대 날짜 리렌더링
   }
 
-function registItem(event) {
-  console.log("registItem");
-  console.log(event.target);
-}
   function hideHistoryBar() {
     el.historyBar.classList.toggle("show");
   }
 
-function removeItem(event) {
-  console.log("removeItem");
-  console.log(event.target);
-}
-
-function cancelItemForm(event) {
-  console.log("cancelItemForm");
-  console.log(event.target);
-}
-
-function removeColumn(event) {
-  console.log("removeColumn");
-  console.log(event.target);
-  function toggleHistoryBar() {
-    el.historyBar.classList.toggle("show");
+  function showItemForm(event) {
+    removeItemForm();
+    const columnEl = getParentElementByDataset(event.target, "column");
+    const columnId = columnEl.dataset.column;
+    renderItemForm(columnId);
+    qs(selector.itemFormInput).focus();
   }
 
+  function removeItemForm() {
+    const existingItemForm = qs(".card-box");
+    if (existingItemForm) existingItemForm.remove();
+  }
+
+  function registItem(event) {
+    event.preventDefault();
+    console.log("registItem");
+    console.log(event.target);
+    // 타이틀, 내용, 작성 날짜, 칼럼 넘버 가져오기
+    // 스토어에 데이터 추가(아이템 아이디 생성)
+    // input에
+    // 아이템 렌더링
+    // 폼 삭제
+  }
+
+  function removeItem(event) {
+    console.log("removeItem");
+    console.log(event.target);
+  }
 }
