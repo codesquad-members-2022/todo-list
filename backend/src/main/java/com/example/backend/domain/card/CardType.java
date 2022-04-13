4b19bedac88c47f0b59348608a758af300a92a22
jun <devjun1023@gmail.com>
2022-04-13 15:41:45 +0900

    [Fix][#16] 오류 수정
    Contents
    - 깃 충돌 해결
    
    Todo
    - 깃 충돌 해결

--- backend/src/main/java/com/example/backend/domain/card/CardType.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.domain.card;
-
-public enum CardType {
-    TODO, COMPLETED, PROGRESSING;
-}
--- backend/src/main/java/com/example/backend/service/column/ColumnService.java
+++
@@ -1,4 +0,0 @@
-package com.example.backend.service.column;
-
-public class ColumnService {
-}
