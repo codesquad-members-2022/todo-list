c02fe5024bf70b0eabbb8bfa16053c796d004718
Damagucci-Juice <jsr9874@gmail.com>
2022-04-13 11:39:40 +0900

    feat: Activity 모델을 fetch 해올 NetworkHandler 클래스를 선언

--- ios/TodoApp/TodoApp/Models/NetworkHandler.swift
+++ ios/TodoApp/TodoApp/Models/NetworkHandler.swift
@@ -12,46 +12,33 @@ class NetworkHandler {
     class func getData(resource: String) {
         // 세션 환경 설정
         let defaultSession = URLSession(configuration: .default)
-        guard let url = URL(string: resource) else {
+        
+        guard let url = URL(string: "\(resource)") else {
             print("URL is nil")
             return
         }
+        
         // Request
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
+        let request = URLRequest(url: url)
         
         // DataTask
-        
-        let dataTask = defaultSession.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?)  in
-            let successRange = 200..<300
-            
+        let dataTask = defaultSession.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             // getting data error
             guard error == nil else {
                 Logger.view.error("Error occur: \(String(describing: error))")
                 return
             }
             
-            guard let data = data, let responseObject = response as? HTTPURLResponse, successRange.contains(responseObject.statusCode) else {
-                Logger.view.error("status code: \((response as? HTTPURLResponse)!.statusCode)")
+            guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
                 return
             }
             // 통신에 성공한 경우 data 에 Data 객체가 전달됩니다.
-//            guard let jsonToArray = try? JSONSerialization.jsonObject(with: data, options: []) else {
-//                Logger.view.error("json to Any Error")
-//                return
-//            }
+            guard let jsonToArray = try? JSONSerialization.jsonObject(with:data, options: []) else {
+                Logger.view.error("json to Any Error")
+                return
+            }
             //원하는 작업
-            do {
-                let decoder = JSONDecoder()
-                let activities = try decoder.decode(Activities.self, from: data)
-                NotificationCenter.default.post(name: Notification.Name.fetch, object: self, userInfo: [NotificationKey.activity: activities])
-            }
-            catch let error {
-                print("--> error: \(error.localizedDescription)")
-
-            }
-            
+            print(jsonToArray)
         }
-        dataTask.resume()
     }
 }
