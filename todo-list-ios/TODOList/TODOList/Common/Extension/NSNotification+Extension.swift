a2f794ab53bfe011f69451f9b3ac4cac527b86ec
sanghee <anshee97@naver.com>
2022-04-14 01:22:03 +0900

    [#27] Feat: Add NSNotification+Extension

---
+++ todo-list-ios/TODOList/TODOList/Common/Extension/NSNotification+Extension.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension NSNotification.Name {
+    static let MemoDidAdd = NSNotification.Name("MemoDidAddNotification")
+    static let MemoDidUpdate = NSNotification.Name("MemoDidUpdateNotification")
+    static let MemoDidDelete = NSNotification.Name("MemoDidDeleteNotification")
+}
