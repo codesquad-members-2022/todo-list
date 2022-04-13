519f7c2031e460643a3f950c3f6d8b9546f11480
Lee SeEun <sally4405@naver.com>
2022-04-13 11:04:49 +0900

    [#5] feat: domain 추가, info.plist 접근 권한 수정

--- iOS/TodoList/Util/NameSpace.swift
+++ iOS/TodoList/Util/NameSpace.swift
@@ -24,11 +24,12 @@ enum APIResponse {
 }
 
 enum API {
-    static let postURL: String = ""
-    static let getTodosURL: String = ""
-    static let getEventURL: String = ""
-    static let putURL: String = ""
-    static let deleteURL: String = ""
+    static let baseURL: String = "http://ec2-54-253-23-157.ap-southeast-2.compute.amazonaws.com/"
+    static let postURL: String = baseURL + "todo"
+    static let getTodosURL: String = baseURL + "todos"
+    static let getEventURL: String = baseURL + "events"
+    static var putURL: (String) -> String = { id in baseURL + "todo/\(id)" }
+    static let deleteURL: (String) -> String = { id in baseURL + "todo/\(id)" }
 }
 
 enum NotificationKeyValue {
