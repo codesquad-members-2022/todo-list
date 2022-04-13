c783a404b7ff9afd41533ff45cb5980ecbdc0ae1
moonyerim <moonyerim2@gmail.com>
2022-04-13 17:30:23 +0900

    Rename: #24 util.js -> fetch.js 변경

---
+++ front/static/resources/js/utils/fetch.js
@@ -0,0 +1,33 @@
+const HTTP_REQUEST = {
+  PUT(data) {
+    return {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  },
+  DELETE() {
+    return {
+      method: 'DELETE'
+    };
+  },
+  POST(data) {
+    return {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  }
+};
+
+const fetchRequest = async (url, option = {}) => {
+  const response = await fetch(url, option);
+  if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+  return response.json();
+};
+
+export { HTTP_REQUEST, fetchRequest };
