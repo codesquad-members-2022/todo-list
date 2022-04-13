4797fd263de1608dda33c889f05840d95104d578
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-04-13 20:32:10 +0900

    feat: 활동 로그 Controller 생성
    - 해당 user가 활동한 기록만 find하게 구현했습니다.

---
+++ backend/src/main/java/com/team15/todoapi/controller/HistoryController.java
@@ -0,0 +1,24 @@
+package com.team15.todoapi.controller;
+
+import com.team15.todoapi.controller.history.HistoryResponse;
+import com.team15.todoapi.domain.History;
+import com.team15.todoapi.service.HistoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/histories")
+@RequiredArgsConstructor
+public class HistoryController {
+
+	private final HistoryService historyService;
+
+	@GetMapping()
+	public List<HistoryResponse> histories(@RequestParam String userId){
+		return historyService.findAll(userId);
+	}
+}
