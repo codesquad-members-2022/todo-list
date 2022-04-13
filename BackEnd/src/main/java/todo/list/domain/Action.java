9d3b4adc17d92f2c443ee712253f9392677e7736
hwicode <khc3796@naver.com>
2022-04-13 13:58:26 +0900

    feat: 클라이언트에서 request를 보낼때 action과 nowStatus를 소문자로 보내도 enum으로 변환시킬 수 있도록 수정

--- BackEnd/src/main/java/todo/list/domain/Action.java
+++ BackEnd/src/main/java/todo/list/domain/Action.java
@@ -1,9 +1,16 @@
 package todo.list.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Action {
     ADD,
     REMOVE,
     UPDATE,
     MOVE;
 
+    @JsonCreator
+    public static Action valueOfWithCaseInsensitive(String name) {
+        name = name.toUpperCase();
+        return valueOf(name);
+    }
 }
--- BackEnd/src/main/java/todo/list/domain/CardStatus.java
+++ BackEnd/src/main/java/todo/list/domain/CardStatus.java
@@ -1,12 +1,16 @@
 package todo.list.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum CardStatus {
     TODO,
     IN_PROGRESS,
     DONE;
 
+    @JsonCreator
     public static CardStatus from(String name) {
         try {
+            name = name.toUpperCase();
             return valueOf(name);
         } catch (NullPointerException e) {
             return null;
