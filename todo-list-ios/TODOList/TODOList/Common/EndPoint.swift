cc8909bc9727efa07ee37030e6bdf4de774555e5
sanghee <anshee97@naver.com>
2022-04-13 16:52:47 +0900

    [#29] Feat: Add HTTPRequestHandler, MemoPostData

---
+++ todo-list-ios/TODOList/TODOList/Common/EndPoint.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+class EndPoint {
+    static let url = "http://13.125.205.248:8080/"
+}
---
+++ todo-list-ios/TODOList/TODOList/Data/Network/Model/MemoPostData.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct MemoPostData: Codable {
+    let title: String
+    let content: String
+    let author: String
+    let status: String
+}
