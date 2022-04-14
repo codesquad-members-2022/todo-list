1bbe7f4ba53262726c237d72bf9e71bafa406264
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2022-04-14 23:43:04 +0900

    fix: CORS 오류 해결 (#70)
    1. CORS config 글로벌 등록
    
    end

--- BE/src/main/java/com/team26/todolist/TodolistApplication.java
+++ BE/src/main/java/com/team26/todolist/TodolistApplication.java
@@ -2,6 +2,9 @@ package com.team26.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodolistApplication {
@@ -10,4 +13,13 @@ public class TodolistApplication {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:9000");
+			}
+		};
+	}
 }
