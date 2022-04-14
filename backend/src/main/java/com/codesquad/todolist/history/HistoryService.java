177a2bbe160780a056fc4a1b8fd00e02be78fc8c
Kang Changgu (Miller) <rkckr1@naver.com>
2022-04-14 14:05:39 +0900

    chore: [#68] Service 계층의 메서드 내 주석 추가
    - CardService 의 findNextId, ColumnService 의 findAll, HistoryService 의 findAll 메서드 내 주석 추가

--- backend/src/main/java/com/codesquad/todolist/history/HistoryService.java
+++ backend/src/main/java/com/codesquad/todolist/history/HistoryService.java
@@ -23,17 +23,22 @@ public class HistoryService {
     }
 
     public Slice<HistoryResponse> findAll(Criteria criteria) {
+        // 페이지 내에 포함된 모든 history 객체를 조회
         List<History> histories = historyRepository.findAll(criteria);
 
+        // 조회한 histories 리스트에서 history Id 속성만 리스트로 추출
         List<Integer> historyIds = histories.stream()
             .map(History::getHistoryId)
             .collect(Collectors.toList());
 
+        // history Id 리스트에 포함된 history Id 가 FK 인 modified field 테이블 조회
         List<ModifiedField> modifiedFields = modifiedFieldRepository.findByHistoryIds(historyIds);
 
+        // map 자료구조에 history Id 별로 modified field 리스트를 저장
         Map<Integer, List<ModifiedField>> modifiedFieldMap = modifiedFields.stream()
             .collect(Collectors.groupingBy(ModifiedField::getHistoryId));
 
+        // 각 history 객체에 조회된 modified field 리스트를 설정
         histories.forEach(
             history -> history.setFields(modifiedFieldMap.get(history.getHistoryId()))
         );
