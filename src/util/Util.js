bc07a3acf97ce3768850930beccdeffeeb44e9f9
jthw1005 <skl0105@naver.com>
2022-04-14 15:53:44 +0900

    Feat: Fetch 함수 구현
    Dott와 페어 프로그래밍 중

--- src/util/Util.js
+++ src/util/Util.js
@@ -1,18 +1,53 @@
-const $ = (query, baseNode = document) => baseNode.querySelector(query);
+const port = 3000;
 
-const $All = (query, baseNode = document) => baseNode.querySelectorAll(query);
+function $(query, baseNode = document) {
+  return baseNode.querySelector(query);
+}
 
-const fetchData = async (url) => {
+function $All(query, baseNode = document) {
+  return baseNode.querySelectorAll(query);
+}
+
+function on(eventType, targetNode, handlerFn) {
+  targetNode.addEventListener(eventType, handlerFn);
+}
+
+async function fetchData(url) {
   try {
     const data = await fetch(url);
     return data.json();
   } catch (err) {
     console.err(err);
   }
-};
-
-function on(eventType, targetNode, handlerFn) {
-  targetNode.addEventListener(eventType, handlerFn);
 }
 
-export { $, $All, fetchData, on };
+async function postData(url, data) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  //return response.json();
+}
+
+async function updateData(url, data) {
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  //return response.json();
+}
+
+async function deleteData(url) {
+  const response = await fetch(url, {
+    method: "DELETE",
+  });
+  //return response.json();
+}
+
+export { $, $All, on, port, fetchData, postData, updateData, deleteData };
