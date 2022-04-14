b14dc33d990f0767c53f8e089217f78a6107e40e
mybloom <baejungeun11@gmail.com>
2022-04-14 18:41:00 +0900

    refactor: 카드 생성시 활동로그 테이블에도 insert되어야 하는데, 도메인상 활동로그는 비교적 덜 중요하게 판단하여, 카드 생성만 되도록 로직 개발
    - 활동로그까지 정상적으로 작동하는 상황(201)과 분리하기 위해 http status 207 을 보냄.

--- backend/src/main/java/com/team15/todoapi/service/CardService.java
+++ backend/src/main/java/com/team15/todoapi/service/CardService.java
@@ -1,17 +1,22 @@
 package com.team15.todoapi.service;
 
+import com.team15.todoapi.controller.dto.DefaultResponse;
 import com.team15.todoapi.controller.dto.card.CardRequest;
 import com.team15.todoapi.controller.dto.card.CardResponse;
 import com.team15.todoapi.domain.Card;
+import com.team15.todoapi.domain.CardAction;
 import com.team15.todoapi.domain.Member;
 import com.team15.todoapi.repository.CardRepository;
 import com.team15.todoapi.repository.MemberRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CardService {
@@ -29,15 +34,25 @@ public class CardService {
 	}
 
 	@Transactional
-	public CardResponse.AddInfo add(CardRequest cardRequest) {
+	public DefaultResponse add(CardRequest cardRequest) {
 		Member member = selectMemberInfo(cardRequest.getUserId());
 
+		DefaultResponse defaultResponse = new DefaultResponse();
 		Card card = Card.of(cardRequest, member.getId());
 		card = cardRepository.add(card);
 
-		historyService.add(card);
+		try {
+			card.insertAction(card, CardAction.ADD.getCode());
+			historyService.add(card);
+			defaultResponse.setHttpStatus(HttpStatus.CREATED);
+		} catch (Exception e) {
+			defaultResponse.setHttpStatus(HttpStatus.MULTI_STATUS);
+			log.info("CARD ADD - CardService.add Exception = {}" + e.getMessage());
+		}finally{
+			defaultResponse.setCustomResponse(CardResponse.AddInfo.from(card));
+		}
 
-		return CardResponse.AddInfo.from(card);
+		return defaultResponse;
 	}
 
 	private Member selectMemberInfo(String userId) {
