123f77d809c72b1bc75ba4507c77f3e0d8fe329f
honeySleepr <gruzzimo@naver.com>
2022-04-14 16:16:39 +0900

    fix: [#70] Update 시 PUT 대신 PATCH 메서드를 사용하도록 변경
    - 카드 값 업데이트 시 제목, 내용, 작성자 필드 데이터를 모두 받을 필요 없이
    변경되는 필드만 받도록 수정

--- backend/src/main/java/com/codesquad/todolist/card/CardController.java
+++ backend/src/main/java/com/codesquad/todolist/card/CardController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -43,7 +44,7 @@ public class CardController {
     }
 
     @ApiOperation(value = "카드 업데이트", notes = "지정한 카드를 새로운 데이터로 업데이트합니다.")
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public HistoryResponse updateCard(
         @ApiParam(name = "id", value = "업데이트할 카드 Id", required = true)
         @PathVariable(value = "id") Integer cardId,
