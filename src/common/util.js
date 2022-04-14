fb025ca3075997abe48c614501623ad55e869f32
happyGyu <happyboyto.dev@gmail.com>
2022-04-15 01:27:21 +0900

    Feat: 드래그앤드롭 구현 but 버그 존재

--- src/common/util.js
+++ src/common/util.js
@@ -36,3 +36,4 @@ export const request = {
     return updatedColumnState;
   }
 };
+
--- src/components/main/column-container/Column/card/Card.js
+++ src/components/main/column-container/Column/card/Card.js
@@ -77,6 +77,7 @@ const setEvents = (cardNode, cardState) => {
     case CARD_TYPE.NORMAL:
       setDeleteBtnEvent(cardNode);
       setDoubleClickEvent(cardNode);
+      setMouseDownEvent(cardNode);
       break;
     case CARD_TYPE.ADDING:
     case CARD_TYPE.EDITING:
@@ -205,4 +206,148 @@ export const reRenderCard = (cardState) => {
 
 const findCardNode = (cardID) => {
   return document.querySelector(`[data-id="${cardID}"]`);
+};
+
+//drag n drop
+function classifyByEventCnt(singleEventHandler, interval) {
+  let cnt = 0;
+  let timer;
+
+  return (...args) => {
+    cnt++;
+    timer = setTimeout(() => {
+      if (cnt === 1) {
+        singleEventHandler(...args);
+        cnt = 0;
+      }
+    }, interval);
+    if (cnt === 2) {
+      clearTimeout(timer);
+      // doubleEventHandler(...args);
+      cnt = 0;
+    }
+  };
+}
+
+const setMouseDownEvent = (cardNode) => {
+  const singleMouseDownEventHandler = getSingleMouseDownEventHandler(cardNode);
+  //const doubleMouseDownEventHandler = getDoubleMouseDownEventHandler(cardNode);
+  cardNode.addEventListener("mousedown", classifyByEventCnt(singleMouseDownEventHandler, 200));
+};
+
+const getSingleMouseDownEventHandler = (cardNode) => (e) => {
+  handleSingleMouseDownEvent(cardNode, e);
+};
+
+const getDoubleMouseDownEventHandler = (cardNode) => () => {
+  handleDoubleClickEvent(cardNode);
+};
+
+const handleSingleMouseDownEvent = (cardNode, e) => {
+  //readyForMove
+  // const shadowCardNode = cardNode.cloneNode(true);
+  const followingCardNode = cardNode.cloneNode(true);
+  followingCardNode.classList.add("following");
+  document.body.append(followingCardNode);
+  followingCardNode.style.left = cardNode.getBoundingClientRect().left + "px";
+  followingCardNode.style.top = cardNode.getBoundingClientRect().top - 15 + "px";
+  const shiftX = e.clientX - cardNode.getBoundingClientRect().left;
+  const shiftY = e.clientY - cardNode.getBoundingClientRect().top;
+
+  cardNode.classList.add("shadow");
+
+  const handleMouseMove = getMouseMoveHandler(shiftX, shiftY, cardNode, followingCardNode);
+  const handleMouseUp = getMouseUpHandler(handleMouseMove, cardNode, followingCardNode);
+  document.body.addEventListener("mousemove", handleMouseMove);
+  document.body.addEventListener("mouseup", handleMouseUp, { once: true });
+};
+
+const getMouseMoveHandler = (shiftX, shiftY, cardNode, followingCardNode) => (event) => {
+  handleFollowingCard(shiftX, shiftY, followingCardNode, event);
+  handleCardShadow(cardNode, followingCardNode, event);
+};
+
+const getMouseUpHandler = (mouseMoveHandler, cardNode, followingCardNode) => () => {
+  endDragDropEvent(mouseMoveHandler, cardNode, followingCardNode);
+};
+
+const handleFollowingCard = (shiftX, shiftY, followingCardNode, event) => {
+  const [x, y] = getCardPosition(shiftX, shiftY, event);
+  moveFollowingCard(followingCardNode, x, y);
+};
+
+const getCardPosition = (shiftX, shiftY, event) => {
+  const x = event.pageX - shiftX + "px";
+  const y = event.pageY - shiftY + "px";
+  return [x, y];
+};
+
+const moveFollowingCard = (followingCardNode, x, y) => {
+  followingCardNode.style.left = x;
+  followingCardNode.style.top = y;
+};
+
+const handleCardShadow = (cardNode, followingCardNode, event) => {
+  const underMouseElement = getUnderMouseElement(followingCardNode, event.clientX, event.clientY);
+  const underMouseElementClass = inspectElementClass(underMouseElement);
+  moveCardShadow(underMouseElementClass, underMouseElement, cardNode, event);
+};
+
+const moveCardShadow = (underMouseElementClass, underMouseElement, cardNode, event) => {
+  switch (underMouseElementClass) {
+    case "card":
+      moveToAroundCard(underMouseElement, cardNode, event);
+      return;
+    case "card-content":
+      const parentCardElement = underMouseElement.closest(".card");
+      moveToAroundCard(parentCardElement, cardNode, event);
+      return;
+    case "column":
+      moveToColumn(underMouseElement, cardNode);
+      return;
+  }
+};
+
+const moveToAroundCard = (underMouseCardElement, cardNode, event) => {
+  const rect = underMouseCardElement.getBoundingClientRect();
+  if (event.clientY < (rect.top + rect.bottom) / 2) {
+    underMouseCardElement.parentNode.insertBefore(cardNode, underMouseCardElement);
+  } else {
+    underMouseCardElement.parentNode.insertBefore(cardNode, underMouseCardElement.nextElementSibling);
+  }
+};
+
+const moveToColumn = (underMouseColumnElement, cardNode) => {
+  const cardList = underMouseColumnElement.querySelector(".card-list");
+  cardList.append(cardNode);
+};
+
+const getUnderMouseElement = (followingCardNode, x, y) => {
+  followingCardNode.hidden = true;
+  const underMouseElement = document.elementFromPoint(x, y);
+  followingCardNode.hidden = false;
+  return underMouseElement;
+};
+
+const inspectElementClass = (underMouseElement) => {
+  if (hasClassName(underMouseElement, "card") && !hasClassName(underMouseElement, "shadow")) {
+    return "card";
+  } else if (hasClassName(underMouseElement, "column")) {
+    return "column";
+  } else if (underMouseElement.closest(".card")) {
+    return "card-content";
+  } else {
+    return "others";
+  }
+};
+
+const hasClassName = (element, className) => {
+  return element.classList.contains(className);
+};
+
+const endDragDropEvent = (mouseMoveHandler, cardNode, followingCardNode) => {
+  document.body.removeEventListener("mousemove", mouseMoveHandler);
+  document.body.removeChild(followingCardNode);
+  cardNode.classList.remove("shadow");
+  //서버한테 전송하는 등의 마무리 작업 로직 추가 필요
 };
