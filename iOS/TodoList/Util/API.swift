0de968227bf9a597445129463f241e1decb113eb
sungju-kim <wnwn0719@gmail.com>
2022-04-14 15:44:55 +0900

    [#6] refactor: URLManager 재사용 가능하게 수정, API 내부 로직 변경

--- iOS/TodoList/Util/API.swift
+++ iOS/TodoList/Util/API.swift
@@ -1,10 +1,63 @@
 import Foundation
 
 enum API {
-    static let baseURL: String = "http://ec2-54-253-23-157.ap-southeast-2.compute.amazonaws.com/"
-    static let postURL: String = baseURL + "todo"
-    static let getTodosURL: String = baseURL + "todos"
-    static let getEventURL: String = baseURL + "events"
-    static var putURL: (String) -> String = { id in baseURL + "todo/\(id)" }
-    static let deleteURL: (String) -> String = { id in baseURL + "todo/\(id)" }
+    case post(_ card: RequestCardData)
+    case get
+    case put(_ id: Int, _ card: RequestCardData)
+    case delete(_ id: Int)
+    case events
+    
+    private var baseURL: String {
+        return "http://ec2-54-253-23-157.ap-southeast-2.compute.amazonaws.com/"
+    }
+    private var url: URL? {
+        switch self {
+        case .post :
+            return URL(string: self.baseURL + "todo")
+        case .get :
+            return URL(string: self.baseURL + "todos")
+        case .put(let id, _) :
+            return URL(string: self.baseURL + "todo/\(id)")
+        case .delete(let id) :
+            return URL(string: self.baseURL + "todo/\(id)")
+        case .events :
+            return URL(string: self.baseURL + "events")
+        }
+        
+    }
+    private var method: String {
+        switch self {
+        case .post :
+            return "POST"
+        case .get :
+            return "GET"
+        case .put :
+            return "PUT"
+        case .delete :
+            return "DELETE"
+        case .events :
+            return "GET"
+        }
+    }
+    
+    private var body: Data? {
+        switch self {
+        case .post(let card) :
+            return try? JSONEncoder().encode(card)
+        case .put(_, let card) :
+            return try? JSONEncoder().encode(card)
+        default:
+            return nil
+        }
+        
+    }
+    
+    var urlRequest: URLRequest? {
+        guard let url = self.url else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = self.method
+        request.httpBody = self.body
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        return request
+    }
 }
