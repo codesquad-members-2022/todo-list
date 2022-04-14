b662e43826cd092002872beefa14c2bb0164a2ec
백상휘 <sanghwiback@gmail.com>
2022-04-14 17:18:26 +0900

    [Feat] 스토리보드 화면을 디자인 시안과 동일하게 작업 진행

--- Client/iOS/TodoList/Cells/TodoTableViewCell.swift
+++ Client/iOS/TodoList/Cells/TodoTableViewCell.swift
@@ -5,12 +5,21 @@ class TodoTableViewCell: UITableViewCell {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var contentLabel: UILabel!
     
+    @IBOutlet weak var roundedView: UIView!
+    
     static let cellName: String = "todoCell"
+    static let cornerRadius: CGFloat = 10
     
     static var nib: UINib {
         return UINib(nibName: String(describing: self), bundle: nil)
     }
-
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        roundedView.layer.cornerRadius = TodoTableViewCell.cornerRadius
+        roundedView.layer.masksToBounds = true
+    }
+    
     func applyTextAllLabels(data card: CardData) {
         setTitleLabelAttribute(card.title)
         setContentLabelAttribute(card.contents)
