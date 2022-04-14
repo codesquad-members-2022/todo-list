120d3b9655d8e77dcd383241053101afe2a5ddf0
banjjak2 <interr0bang@naver.com>
2022-04-14 12:39:16 +0900

    chore: MainActivity 테스트 코드를 뷰모델로 이동
    Espresso를 이용해 TestCase 작성 예정

--- Android/app/src/main/java/com/example/todolist/MainActivity.kt
+++ Android/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -10,32 +10,32 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.todolist.data.TasksRepository
 import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.history.HistoryAdapter
-import com.example.todolist.history.HistoryViewModel
 import com.example.todolist.tasks.data.Task
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var historyViewModel: HistoryViewModel
+    private lateinit var tasksViewModel: TasksViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        historyViewModel = ViewModelProvider(this, ViewModelFactory(TasksRepository())).get(HistoryViewModel::class.java)
+        tasksViewModel = ViewModelProvider(this, ViewModelFactory(TasksRepository())).get(
+            TasksViewModel::class.java)
 
         val historyAdapter = HistoryAdapter()
         binding.recyclerviewHistory.adapter = historyAdapter
         binding.btnMenu.setOnClickListener {
             binding.mainLayout.openDrawer(GravityCompat.END)
-            historyViewModel.getHistories()
+            tasksViewModel.getHistories()
         }
 
         binding.btnClose.setOnClickListener { binding.mainLayout.closeDrawer(GravityCompat.END) }
         binding.naviView.setNavigationItemSelectedListener(this)
 
-        historyViewModel.historyList.observe(this) { historyAdapter.submitList(it) }
-        historyViewModel.checkLoading.observe(this) {
+        tasksViewModel.historyList.observe(this) { historyAdapter.submitList(it) }
+        tasksViewModel.checkLoading.observe(this) {
             if (it) {
                 binding.spinner.visibility = View.VISIBLE
                 binding.recyclerviewHistory.visibility = View.GONE
@@ -45,27 +45,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
         }
 
-        val task1 = Task(
-            1,
-            "테스트하기1",
-            "콘텐츠테스트1",
-            "jung",
-            "doing",
-            "2022-04-06T15:30:00.000+09:00",
-            "2022-04-06T15:30:00.000+09:00"
-        )
-
-        val task2 = Task(
-            2,
-            "테스트하기2",
-            "콘텐츠테스트2",
-            "park",
-            "todo",
-            "2022-04-06T15:30:00.000+09:00",
-            "2022-04-06T15:30:00.000+09:00"
-        )
-
-        binding.todoTodoView.addTasks(listOf(task1, task2))
+        binding.todoTodoView.addTasks(tasksViewModel.getSomeTasks())
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
--- Android/app/src/main/java/com/example/todolist/TasksViewModel.kt
+++ Android/app/src/main/java/com/example/todolist/TasksViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.todolist.history
+package com.example.todolist
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -6,9 +6,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todolist.data.Repository
 import com.example.todolist.history.data.HistoryCard
+import com.example.todolist.tasks.data.Task
 import kotlinx.coroutines.launch
 
-class HistoryViewModel(private val repository: Repository): ViewModel() {
+class TasksViewModel(private val repository: Repository): ViewModel() {
     private val _historyList = MutableLiveData<List<HistoryCard>>()
     val historyList: LiveData<List<HistoryCard>> get() = _historyList
 
@@ -24,5 +25,29 @@ class HistoryViewModel(private val repository: Repository): ViewModel() {
                 _checkLoading.value = false
             }
         }
+    }
+
+    fun getSomeTasks(): List<Task> {
+        val task1 = Task(
+            1,
+            "테스트하기1",
+            "콘텐츠테스트1",
+            "jung",
+            "doing",
+            "2022-04-06T15:30:00.000+09:00",
+            "2022-04-06T15:30:00.000+09:00"
+        )
+
+        val task2 = Task(
+            2,
+            "테스트하기2",
+            "콘텐츠테스트2",
+            "park",
+            "todo",
+            "2022-04-06T15:30:00.000+09:00",
+            "2022-04-06T15:30:00.000+09:00"
+        )
+
+        return listOf(task1, task2)
     }
 }
