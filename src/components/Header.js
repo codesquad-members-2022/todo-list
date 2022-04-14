862c21c8ccae8ca0ff898b8eb68aca6c6b26e7db
bukim0329 <bukim0329@gmail.com>
2022-04-14 18:50:20 +0900

    [#29]Feat: 스크롤에 따른 헤더 변화, history 이벤트 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,18 @@ export default class Header extends Component {
   setEvent() {
     this.addEvent("click", ".menu-button", () => {
       const $menu = document.querySelector(".menu");
-      $menu.classList.remove("hidden-menu");
+      $menu.classList.remove("display-none");
+      setTimeout(() => $menu.classList.remove("hidden-menu"), 0);
+    });
+    document.addEventListener("scroll", () => {
+      document.querySelector("html").scrollTop > 98 ? this.$target.classList.add("fixed-header") : this.$target.classList.remove("fixed-header");
     });
   }
 
   template() {
     return /* html */ `
-      <header class="header">
-        <div class="service-name">TO-DO LIST</div>
-        <img class="menu-button" src="../src/style/images/menuButton.png" alt="메뉴버튼" />
-      </header>
+      <div class="service-name">TO-DO LIST</div>
+      <img class="menu-button" src="../src/style/images/menuButton.png" alt="메뉴버튼" />
     `;
   }
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,6 +8,29 @@ export default class Menu extends Component {
     this.addEvent("click", ".close-button", () => {
       this.$target.classList.add("hidden-menu");
     });
+
+    this.addEvent("transitionend", ".menu", () => {
+      if (this.$target.classList.contains("hidden-menu")) {
+        this.$target.classList.add("display-none");
+      }
+    });
+
+    this.addEvent("click", ".test", () => {
+      const $historyAlert = this.$target.querySelector(".action-alert");
+      $historyAlert.classList.remove("hidden-alert");
+    });
+
+    this.addEvent("click", ".action-alert", (e) => {
+      e.target.classList.add("hidden-alert");
+      const data = {
+        0: { user: "sam", todo: "HTML/CSS 공부하기", beforePosition: "하고 있는 일", afterPosition: "해야할 일", action: "이동" },
+        1: { user: "sam", todo: "HTML/CSS 공부하기", beforePosition: "해야할 일", afterPosition: "하고 있는 일", action: "이동" },
+        2: { user: "sam", todo: "HTML/CSS 공부하기", beforePosition: "", afterPosition: "해야할 일", action: "등록" },
+        3: { user: "sam", todo: "블로그에 포스팅할 것", beforePosition: "", afterPosition: "해야할 일", action: "등록" },
+        4: { user: "sam", todo: "Github 공부하기", beforePosition: "", afterPosition: "해야할 일", action: "등록" },
+      };
+      this.setState(data);
+    });
   }
 
   mounted() {
@@ -20,8 +43,10 @@ export default class Menu extends Component {
 
   template() {
     return /* html */ `
+      <div class="action-alert hidden-alert">새로운 히스토리가 있습니다.</div>
       <img class="close-button" src="../src/style/images/cross.png" alt="끄기버튼" />
       ${'<div class="history"></div>'.repeat(Object.entries(this.state).length)}
+      <button class="test">action 발생!</button>
     `;
   }
 }
