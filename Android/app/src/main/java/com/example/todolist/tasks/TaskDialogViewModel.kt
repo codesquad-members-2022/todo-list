d172fd107c2be3b1c5c3db291c0dadaac796692b
banjjak2 <interr0bang@naver.com>
2022-04-14 20:10:19 +0900

    feat: 텍스트 입력에 따라 버튼 enabled 처리

--- Android/app/src/main/java/com/example/todolist/tasks/TaskDialogViewModel.kt
+++ Android/app/src/main/java/com/example/todolist/tasks/TaskDialogViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.todolist.tasks
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.todolist.data.Repository
 
 class TaskDialogViewModel(private val repository: Repository) : ViewModel() {
-
+    var checkTitle = MutableLiveData(false)
+    var checkBody = MutableLiveData(false)
 }
