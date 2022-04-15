d255be012a12a777513a2baaba5a056552c6747c
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-15 14:29:39 +0900

    [#29] Add: JSONHandler 싱글톤 추가

---
+++ todo-list-ios/TODOList/TODOList/Network/JSONHandler.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+final class JSONHandler {
+    
+    private static let encoder = JSONEncoder()
+    private static let decoder = JSONDecoder()
+    
+    static func convertObjectToJSON<T: Encodable>(model: T) -> Data? {
+        return try? encoder.encode(model)
+    }
+    
+    static func convertJSONToObject<T: Decodable>(data: Data) -> T? {
+        return try? decoder.decode(T.self, from: data)
+    }
+    
+}
