dc62c04f2a3db1660dc7029710e5684a7d8a3b27
mybloom <baejungeun11@gmail.com>
2022-04-15 11:14:42 +0900

    refactor: iOS요청으로 CardRequest 객체 컬럼 추가
    - id , createdAt 속성 추가
    - iOS에서는 객체 하나로 최대한 함께 사용하는 것을 권장하여 각 기능을 하나의 Request객체로 사용할 수 있도록 서버단에서는 불필요한 속성이지만 추가하였습니다.

--- backend/src/main/java/com/team15/todoapi/controller/dto/card/CardRequest.java
+++ backend/src/main/java/com/team15/todoapi/controller/dto/card/CardRequest.java
@@ -1,5 +1,6 @@
 package com.team15.todoapi.controller.dto.card;
 
+import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -8,14 +9,17 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CardRequest {
-
+	
 	@NotBlank
 	private String title;
 	@NotBlank
 	private String content;
 	@NotBlank
 	private String userId;
-
 	private int section;
+
+	//iOS요청 : 각 기능별로 request객체를 통일 요청하여 추가
+	private Long id;
+	private LocalDateTime createdAt;
 
 }
