42957fd6b5e5596ba4089ddfd6197fcf9d3dcd26
seyeong <seyoung7555@naver.com>
2022-04-15 11:15:14 +0900

    refactor: CardsController에서 LogService 로직 분리

--- backend/src/main/java/com/example/backend/web/controller/CardsController.java
+++ backend/src/main/java/com/example/backend/web/controller/CardsController.java
@@ -1,9 +1,6 @@
 package com.example.backend.web.controller;
 
-import com.example.backend.domain.ActionType;
-import com.example.backend.domain.Card;
 import com.example.backend.service.CardService;
-import com.example.backend.service.LogService;
 import com.example.backend.web.dto.*;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +9,9 @@ import org.springframework.web.bind.annotation.*;
 public class CardsController {
 
     private final CardService cardService;
-    private final LogService logService;
 
-    public CardsController(CardService cardService, LogService logService) {
+    public CardsController(CardService cardService) {
         this.cardService = cardService;
-        this.logService = logService;
     }
 
     @ApiOperation(value = "Card 조회")
@@ -28,63 +23,24 @@ public class CardsController {
     @ApiOperation(value = "Card 생성")
     @PostMapping("/cards")
     public CardListResponseDto save(@RequestBody CardSaveRequestDto dto) {
-        String title = dto.getTitle();
-        String columnName = dto.getColumnName();
-        LogSaveRequestDto logSaveRequestDto = new LogSaveRequestDto.Builder()
-                .title(title)
-                .curColumnName(columnName)
-                .actionType(ActionType.ADD)
-                .build();
-        logService.save(logSaveRequestDto);
-
         return cardService.save(dto);
     }
 
     @ApiOperation(value = "Card 수정")
     @PutMapping("/cards/{id}")
     public CardListResponseDto update(@PathVariable Long id, @RequestBody CardUpdateRequestDto dto) {
-        String title = dto.getTitle();
-        LogSaveRequestDto logSaveRequestDto = new LogSaveRequestDto.Builder()
-                .title(title)
-                .actionType(ActionType.UPDATE)
-                .build();
-        logService.save(logSaveRequestDto);
-
         return cardService.update(id, dto);
     }
 
     @ApiOperation(value = "Card 삭제")
     @DeleteMapping("/cards/{id}")
     public Long delete(@PathVariable Long id) {
-        Card card = cardService.findById(id);
-        LogSaveRequestDto logSaveRequestDto = new LogSaveRequestDto.Builder()
-                .title(card.getTitle())
-                .curColumnName(card.getColumnName())
-                .actionType(ActionType.REMOVE)
-                .build();
-        logService.save(logSaveRequestDto);
-
         return cardService.delete(id);
     }
 
     @ApiOperation(value = "Card 이동")
     @PutMapping("/cards")
     public Columns move(@RequestBody CardMoveRequestDto dto) {
-        Long id = dto.getId();
-        Card currentCard = cardService.findById(id);
-        String title = currentCard.getTitle();
-        String columnName = currentCard.getColumnName();
-
-        if (!columnName.equals(dto.getNewColumnName())) {
-            LogSaveRequestDto logSaveRequestDto = new LogSaveRequestDto.Builder()
-                    .title(title)
-                    .prevColumnName(columnName)
-                    .curColumnName(dto.getNewColumnName())
-                    .actionType(ActionType.MOVE)
-                    .build();
-            logService.save(logSaveRequestDto);
-        }
-
         return cardService.move(dto);
     }
 }
