65cb61259a08e35db511a417d65d9f5448cc9328
seok <dhdustnr0134@naver.com>
2022-04-15 15:12:52 +0900

    chore : 쓸데없는 파일 제거

--- BackEnd/src/main/resources/data.sql
+++ BackEnd/src/main/resources/data.sql
@@ -1,14 +1,3 @@
-insert into activity_log
-    (activity_log_action, title, now_status, before_status) values
-    ("ADD", "title1", "TODO", null),
-    ("ADD", "title2", "TODO", null),
-    ("REMOVE", "title3", "IN_PROGRESS", null),
-    ("REMOVE", "title4", "IN_PROGRESS", null),
-    ("UPDATE", "title5", "DONE", null),
-    ("UPDATE", "title6", "DONE", null),
-    ("MOVE", "title7", "TODO", "IN_PROGRESS"),
-    ("MOVE", "title8", "IN_PROGRESS", "DONE");
-
 insert into card
     (title, contents, card_status, author) values
     ("title1", "contents1", "TODO", "ANDROID"),
