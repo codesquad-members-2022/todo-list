bc0b9851628a1898ad15589c96596510c16b1a75
ju-kkim <wndus8198@naver.com>
2022-04-15 15:51:59 +0900

    chore: 주석 삭제

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -4,11 +4,8 @@ import css from '../scss/style.scss';
 import Controller from './Controller/Controller.js';
 import { Header } from './Header/main.js';
 import { History } from './History/main.js';
-// import { dragNdrop } from './dragNdrop';
 
 (function () {
   const controller = new Controller({ Header, History });
   controller.initAlert();
 })();
-
-// dragNdrop();
