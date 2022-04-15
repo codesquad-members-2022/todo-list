e2941bc7d2956f8241ae5dd6e678c34c1f76b8ce
Sangbeom Heo <mansaout@gmail.com>
2022-04-15 09:34:27 +0900

    Feat: 컬럼의 아이템 개수 기본 값을 0으로 변경

--- client/public/js/views/column/column.js
+++ client/public/js/views/column/column.js
@@ -1,12 +1,12 @@
 import { btnPlus, btnX } from "../../utils/assetURL";
 
-export function createColumn({ id, title, length }) {
+export function createColumn({ id, title }) {
   return `
     <li class="column" data-column="${id}">
       <div class="column__header">
         <div class="column__header__content">
           <h2 class="column__header--title">${title}</h2>
-          <div class="column__header--card-count">${length}</div>
+          <div class="column__header--card-count">0</div>
         </div>
         <div class="column__header__btn">
           <img class="column__header--add-card" src=${btnPlus} />
