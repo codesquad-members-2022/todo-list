91b0fa6e95e39e011321c360bb9ac9d4c038633e
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-04-15 11:33:01 +0900

    chore: 팀 네이밍 컨벤션에 맞추어 상수명, 테이블명 수정

--- backend/todolist/src/main/java/kr/codesquad/todolist/dao/HistoryDao.java
+++ backend/todolist/src/main/java/kr/codesquad/todolist/dao/HistoryDao.java
@@ -17,50 +17,49 @@ import java.util.List;
 @Repository
 public class HistoryDao {
 
-    private static final String TABLE_NAME = "history";
-    private static final String PRIMARY_KEY_COLUMN_NAME = "id";
-    private static final String ACTION_COLUMN_NAME = "action";
-    private static final String CARD_SUBJECT_COLUMN_NAME = "cardSubject";
-    private static final String PREV_CARD_STATUS_COLUMN_NAME = "prevCardStatus";
-    private static final String CURRENT_CARD_STATUS_COLUMN_NAME = "currentCardStatus";
-    private static final String CREATED_AT_COLUMN_NAME = "createdAt";
-    private static final String USER_ID_COLUMN_NAME = "userId";
-    private static final String CARD_ID_COLUMN_NAME = "cardId";
+    private static final String HISTORY_TABLE_NAME = "todo_history_table";
+    private static final String HISTORY_PRIMARY_KEY = "id";
+    private static final String HISTORY_ACTION = "action";
+    private static final String HISTORY_CARD_SUBJECT = "cardSubject";
+    private static final String HISTORY_PREV_CARD_STATUS = "prevCardStatus";
+    private static final String HISTORY_CURRENT_CARD_STATUS = "currentCardStatus";
+    private static final String HISTORY_CREATED_AT = "createdAt";
+    private static final String HISTORY_USER_ID = "userId";
+    private static final String HISTORY_CARD_ID = "cardId";
 
     private final JdbcTemplate jdbcTemplate;
 
     public void save(History history) {
         new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName(TABLE_NAME)
-                .usingGeneratedKeyColumns(PRIMARY_KEY_COLUMN_NAME)
+                .withTableName(HISTORY_TABLE_NAME)
+                .usingGeneratedKeyColumns(HISTORY_PRIMARY_KEY)
                 .execute(new BeanPropertySqlParameterSource(history));
     }
 
     public List<History> findAllByUserId(Long userId) {
-        String sql = String.format("SELECT %s, %s, %s, %s, %s, %s, %s, %s FROM %s WHERE %s = ?;",
-                PRIMARY_KEY_COLUMN_NAME,
-                ACTION_COLUMN_NAME,
-                CARD_SUBJECT_COLUMN_NAME,
-                PREV_CARD_STATUS_COLUMN_NAME,
-                CURRENT_CARD_STATUS_COLUMN_NAME,
-                CREATED_AT_COLUMN_NAME,
-                USER_ID_COLUMN_NAME,
-                CARD_ID_COLUMN_NAME,
-                TABLE_NAME,
-                USER_ID_COLUMN_NAME);
+        String sql = String.format("SELECT %1$s, %2$s, %3$s, %4$s, %5$s, %6$s, %7$s, %8$s FROM %9$s WHERE %7$s = ?;",
+                HISTORY_PRIMARY_KEY,
+                HISTORY_ACTION,
+                HISTORY_CARD_SUBJECT,
+                HISTORY_PREV_CARD_STATUS,
+                HISTORY_CURRENT_CARD_STATUS,
+                HISTORY_CREATED_AT,
+                HISTORY_USER_ID,
+                HISTORY_CARD_ID,
+                HISTORY_TABLE_NAME);
 
         return jdbcTemplate.query(sql, historyRowMapper(), userId);
     }
 
     private RowMapper<History> historyRowMapper() {
         return (rs, rowNum) ->
-                new History(rs.getLong(PRIMARY_KEY_COLUMN_NAME),
-                        History.Action.valueOf(rs.getString(ACTION_COLUMN_NAME)),
-                        rs.getString(CARD_SUBJECT_COLUMN_NAME),
-                        Card.TodoStatus.valueOf(rs.getString(PREV_CARD_STATUS_COLUMN_NAME)),
-                        Card.TodoStatus.valueOf(rs.getString(CURRENT_CARD_STATUS_COLUMN_NAME)),
-                        rs.getTimestamp(CREATED_AT_COLUMN_NAME).toLocalDateTime(),
-                        rs.getLong(USER_ID_COLUMN_NAME),
-                        rs.getLong(CARD_ID_COLUMN_NAME));
+                new History(rs.getLong(HISTORY_PRIMARY_KEY),
+                        History.Action.valueOf(rs.getString(HISTORY_ACTION)),
+                        rs.getString(HISTORY_CARD_SUBJECT),
+                        Card.TodoStatus.valueOf(rs.getString(HISTORY_PREV_CARD_STATUS)),
+                        Card.TodoStatus.valueOf(rs.getString(HISTORY_CURRENT_CARD_STATUS)),
+                        rs.getTimestamp(HISTORY_CREATED_AT).toLocalDateTime(),
+                        rs.getLong(HISTORY_USER_ID),
+                        rs.getLong(HISTORY_CARD_ID));
     }
 }
